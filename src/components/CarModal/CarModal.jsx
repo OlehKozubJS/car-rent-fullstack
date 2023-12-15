3e439dfaff0d505b650cc66ba047113d8bb2573a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-16 00:27:48 +0200

    Update CarModal.jsx

--- src/components/CarModal/CarModal.jsx
+++ src/components/CarModal/CarModal.jsx
@@ -1,21 +1,7 @@
 import { useState, useEffect } from 'react';
 import { SVGimage } from '../../images';
-import cars from '../advertsCars.json';
-import {
-  CarModalBackdrop,
-  CarModalContainer,
-  CarModalCloseButtonContainer,
-  CarModalCloseButton,
-  CarModalHeader,
-  CarModalHeaderMake,
-  CarModalItems,
-  CarModalItem,
-  CarModalDescription,
-  CarModalSubHeader,
-  CarModalRentalItem,
-  CarModalRentalItemValue,
-  CarModalSubmitButton,
-} from './CarModal.module.css';
+import cars from '../../advertsCars.json';
+import css from './CarModal.module.css';
 
 const CarModal = ({ carId, onClose }) => {
   const [car] = useState(cars.find(car => car.id === carId));
@@ -35,68 +21,70 @@ const CarModal = ({ carId, onClose }) => {
   }, [onClose]);
 
   return (
-    <div>
-      <div>
-        <button onClick={onClose}>
-          <SVGimage name="close" />
-        </button>
+    <div className={css.CarModalBackdrop}>
+      <div className={css.CarModalContainer}>
+        <div className={css.CarModalCloseButtonContainer}>
+          <button className={css.CarModalCloseButton} onClick={onClose}>
+            <SVGimage name="close" />
+          </button>
+        </div>
+        <img src={car.img} alt="" />
+        <h3 className={css.CarModalHeader}>
+          <span>{car.make}</span>
+          <span>{car.model}</span>, <span>{car.year}</span>
+        </h3>
+        <div>
+          <div>{car.address.split(', ')[1]}</div>
+          <div>{car.address.split(', ')[2]}</div>
+          <div>
+            Id:
+            <span>{car.id}</span>
+          </div>
+          <div>
+            Year:
+            <span>{car.year}</span>
+          </div>
+          <div>
+            Type:
+            <span>{car.type}</span>
+          </div>
+          <div>
+            Fuel Consumption:
+            <span>{car.fuelConsumption}</span>
+          </div>
+          <div>
+            Engine Size:
+            <span>{car.engineSize}</span>
+          </div>
+        </div>
+        <h4>Accessories and functionalities:</h4>
+        <ul>
+          {car.accessories.map(item => {
+            return <li>{item}</li>;
+          })}
+          {car.functionalities.map(item => {
+            return <li>{item}</li>;
+          })}
+        </ul>
+        <h4>Rental Conditions: </h4>
+        <div>
+          <div>
+            Minimum age:
+            <span>{car.rentalConditions.split('\n')[0].split(' ')[1]}</span>
+          </div>
+          <div>{car.rentalConditions.split('\n')[1]}</div>
+          <div>{car.rentalConditions.split('\n')[2]}</div>
+          <div>
+            Mileage:
+            <span>{car.mileage}</span>
+          </div>
+          <div>
+            Price:
+            <span>{car.rentalPrice}</span>
+          </div>
+        </div>
+        <button>Rental car</button>
       </div>
-      <img src={car.img} alt="" />
-      <h3>
-        <span>{car.make}</span>
-        <span>{car.model}</span>, <span>{car.year}</span>
-      </h3>
-      <div>
-        <div>{car.address.split(', ')[1]}</div>
-        <div>{car.address.split(', ')[2]}</div>
-        <div>
-          Id:
-          <span>{car.id}</span>
-        </div>
-        <div>
-          Year:
-          <span>{car.year}</span>
-        </div>
-        <div>
-          Type:
-          <span>{car.type}</span>
-        </div>
-        <div>
-          Fuel Consumption:
-          <span>{car.fuelConsumption}</span>
-        </div>
-        <div>
-          Engine Size:
-          <span>{car.engineSize}</span>
-        </div>
-      </div>
-      <h4>Accessories and functionalities:</h4>
-      <ul>
-        {car.accessories.map(item => {
-          return <li>{item}</li>;
-        })}
-        {car.functionalities.map(item => {
-          return <li>{item}</li>;
-        })}
-      </ul>
-      <h4>Rental Conditions: </h4>
-      <div>
-        <div>
-          Minimum age:
-          <span>{car.rentalConditions.split('\n')[0].split(' ')[1]}</span>
-        </div>
-        <div>{car.rentalConditions.split('\n')[1]}</div>
-        <div>{car.rentalConditions.split('\n')[2]}</div>
-        <div>
-          Mileage:
-          <span>{car.mileage}</span>
-        </div>
-        <div>
-          Price:
-          <span>{car.rentalPrice}</span>
-        </div>
-      </div>
-      <button>Rental car</button>
     </div>
   );
 };
