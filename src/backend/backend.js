f5f3651e4b3fc7e1788feb864d0f26d6bfcf5da4
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-18 23:52:09 +0200

    Update backend.js

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -2,5 +2,9 @@ import { readFile, writeFile } from 'fs/promises';
 
 const getData = async () => {
   const data = await readFile('./data.txt');
-  return data;
+  return JSON.parse(data);
+};
+
+const setData = async data => {
+  await writeFile('./data.txt', JSON.stringify(data));
 };
