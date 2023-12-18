8ab4cd2cfa7c4195702e9f7e1f7f9c8ed73177c0
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-19 00:02:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState /*useEffect*/ } from 'react';
 import { CarModal } from './components/CarModal/CarModal';
+import { getData, setData } from './backend';
 //import cars from "./advertsCars.json";
 
 function App() {
