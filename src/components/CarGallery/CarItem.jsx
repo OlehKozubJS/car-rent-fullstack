eb9df500aa26da080e5d11e77b6bd0e350eb181a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-27 20:42:56 +0200

    Update CarItem.jsx

---
+++ src/components/CarGallery/CarItem.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+import cars from "../../advertsCars.json";
+import carBrands from "../../makes.json";
+
+import { SVGimage } from "../../images";
+
+const CarItem = ({}) => {
+  return <>This is car item</>;
+};
+
+export { CarItem };
