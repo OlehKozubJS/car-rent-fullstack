85ea4aeeb04f984c01fc237fe51fd9e6a9e2c089
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-27 20:42:22 +0200

    Update CarGallery.jsx

---
+++ src/components/CarGallery/CarGallery.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+import cars from "../../advertsCars.json";
+import carBrands from "../../makes.json";
+
+import { SVGimage } from "../../images";
+
+const CarGallery = ({}) => {
+  return <>This is car gallery</>;
+};
+
+export { CarGallery };
