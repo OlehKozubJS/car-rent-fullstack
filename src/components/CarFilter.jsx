0b9eab7731175155af6188752ce17fba0d200e6d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-27 20:25:48 +0200

    Delete CarFilter.jsx

--- src/components/CarFilter.jsx
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from "react";
-import { SVGimage } from "../images";
-import cars from "../advertsCars.json";
-
-const CarFilter = ({}) => {
-  return <>This is car filter</>;
-};
-
-export { CarFilter };
