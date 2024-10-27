fae665b1ea2c53d08ae4a8318886521bf85d154e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-27 20:25:34 +0200

    Create CarFilter.jsx

---
+++ src/components/CarFilter/CarFilter.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import { SVGimage } from "../images";
+import cars from "../advertsCars.json";
+
+const CarFilter = ({}) => {
+  return <>This is car filter</>;
+};
+
+export { CarFilter };
