ed50baa64a44aa38c9eaa346dc64695ddec48ea8
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-27 23:47:30 +0200

    Update CarBrandsSelector.jsx

--- src/components/CarFilter/CarBrandsSelector.jsx
+++ src/components/CarFilter/CarBrandsSelector.jsx
@@ -5,7 +5,7 @@ import makes from "../../makes.json";
 
 import { SVGimage } from "../../images";
 
-const CarFilter = ({}) => {
+const CarBrandsSelector = ({}) => {
   const [carBrands, setCarBrands] = useState(makes);
   const [isCarBrandsOptions, setIsCarBrandsOptions] = useState(false);
 
@@ -18,44 +18,33 @@ const CarFilter = ({}) => {
   };
 
   return (
-    <form>
-      <div></div>
-      <div></div>
-      <label htmlFor="mileageFrom">
-        <input id="mileageFrom" type="number" placeholder="From" />
+    <div>
+      <label htmlFor="carBrandSearch">
+        <input id="carBrandSearch" type="text" placeholder="Enter the text" />
       </label>
-      <label htmlFor="mileageTo">
-        <input id="mileageTo" type="number" placeholder="To" />
-      </label>
-      <div>
-        <label htmlFor="carBrandSearch">
-          <input id="carBrandSearch" type="text" placeholder="Enter the text" />
-        </label>
-        <button
-          type="button"
-          onClick={
-            isCarBrandsOptions ? closeCarBrandOptions : openCarBrandOptions
-          }
-        >
-          {<SVGimage name={isCarBrandsOptions ? "up" : "down"} />}
-        </button>
-        {isCarBrandsOptions && (
-          <ul>
-            {carBrands.map((carBrand, carBrandIndex) => {
-              return (
-                <li key={carBrandIndex}>
-                  <button type="button" value={carBrand}>
-                    {carBrand}
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      <button type="submit">Search</button>
-    </form>
+      <button
+        type="button"
+        onClick={
+          isCarBrandsOptions ? closeCarBrandOptions : openCarBrandOptions
+        }
+      >
+        {<SVGimage name={isCarBrandsOptions ? "up" : "down"} />}
+      </button>
+      {isCarBrandsOptions && (
+        <ul>
+          {carBrands.map((carBrand, carBrandIndex) => {
+            return (
+              <li key={carBrandIndex}>
+                <button type="button" value={carBrand}>
+                  {carBrand}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
   );
 };
 
-export { CarFilter };
+export { CarBrandsSelector };
