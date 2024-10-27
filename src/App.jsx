b2fa57bb1b1f874c6b84badd5b713889d58942df
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-27 20:00:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState /*useEffect*/ } from "react";
+import { useState, useEffect } from "react";
 import { CarModal } from "./components/CarModal/CarModal";
 //import cars from "./advertsCars.json";
 
@@ -13,10 +13,26 @@ function App() {
     setIsModal(false);
   };
 
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    let dateAnimation;
+    const dateAnimationFunction = () => {
+      setDate(new Date());
+    };
+
+    dateAnimation = setInterval(dateAnimationFunction, 1000);
+
+    return () => {
+      clearInterval(dateAnimation);
+    };
+  }, []);
+
   return (
     <div>
       <button onClick={openModal}>Open Modal</button>
       {isModal && <CarModal carId={9582} onClose={closeModal} />}
+      {String(date)}
     </div>
   );
 }
