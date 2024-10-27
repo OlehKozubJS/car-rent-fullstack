b722dc424cb62e19ca802de0e7954a68cfb90bfa
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-27 21:26:07 +0200

    Update CarFilter.jsx

--- src/components/CarFilter/CarFilter.jsx
+++ src/components/CarFilter/CarFilter.jsx
@@ -6,7 +6,19 @@ import carBrands from "../../makes.json";
 import { SVGimage } from "../../images";
 
 const CarFilter = ({}) => {
-  return <>This is car filter</>;
+  return (
+    <form>
+      <div></div>
+      <div></div>
+      <label htmlFor="">
+        <input type="number" />
+      </label>
+      <label htmlFor="">
+        <input type="number" />
+      </label>
+      <button type="submit">Search</button>
+    </form>
+  );
 };
 
 export { CarFilter };
