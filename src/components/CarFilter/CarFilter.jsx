f06500ee35eabb7e3133562ed52f9e8753235da8
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-27 20:32:36 +0200

    Update CarFilter.jsx

--- src/components/CarFilter/CarFilter.jsx
+++ src/components/CarFilter/CarFilter.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
-import { SVGimage } from "../../images";
+
 import cars from "../../advertsCars.json";
+import carBrands from "../../makes.json";
+
+import { SVGimage } from "../../images";
 
 const CarFilter = ({}) => {
   return <>This is car filter</>;
