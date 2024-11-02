80c8fff30b5975da0dd96c0a96e787e3b43d2389
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-02 15:29:16 +0200

    Update ScrollRangeTest.jsx

--- src/ScrollRangeTest.jsx
+++ src/ScrollRangeTest.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 
 const ScrollRangeTest = () => {
+  const [isDraggable, setIsdraggable] = useState(false);
+  const [mouseDownY, setMouseDownY] = useState(0);
+  const [valueY, setValueY] = useState(0);
+
   return (
-    <div style={{ width: "10px", height: "500px", backgroundColor: "cyan" }}>
-      <div
-        style={{ wigth: "10px", height: "250px", backgroundColor: "blue" }}
-      ></div>
-    </div>
+    <>
+      <div style={{ width: "10px", height: "500px", backgroundColor: "cyan" }}>
+        <div
+          style={{ wigth: "10px", height: "250px", backgroundColor: "blue" }}
+        ></div>
+      </div>
+      <p>{isDraggable}</p>
+      <p>{mouseDownY}</p>
+      <p>{valueY}</p>
+    </>
   );
 };
 
