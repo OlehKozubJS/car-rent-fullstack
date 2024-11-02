6e1b0bf00ba5218328c57479736b9bb8c0296e74
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-02 15:54:09 +0200

    Update ScrollRangeTest.jsx

--- src/ScrollRangeTest.jsx
+++ src/ScrollRangeTest.jsx
@@ -5,10 +5,19 @@ const ScrollRangeTest = () => {
   const [mouseDownY, setMouseDownY] = useState(0);
   const [valueY, setValueY] = useState(0);
 
+  const handleMouseDown = (event) => {};
+  const handleMouseUp = (event) => {};
+  const handleMouseMove = (event) => {};
+  const handleMouseLeave = (event) => {};
+
   return (
     <>
       <div style={{ width: "10px", height: "500px", backgroundColor: "cyan" }}>
         <div
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+          onMouseLeave={handleMouseLeave}
           style={{ wigth: "10px", height: "250px", backgroundColor: "blue" }}
         ></div>
       </div>
