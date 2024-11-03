3e8102e79c791273d4f3469b778949db711a37ae
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-03 14:58:30 +0200

    Update ScrollRangeTest.jsx

--- src/ScrollRangeTest.jsx
+++ src/ScrollRangeTest.jsx
@@ -25,24 +25,22 @@ const ScrollRangeTest = () => {
       <Global
         styles={css`
           .menu-container {
-             {
-              box-sizing: border-box;
+            box-sizing: border-box;
 
-              height: 272px;
-              width: 224px;
+            height: 272px;
+            width: 224px;
 
-              padding-block: 14px;
-              padding-left: 18px;
-              padding-right: 8px;
+            padding-block: 14px;
+            padding-left: 18px;
+            padding-right: 8px;
 
-              border-style: solid;
-              border-radius: 14px;
-              border-width: 1px;
-              border-color: rgba(18, 20, 23, 0.05);
+            border-style: solid;
+            border-radius: 14px;
+            border-width: 1px;
+            border-color: rgba(18, 20, 23, 0.05);
 
-              display: flex;
-              justify-content: space-between;
-            }
+            display: flex;
+            justify-content: space-between;
           }
 
           .menu-list {
@@ -66,3 +64,7 @@ const ScrollRangeTest = () => {
 };
 
 export { ScrollRangeTest };
+
+/*
+
+*/
