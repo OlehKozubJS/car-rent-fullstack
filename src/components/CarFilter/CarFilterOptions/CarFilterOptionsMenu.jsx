1042550ebe8cd2636296e3c028b497f35a33f3a2
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-06 00:26:29 +0200

    Update CarFilterOptionsMenu.jsx

--- src/components/CarFilter/CarFilterOptions/CarFilterOptionsMenu.jsx
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsMenu.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Global, css } from "@emotion/react";
 
 import { ScrollRange } from "./ScrollRange";
 import { CarFilterOptionsMenuList } from "./CarFilterOptionsMenuList";
