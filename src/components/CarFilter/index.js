1baf9b964895fc6c47ad9ec9fa74d7150b23533c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-05 16:54:53 +0200

    Update index.js

--- src/components/CarFilter/index.js
+++ src/components/CarFilter/index.js
@@ -1,5 +1,4 @@
 import { CarFilter } from "./CarFilter";
 import { ScrollRangeTest } from "./ScrollRangeTest";
-import { SemiHiddenMenuTest } from "./SemiHiddenMenuTest";
 
-export { CarFilter, ScrollRangeTest, SemiHiddenMenuTest };
+export { CarFilter, ScrollRangeTest };
