240a3fe3ed6d6be14d3652604a37af659e0e960d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-06 00:08:45 +0200

    Update CarFilterOptionsStyles.jsx

---
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsStyles.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { Global, css } from "@emotion/react";
+
+import { ScrollRange } from "./ScrollRange";
+import { CarFilterOptionsMenuList } from "./CarFilterOptionsMenuList";
+
+const CarFilterOptionsStyles = ({ options, onChange }) => {
+  return (
+    <Global
+      styles={css`
+        .menu-container {
+          box-sizing: border-box;
+
+          height: ${menuContainerHeight}px;
+          width: 224px;
+
+          padding-block: 14px;
+          padding-left: 18px;
+          padding-right: 8px;
+
+          border-style: solid;
+          border-radius: 14px;
+          border-width: 1px;
+          border-color: rgba(18, 20, 23, 0.05);
+
+          display: flex;
+          justify-content: space-between;
+          align-items: flex-start;
+
+          overflow: hidden;
+        }
+
+        .menu-list {
+          list-style: none;
+          background-color: red;
+          margin: 0;
+          padding: 0;
+        }
+
+        .menu-option-radio {
+          display: none;
+        }
+
+        .options-list-container {
+          position: relative;
+        }
+      `}
+    />
+  );
+};
+
+export { CarFilterOptionsMenu };
