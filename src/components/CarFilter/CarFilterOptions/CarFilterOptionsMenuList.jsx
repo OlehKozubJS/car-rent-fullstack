0441d5c2ae9ca9247e14a0ea4fbd342b37828d12
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-05 23:41:24 +0200

    Create CarFilterOptionsMenuList.jsx

---
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsMenuList.jsx
@@ -0,0 +1,25 @@
+const CarFilterOptionsMenuList = ({ options, onChange }) => {
+  return (
+    <ul className="menu-list">
+      {options.map((option, optionIndex) => {
+        return (
+          <li key={optionIndex}>
+            <label htmlFor={optionIndex}>
+              <input
+                id={optionIndex}
+                type="radio"
+                name="menu-option-radio"
+                value={option}
+                onChange={onChange}
+                className="menu-option-radio"
+              />
+              <span>{option}</span>
+            </label>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export { CarFilterOptionsMenuList };
