2856bcda3289a0764ed8994cd397b680cd401cd5
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-05 15:54:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState, useEffect } from "react";
 
 import cars from "./advertsCars.json";
 
-import { CarFilter, ScrollRangeTest } from "./components/CarFilter";
+import {
+  CarFilter,
+  ScrollRangeTest,
+  SemiHiddenMenuTest,
+} from "./components/CarFilter";
 import { CarModal } from "./components/CarModal";
 import { CarGallery } from "./components/CarGallery";
 
