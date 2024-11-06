42f836ea5bfc9a041a9d224e94f42fa6e7acafb1
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-07 00:28:54 +0200

    Create menuListStyle.js

---
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsStyles/menuListStyle.js
@@ -0,0 +1,25 @@
+const menuListStyle = `        
+        .menu-list-container {
+          height: 244px;
+          width: 190px;
+
+          overflow: hidden;
+          position: relative;
+        }
+
+        .menu-list {
+          list-style: none;
+          margin: 0;
+          padding: 0;
+
+          position: absolute;
+          left: 0px;
+
+          color: rgba(18, 20, 23, 0.2);
+        }
+
+        .menu-option-radio {
+          display: none;
+        }`;
+
+export { menuListStyle };
