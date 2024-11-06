67e06e569e12b08a208901561b1e2a7bc2c540bb
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-07 00:48:31 +0200

    Create carFilterInputStyle.js

---
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsStyles/carFilterInputStyle.js
@@ -0,0 +1,29 @@
+const carFilterInputStyle = `        
+        .car-filter-input-label {
+          display: flex;
+
+          box-sizing: border-box;
+          height: 48px;
+          width: 224px;
+          padding: 14px 18px;
+
+          background-color: rgb(247, 247, 251);
+          border-radius: 14px;
+
+          margin-bottom: 4px;
+        }
+
+        .car-filter-input {
+          outline: none;
+          border: none;
+
+          background-color: inherit;
+
+          font-size: 18px;
+          color: rgb(18, 20, 23);
+        }
+
+        .car-filter-input:focus-within {
+        }`;
+
+export { carFilterInputStyle };
