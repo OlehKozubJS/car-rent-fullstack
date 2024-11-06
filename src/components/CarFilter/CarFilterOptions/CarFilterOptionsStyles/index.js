24a123fcd3e881ff60555c0ed2e8699dabf58db8
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-07 00:53:15 +0200

    Update index.js

---
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsStyles/index.js
@@ -0,0 +1,13 @@
+import { menuContainerStyle } from "./menuContainerStyle";
+import { menuListStyle } from "./menuListStyle";
+import { scrollRangeStyle } from "./scrollRangeStyle";
+import { carFilterInputStyle } from "./carFilterInputStyle";
+
+const carFilterOptionsStyles = [
+  menuContainerStyle,
+  menuListStyle,
+  scrollRangeStyle,
+  carFilterInputStyle,
+].join(" ");
+
+export { carFilterOptionsStyles };
