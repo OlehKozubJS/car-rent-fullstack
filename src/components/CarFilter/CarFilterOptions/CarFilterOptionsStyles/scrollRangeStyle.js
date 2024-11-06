8ca28aad4ef030b30a03a847850c9bf8e9e1ea85
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-07 00:45:06 +0200

    Create scrollRangeStyle.js

---
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsStyles/scrollRangeStyle.js
@@ -0,0 +1,18 @@
+const scrollRangeStyle = `        
+        .scroll-range-stator {
+          width: 8px;
+
+          position: relative;
+        }
+
+        .scroll-range-rotor {
+          position: absolute;
+          width: 8px;
+          right: 0px;
+
+          background-color: rgba(18, 20, 23, 0.05);
+
+          border-radius: 10px;
+        }`;
+
+export { scrollRangeStyle };
