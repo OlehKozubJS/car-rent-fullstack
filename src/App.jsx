7ad7976875e41ecb1a16aa8a6fdd881ee3a549f8
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-07 00:58:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import cars from "./advertsCars.json";
 
 import { CarFilter, CarGallery, CarModal } from "./components";
 
+import { CarRentStyles } from "./CarRentStyles";
+
 const App = () => {
   const [isModal, setIsModal] = useState(false);
 
@@ -31,6 +33,7 @@ const App = () => {
 
   return (
     <div>
+      <CarRentStyles />
       <button onClick={openModal}>Open Modal</button>
       {isModal && <CarModal carId={9582} onClose={closeModal} />}
       {String(date)}
