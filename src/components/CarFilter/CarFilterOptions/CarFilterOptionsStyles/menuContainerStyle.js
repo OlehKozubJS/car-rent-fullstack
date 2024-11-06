cdf1afaa8da599d8c63298f8e21766cc90660b27
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-07 00:27:11 +0200

    Create menuContainerStyle.js

---
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsStyles/menuContainerStyle.js
@@ -0,0 +1,21 @@
+const menuContainerStyle = `        
+        .menu-container {
+          box-sizing: border-box;
+
+          width: 224px;
+
+          padding-block: 14px;
+          padding-left: 18px;
+          padding-right: 8px;
+
+          border-style: solid;
+          border-radius: 14px;
+          border-width: 1px;
+          border-color: rgba(18, 20, 23, 0.05);
+
+          display: flex;
+          justify-content: space-between;
+          align-items: flex-start;
+        }`;
+
+export { menuContainerStyle };
