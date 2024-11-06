73ea151fbc36a6533f1187194bb4173e242ea768
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-06 14:21:05 +0200

    Update CarFilterOptionsMenu.jsx

--- src/components/CarFilter/CarFilterOptions/CarFilterOptionsMenu.jsx
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsMenu.jsx
@@ -16,30 +16,33 @@ const CarFilterOptionsMenu = ({ options, onChange }) => {
     const menuList = document.querySelector(".menu-list");
 
     if (menuList.offsetHeight < 244) {
-      setMenuContainerHeight(menuList.offsetHeight + 28);
+      setMenuListHeight(menuList.offsetHeight);
     } else {
-      setMenuContainerHeight(272);
+      setMenuListHeight(244);
     }
   }, []);
 
   return (
-    <div
-      className="menu-container"
-      style={{ height: `${menuContainerHeight}px` }}
-    >
-      <div className="menu-list-container">
-        <CarFilterOptionsMenuList
-          options={options}
-          onChange={onChange}
-          top={-valueY * 5}
+    <>
+      <div
+        className="menu-container"
+        style={{ height: `${menuListHeight + 28}px` }}
+      >
+        <div className="menu-list-container">
+          <CarFilterOptionsMenuList
+            options={options}
+            onChange={onChange}
+            top={-valueY * 10}
+          />
+        </div>
+        <ScrollRange
+          value={valueY}
+          onChange={handleScrollRangeValue}
+          height={50}
         />
       </div>
-      <ScrollRange
-        value={valueY}
-        onChange={handleScrollRangeValue}
-        height={5}
-      />
-    </div>
+      {menuContainerHeight}
+    </>
   );
 };
 
