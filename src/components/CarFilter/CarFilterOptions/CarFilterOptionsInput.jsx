b26da74a6f340807b2dd40007e3fb1b4db4c83f7
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-07 01:23:11 +0200

    Update CarFilterOptionsInput.jsx

--- src/components/CarFilter/CarFilterOptions/CarFilterOptionsInput.jsx
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsInput.jsx
@@ -1,12 +1,8 @@
 import { useState, useEffect } from "react";
 
-import makes from "../../../makes.json";
-
-import { CarFilterOptionsMenu } from "./CarFilterOptionsMenu";
-
 import { SVGimage } from "../../../images";
 
-const CarFilterOptions = ({}) => {
+const CarFilterOptionsInput = ({}) => {
   const [carBrands] = useState(makes);
   const [isCarBrandsOptions, setIsCarBrandsOptions] = useState(false);
   const [searchResult, setSearchResult] = useState("");
@@ -30,40 +26,25 @@ const CarFilterOptions = ({}) => {
   };
 
   return (
-    <div>
-      <h3>Car brand</h3>
-      <label htmlFor="car-filter-input" className="car-filter-input-label">
-        <input
-          id="car-filter-input"
-          type="text"
-          placeholder="Enter the text"
-          value={searchResult}
-          onChange={handleSearchResult}
-          className="car-filter-input"
-        />
-        <button
-          type="button"
-          onClick={
-            isCarBrandsOptions ? closeCarBrandOptions : openCarBrandOptions
-          }
-        >
-          {<SVGimage name={isCarBrandsOptions ? "up" : "down"} />}
-        </button>
-      </label>
-      {isCarBrandsOptions && (
-        <CarFilterOptionsMenu
-          options={
-            searchResult
-              ? carBrands.filter((carBrand) =>
-                  carBrand.toLowerCase().includes(searchResult)
-                )
-              : carBrands
-          }
-          onChange={handleCarBrand}
-        />
-      )}
-    </div>
+    <label htmlFor="car-filter-input" className="car-filter-input-label">
+      <input
+        id="car-filter-input"
+        type="text"
+        placeholder="Enter the text"
+        value={searchResult}
+        onChange={handleSearchResult}
+        className="car-filter-input"
+      />
+      <button
+        type="button"
+        onClick={
+          isCarBrandsOptions ? closeCarBrandOptions : openCarBrandOptions
+        }
+      >
+        {<SVGimage name={isCarBrandsOptions ? "up" : "down"} />}
+      </button>
+    </label>
   );
 };
 
-export { CarFilterOptions };
+export { CarFilterOptionsInput };
