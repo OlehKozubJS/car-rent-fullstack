8543022329e5307f2b6218662be64d54e2cd9b28
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-07 00:57:09 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,6 @@ import { CarFilter } from "./CarFilter";
 import { CarGallery } from "./CarGallery";
 import { CarModal } from "./CarModal";
 
-export { CarFilter, CarGallery, CarModal };
+import { carFilterOptionsStyles } from "./CarFilter";
+
+export { CarFilter, CarGallery, CarModal, carFilterOptionsStyles };
