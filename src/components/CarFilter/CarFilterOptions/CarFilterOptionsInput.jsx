8c6122f06d8bee3a59877b7588cf3dab9ba2e6ea
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-07 02:48:46 +0200

    Update CarFilterOptionsInput.jsx

--- src/components/CarFilter/CarFilterOptions/CarFilterOptionsInput.jsx
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsInput.jsx
@@ -2,13 +2,11 @@ import { useState, useEffect } from "react";
 
 import { SVGimage } from "../../../images";
 
-const CarFilterOptionsInput = ({ options }) => {
-  const [carBrands] = useState(makes);
+const CarFilterOptionsInput = ({ value, onChange, onClick }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
 
   const open = () => {
-    setSearchQuery("");
     setIsOpen(true);
   };
 
