ac607cec0ad085487769db1c755bd1c6ea34516c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-09 00:19:09 +0200

    Update carFilterOptionsStyle.js

---
+++ src/components/CarFilter/CarFilterOptions/CarFilterOptionsStyles/carFilterOptionsStyle.js
@@ -0,0 +1,17 @@
+const carFilterOptionsStyle = `        
+        .not-found-message {
+          box-sizing: border-box;
+
+          width: 224px;
+
+          border-style: solid;
+          border-radius: 14px;
+          border-width: 1px;
+          border-color: rgba(18, 20, 23, 0.05);
+
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }`;
+
+export { carFilterOptionsStyle };
