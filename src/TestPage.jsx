b9accde600db698efde1fd7e62cd628916d550a6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-14 00:46:12 +0200

    Update TestPage.jsx

--- src/TestPage.jsx
+++ src/TestPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import cars from "./advertsCars.json";
+import { cars } from "./data_base";
 
 import { CarFilter, CarGallery, CarModal } from "./components";
 
