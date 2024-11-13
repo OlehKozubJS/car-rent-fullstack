778cb84043cab13549a9c9cf1e819b90120185a5
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-14 00:57:30 +0200

    Update store.js

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { combineReducers } from "redux";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
