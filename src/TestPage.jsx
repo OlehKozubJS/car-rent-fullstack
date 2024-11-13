c39a9a081185f5613ed27635c04405865ae35a68
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-13 12:06:42 +0200

    Create TestPage.jsx

---
+++ src/TestPage.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+
+import cars from "./advertsCars.json";
+
+import { CarFilter, CarGallery, CarModal } from "./components";
+
+const App = () => {
+  const [timeOne, setTimeOne] = useState(0);
+  const [timeTwo, setTimeTwo] = useState(0);
+  const [timeThree, setTimeThree] = useState(0);
+
+  const [date, setDate] = useState(new Date());
+
+  const dateAnimationFunction = () => {
+    setDate(new Date());
+  };
+
+  const dateAnimationFunctionOne = () => {
+    setTimeOne(timeOne + 1);
+  };
+
+  const dateAnimationFunctionTwo = () => {
+    setTimeTwo(timeTwo + 1);
+  };
+
+  const dateAnimationFunctionThree = () => {
+    setTimeThree(timeThree + 1);
+  };
+
+  const handleInterval = (callback, timePeriod) => {
+    return () => {
+      let newAnimation = setInterval(callback, timePeriod);
+
+      return () => {
+        clearInterval(newAnimation);
+      };
+    };
+  };
+
+  useEffect(handleInterval(dateAnimationFunction, 1000), []);
+
+  useEffect(handleInterval(dateAnimationFunctionOne, 500), [timeOne]);
+  useEffect(handleInterval(dateAnimationFunctionTwo, 1000), [timeTwo]);
+  useEffect(handleInterval(dateAnimationFunctionThree, 2000), [timeThree]);
+
+  return (
+    <div>
+      {String(date)}
+      <p>{timeOne}</p>
+      <p>{timeTwo}</p>
+      <p>{timeThree}</p>
+    </div>
+  );
+};
+
+export { App };
