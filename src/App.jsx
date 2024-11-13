90eff21c3b03757c7af132de0311264bbb0994db
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-13 12:09:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,6 @@ import { CarRentStyles } from "./CarRentStyles";
 
 const App = () => {
   const [isModal, setIsModal] = useState(false);
-  const [timeOne, setTimeOne] = useState(0);
-  const [timeTwo, setTimeTwo] = useState(0);
-  const [timeThree, setTimeThree] = useState(0);
 
   const openModal = () => {
     setIsModal(true);
@@ -20,51 +17,13 @@ const App = () => {
     setIsModal(false);
   };
 
-  const [date, setDate] = useState(new Date());
-
-  const dateAnimationFunction = () => {
-    setDate(new Date());
-  };
-
-  const dateAnimationFunctionOne = () => {
-    setTimeOne(timeOne + 1);
-  };
-
-  const dateAnimationFunctionTwo = () => {
-    setTimeTwo(timeTwo + 1);
-  };
-
-  const dateAnimationFunctionThree = () => {
-    setTimeThree(timeThree + 1);
-  };
-
-  const handleInterval = (callback, timePeriod) => {
-    return () => {
-      let newAnimation = setInterval(callback, timePeriod);
-
-      return () => {
-        clearInterval(newAnimation);
-      };
-    };
-  };
-
-  useEffect(handleInterval(dateAnimationFunction, 1000), []);
-
-  useEffect(handleInterval(dateAnimationFunctionOne, 500), [timeOne]);
-  useEffect(handleInterval(dateAnimationFunctionTwo, 1000), [timeTwo]);
-  useEffect(handleInterval(dateAnimationFunctionThree, 2000), [timeThree]);
-
   return (
     <div>
       <CarRentStyles />
       <button onClick={openModal}>Open Modal</button>
       {isModal && <CarModal carId={9582} onClose={closeModal} />}
-      {String(date)}
       <CarGallery />
       <CarFilter />
-      <p>{timeOne}</p>
-      <p>{timeTwo}</p>
-      <p>{timeThree}</p>
     </div>
   );
 };
