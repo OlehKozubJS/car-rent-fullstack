5225482d90c0c17b8104c79609a4c5ba8589adf9
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-14 15:03:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { ReducerComponent } from "./redux";
+
 import { App } from "./App.jsx";
 
 const htmlRoot = document.querySelector("#root");
