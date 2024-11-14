5f281ab8ef8adb4db53a47eb5f22a2c42a36ae28
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-15 01:00:39 +0200

    Update CarGallery.jsx

--- src/components/CarGallery/CarGallery.jsx
+++ src/components/CarGallery/CarGallery.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import {} from "react-redux";
 
-import {} from "../../redux";
+import { selectedCars } from "../../hooks";
 
 import { CarItem } from "./CarItem";
 import { SVGimage } from "../../images";
