1de422c3069b93deadcd6376a89741e3e6787377
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-15 02:10:32 +0200

    Update CarFilter.jsx

--- src/components/CarFilter/CarFilter.jsx
+++ src/components/CarFilter/CarFilter.jsx
@@ -3,23 +3,10 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { cars, makes } from "./imports";
 
-import {
-  getCarModel,
-  getSelectedCars,
-  selectedCars,
-  setSelectedCars,
-} from "../../hooks";
-
 import { CarFilterOptions } from "./CarFilterOptions";
 import { CarMileageSelector } from "./CarMileageSelector";
 
 const CarFilter = () => {
-  const dispatch = useDispatch();
-
-  const [carModel] = useState(useSelector(getCarModel));
-
-  dispatch(setSelectedCars({ cars, carModel }));
-
   return (
     <form>
       <CarFilterOptions carBrands={makes} />
