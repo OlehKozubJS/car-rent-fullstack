7e009f4f86ea74d6d4c42261ab55601b1fa06d47
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-20 01:07:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-
 import { cars } from "./data_base";
 
-import { setSelectedCars } from "./hooks";
-
-import { CarFilter, CarGallery, CarModal } from "./components";
-
 import { TestPage } from "./TestPage";
-
-import { CarRentStyles } from "./CarRentStyles";
-import { getCarModel } from "./hooks";
 
 const App = () => {
   const [isModal, setIsModal] = useState(false);
