62babd2c88f6e37dd60ea995b05ed60a1236e302
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-20 01:01:26 +0200

    Create TestPage.jsx

---
+++ src/pages/TestPage.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+
+import { cars } from "./data_base";
+
+import { handleEventListener, handleInterval, handleTimeOut } from "./hooks";
+
+import { CarFilter, CarGallery, CarModal } from "./components";
+
+const TestPage = () => {
+  const [isTestPage, setIsTestPage] = useState(false);
+
+  const [time, setTime] = useState(0);
+
+  const [fibonacciArray, setFibonacciArray] = useState([0, 1]);
+
+  const [primeNumbersArray, setPrimeNumbersArray] = useState([2]);
+
+  const [date, setDate] = useState(new Date());
+
+  const openTestPage = () => {
+    setIsTestPage(true);
+  };
+
+  const closeTestPage = () => {
+    setIsTestPage(false);
+  };
+
+  const dateAnimationFunction = () => {
+    setDate(new Date());
+  };
+
+  const timeAnimationFunctionOne = () => {
+    setTime(time + 1);
+  };
+
+  const fibonacciTimeFunction = () => {
+    if (fibonacciArray.length === 20) {
+      return;
+    }
+    setFibonacciArray([
+      ...fibonacciArray,
+      fibonacciArray[fibonacciArray.length - 1] +
+        fibonacciArray[fibonacciArray.length - 2],
+    ]);
+  };
+
+  const primeNumbersFunction = () => {
+    if (primeNumbersArrayp[-1] < 100) {
+      //primeNumbersArray.forEach((primeNumber) => {});
+      //setPrimeNumbersArray([...primeNumbersArray, ]);
+    }
+  };
+
+  const varNameToString = (objectName) => {
+    return Object.keys(objectName)[0];
+  };
+
+  useEffect(handleInterval(dateAnimationFunction, 1000), []);
+
+  useEffect(handleInterval(timeAnimationFunctionOne, 500), [time]);
+
+  useEffect(handleTimeOut(fibonacciTimeFunction, 1000), [fibonacciArray]);
+
+  return isTestPage ? (
+    <div>
+      <button type="button" onClick={closeTestPage}>
+        Close test page
+      </button>
+      {String(date)}
+      <p>{time}</p>
+      <p>{fibonacciArray.join(", ")}</p>
+      <p>{""}</p>
+    </div>
+  ) : (
+    <button type="button" onClick={openTestPage}>
+      Open test page
+    </button>
+  );
+};
+
+export { TestPage };
