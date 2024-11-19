0f0685800df6aa9b7ae1856d74a694fbc7691fd3
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-19 23:27:15 +0200

    Update selectedCarsReducer.js

--- src/hooks/selectedCarsReducer.js
+++ src/hooks/selectedCarsReducer.js
@@ -1,23 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const selectedCarsSlice = createSlice({
+import { createNewReducer } from "./imports";
+
+const [selectedCars, setSelectedCars, getSelectedCars] = createNewReducer({
   name: "selectedCars",
   initialState: [],
-  reducers: {
-    setSelectedCars(state, action) {
-      const { cars, carMake } = action.payload;
-
-      return cars.filter((car) => car.make === carMake);
-    },
-  },
 });
-
-const selectedCars = selectedCarsSlice.reducer;
-
-const { setSelectedCars } = selectedCarsSlice.actions;
-
-const getSelectedCars = (state) => {
-  return state.selectedCars;
-};
 
 export { selectedCars, setSelectedCars, getSelectedCars };
