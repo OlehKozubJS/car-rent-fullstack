0a3aa8ea6041f89a86bcc5504b866bc9dacfd7f0
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-20 01:22:07 +0200

    Update imports.js

---
+++ src/pages/imports.js
@@ -0,0 +1,21 @@
+import { cars, makes } from "../data_base";
+import {
+  carModel,
+  setCarModel,
+  getCarModel,
+  selectedCars,
+  setSelectedCars,
+  getSelectedCars,
+  handleEventListener,
+  handleInterval,
+  handleTimeOut,
+} from "../hooks";
+import {
+  CarFilter,
+  CarGallery,
+  CarModal,
+  carFilterOptionsStyles,
+} from "../components";
+import { CarRentStyles } from "../CarRentStyles";
+
+export {};
