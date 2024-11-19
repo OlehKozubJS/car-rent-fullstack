5cb9befdfcf377ad8a383b318b72912c7b6b565a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-20 01:01:13 +0200

    Create MainPage.jsx

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { cars } from "./data_base";
+
+import { setSelectedCars } from "./hooks";
+
+import { CarFilter, CarGallery, CarModal } from "./components";
+
+import { TestPage } from "./TestPage";
+
+import { CarRentStyles } from "./CarRentStyles";
+import { getCarModel } from "./hooks";
+
+const App = () => {
+  const [isModal, setIsModal] = useState(false);
+
+  const openModal = () => {
+    setIsModal(true);
+  };
+
+  const closeModal = () => {
+    setIsModal(false);
+  };
+
+  const dispatch = useDispatch();
+
+  const carMake = useSelector(getCarModel);
+
+  useEffect(() => {
+    dispatch(setSelectedCars(cars.filter((car) => car.make === carMake)));
+  }, [carMake]);
+
+  return (
+    <div>
+      <CarRentStyles />
+      <button onClick={openModal}>Open Modal</button>
+      {isModal && <CarModal carId={9582} onClose={closeModal} />}
+      <CarFilter />
+      <CarGallery />
+      <TestPage />
+    </div>
+  );
+};
+
+export { App };
