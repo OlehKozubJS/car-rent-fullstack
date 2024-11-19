28072c3c7efc197185931dd19a4d0f4f46c6760e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-20 01:08:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,8 @@ import { cars } from "./data_base";
 import { TestPage } from "./TestPage";
 
 const App = () => {
-  const [isModal, setIsModal] = useState(false);
-
-  const openModal = () => {
-    setIsModal(true);
-  };
-
-  const closeModal = () => {
-    setIsModal(false);
-  };
-
-  const dispatch = useDispatch();
-
-  const carMake = useSelector(getCarModel);
-
-  useEffect(() => {
-    dispatch(setSelectedCars(cars.filter((car) => car.make === carMake)));
-  }, [carMake]);
-
   return (
     <div>
-      <CarRentStyles />
-      <button onClick={openModal}>Open Modal</button>
-      {isModal && <CarModal carId={9582} onClose={closeModal} />}
-      <CarFilter />
-      <CarGallery />
       <TestPage />
     </div>
   );
