4e0bf7c9b158c57b917e9537e7a5c714ce1019fc
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 23:06:51 +0200

    Create CarBrandSelectorInput.jsx

---
+++ src/components/CarFilter/CarBrandSelector/CarBrandSelectorInput.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+import { SVGimage } from "../../../images";
+
+const CarFilterOptionsInput = ({
+  inputValue,
+  onChange,
+  buttonValue,
+  onClick,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleChange = (event) => {
+    onChange(event.target.value);
+  };
+
+  const open = () => {
+    setIsOpen(true);
+  };
+
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    onClick(isOpen);
+  }, [isOpen]);
+
+  useEffect(() => {
+    setIsOpen(buttonValue);
+  }, [buttonValue]);
+
+  return (
+    <label htmlFor="car-filter-input" className="car-filter-input-label">
+      <input
+        id="car-filter-input"
+        type="text"
+        placeholder="Enter the text"
+        value={inputValue}
+        onChange={handleChange}
+        className="car-filter-input"
+      />
+      <button
+        type="button"
+        onClick={isOpen ? close : open}
+        className="car-filter-input-button"
+      >
+        {<SVGimage name={isOpen ? "up" : "down"} />}
+      </button>
+    </label>
+  );
+};
+
+export { CarFilterOptionsInput };
