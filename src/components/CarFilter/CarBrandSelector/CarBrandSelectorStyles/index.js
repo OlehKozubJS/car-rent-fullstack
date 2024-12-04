2ddfccdb6f6b23d08bbae3753e9977ad9b0a21c2
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-05 00:46:54 +0200

    Update index.js

--- src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/index.js
+++ src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/index.js
@@ -1,11 +1,11 @@
-import { carFilterOptionsStyle } from "./carBrandSelectorStyle";
+import { CarBrandSelectorNotFoundMessageStyle } from "./carBrandSelectorStyle";
 import { carFilterInputStyle } from "./carBrandSelectorInputStyle";
 import { menuContainerStyle } from "./carBrandSelectorMenuStyle";
 import { menuListStyle } from "./carBrandSelectorMenuListStyle";
 import { scrollRangeStyle } from "./scrollRangeStyle";
 
 const carBrandSelectorStyles = [
-  carFilterOptionsStyle,
+  CarBrandSelectorNotFoundMessageStyle,
   carFilterInputStyle,
   menuContainerStyle,
   menuListStyle,
