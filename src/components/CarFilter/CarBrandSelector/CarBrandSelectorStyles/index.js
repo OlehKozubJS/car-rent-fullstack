d57bd46287bc6f968d44c8c417c01d1da71148a6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 23:54:27 +0200

    Create index.js

---
+++ src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/index.js
@@ -0,0 +1,15 @@
+import { carFilterOptionsStyle } from "./carFilterOptionsStyle";
+import { carFilterInputStyle } from "./carFilterInputStyle";
+import { menuContainerStyle } from "./menuContainerStyle";
+import { menuListStyle } from "./menuListStyle";
+import { scrollRangeStyle } from "./scrollRangeStyle";
+
+const carFilterOptionsStyles = [
+  carFilterOptionsStyle,
+  carFilterInputStyle,
+  menuContainerStyle,
+  menuListStyle,
+  scrollRangeStyle,
+].join(" ");
+
+export { carFilterOptionsStyles };
