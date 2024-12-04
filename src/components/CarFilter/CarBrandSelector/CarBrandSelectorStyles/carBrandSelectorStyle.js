689345863ef7fb1da5fc50ab93519b8371146f76
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 23:53:03 +0200

    Create carBrandSelectorStyle.js

---
+++ src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/carBrandSelectorStyle.js
@@ -0,0 +1,22 @@
+const carFilterOptionsStyle = `        
+        .car-filter-options-not-found-message {
+          box-sizing: border-box;
+
+          width: 224px;
+
+          padding-block: 14px;
+          padding-inline: 18px;
+
+          border-style: solid;
+          border-radius: 14px;
+          border-width: 1px;
+          border-color: rgba(180, 20, 23, 0.05);
+
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          color: rgba(180, 20, 23, 0.5);
+        }`;
+
+export { carFilterOptionsStyle };
