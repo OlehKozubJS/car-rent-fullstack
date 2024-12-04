502889c9e7e7b266f96becab32cffab6208d9d36
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 21:15:15 +0200

    Update TestPage.jsx

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -105,6 +105,59 @@ const TestPage = () => {
       <p>{fibonacciArray.join(", ")}</p>
       <p>{primeNumbersArray.join(", ")}</p>
       <p>{squares.join(", ")}</p>
+      <form onSubmit="handleSubmit()">
+        <div>
+          <h3>Car brand (make)</h3>
+          <label for="carMakeInput">
+            <input
+              id="carMakeInput"
+              type="text"
+              placeholder="Enter car make name or its part"
+              onChange="handleMake()"
+              style="width: 200px;"
+            />
+          </label>
+        </div>
+        <div>
+          <h3>Car rental price</h3>
+          <label for="minCarRentalPriceInput">
+            <input
+              id="minCarRentalPriceInput"
+              type="number"
+              placeholder="From"
+              onChange="handleMinimalRentalPrice()"
+            />
+          </label>
+          <label for="maxCarRentalPriceInput">
+            <input
+              id="maxCarRentalPriceInput"
+              type="number"
+              placeholder="To"
+              onChange="handleMaximalRentalPrice()"
+            />
+          </label>
+        </div>
+        <div>
+          <h3>Car mileage</h3>
+          <label for="minCarMileageInput">
+            <input
+              id="minCarMileageInput"
+              type="number"
+              placeholder="From"
+              onChange="handleMinimalMileage()"
+            />
+          </label>
+          <label for="mapCarMileageInput">
+            <input
+              id="maxCarMileageInput"
+              type="number"
+              placeholder="To"
+              onChange="handleMaximalMileage()"
+            />
+          </label>
+        </div>
+        <button type="submit">Find car</button>
+      </form>
     </div>
   ) : (
     <button type="button" onClick={openTestPage}>
