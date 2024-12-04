ce78964b471790bd02f78ce4b4191b41bfd6593d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 23:45:57 +0200

    Create carBrandSelectorMenuListStylee.js

---
+++ src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/carBrandSelectorMenuListStylee.js
@@ -0,0 +1,33 @@
+const menuListStyle = `        
+        .menu-list-container {
+          height: 244px;
+          width: 190px;
+
+          overflow: hidden;
+          position: relative;
+        }
+
+        .menu-list {
+          list-style: none;
+          margin: 0;
+          padding: 0;
+
+          position: absolute;
+          left: 0px;          
+        }
+
+        .menu-item {
+          color: rgba(18, 20, 23, 0.2);
+          
+          transition: 500ms;
+        }
+
+        .menu-item:hover {
+          color: rgb(18, 20, 23);
+        }
+
+        .menu-option-radio {
+          display: none;
+        }`;
+
+export { menuListStyle };
