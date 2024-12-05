b1305e8859977c13f60e76870a2be34ec98b6012
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-05 03:36:10 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -1,3 +1,11 @@
 import { CarRentGlobalStyle } from "./CarRentGlobalStyle";
 
+import {
+  ManropeR,
+  ManropeM,
+  ManropeB,
+  MontserratR,
+  MontserratB,
+} from "./fonts";
+
 export { CarRentGlobalStyle };
