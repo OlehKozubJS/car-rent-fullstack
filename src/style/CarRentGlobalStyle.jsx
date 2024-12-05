62a9f15ba8c12097b025f261de5d70d29e2d8332
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-05 06:49:18 +0200

    Update CarRentGlobalStyle.jsx

--- src/style/CarRentGlobalStyle.jsx
+++ src/style/CarRentGlobalStyle.jsx
@@ -4,41 +4,39 @@ const CarRentGlobalStyle = () => {
   return (
     <Global
       styles={css`
-        body {
-          @font-face {
-            font-family: "Manrope-Regular";
-            src: local("Manrope-Regular"),
-              url("./fonts/Manrope-Regular.ttf") format("truetype");
-            font-weight: 400;
-          }
+        @font-face {
+          font-family: "Manrope-Regular";
+          src: local("Manrope-Regular"),
+            url("./fonts/Manrope-Regular.ttf") format("truetype");
+          font-weight: 400;
+        }
 
-          @font-face {
-            font-family: "Manrope-Medium";
-            src: local("Manrope-Medium"),
-              url("./fonts/Manrope-Medium.ttf") format("truetype");
-            font-weight: 500;
-          }
+        @font-face {
+          font-family: "Manrope-Medium";
+          src: local("Manrope-Medium"),
+            url("./fonts/Manrope-Medium.ttf") format("truetype");
+          font-weight: 500;
+        }
 
-          @font-face {
-            font-family: "Manrope-SemiBold";
-            src: local("Manrope-SemiBold"),
-              url("./fonts/Manrope-SemiBold.ttf") format("truetype");
-            font-weight: 600;
-          }
+        @font-face {
+          font-family: "Manrope-SemiBold";
+          src: local("Manrope-SemiBold"),
+            url("./fonts/Manrope-SemiBold.ttf") format("truetype");
+          font-weight: 600;
+        }
 
-          @font-face {
-            font-family: "Montserrat-Regular";
-            src: local("Montserrat-Regular"),
-              url("./fonts/Montserrat-Regular.ttf") format("truetype");
-            font-weight: 400;
-          }
+        @font-face {
+          font-family: "Montserrat-Regular";
+          src: local("Montserrat-Regular"),
+            url("./fonts/Montserrat-Regular.ttf") format("truetype");
+          font-weight: 400;
+        }
 
-          @font-face {
-            font-family: "Montserrat-SemiBold";
-            src: local("Montserrat-SemiBold"),
-              url("./fonts/Montserrat-SemiBold.ttf") format("truetype");
-            font-weight: 600;
-          }
+        @font-face {
+          font-family: "Montserrat-SemiBold";
+          src: local("Montserrat-SemiBold"),
+            url("./fonts/Montserrat-SemiBold.ttf") format("truetype");
+          font-weight: 600;
         }
       `}
     />
