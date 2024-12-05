4390a4bb6668064b4cd1909c3da4b4432dd76cf6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-05 05:28:08 +0200

    Update CarModal.jsx

--- src/components/CarModal/CarModal.jsx
+++ src/components/CarModal/CarModal.jsx
@@ -83,31 +83,31 @@ const CarModal = ({ carId, onClose }) => {
             Id:{" "}
             <CarModalItemValueStyleBase>{car.id}</CarModalItemValueStyleBase>
           </CarModalItemStyleBase>
-          <CarModalItemStyleBase className="{ManropeR}">
+          <CarModalItemStyleBase className={ManropeR}>
             Year:{" "}
             <CarModalItemValueStyleBase>{car.year}</CarModalItemValueStyleBase>
           </CarModalItemStyleBase>
-          <CarModalItemStyleBase className="{ManropeR}">
+          <CarModalItemStyleBase className={ManropeR}>
             Type:{" "}
             <CarModalItemValueStyleBase>{car.type}</CarModalItemValueStyleBase>
           </CarModalItemStyleBase>
-          <CarModalItemStyleBase className="{ManropeR}">
+          <CarModalItemStyleBase className={ManropeR}>
             Fuel Consumption:{" "}
             <CarModalItemValueStyleBase>
               {car.fuelConsumption}
             </CarModalItemValueStyleBase>
           </CarModalItemStyleBase>
-          <CarModalItemStyleBase className="{ManropeR}">
+          <CarModalItemStyleBase className={ManropeR}>
             Engine Size:{" "}
             <CarModalItemValueStyleBase>
               {car.engineSize}
             </CarModalItemValueStyleBase>
           </CarModalItemStyleBase>
         </CarModalItemsStyleBase>
-        <CarModalDescriptionStyleBase className="{ManropeR}">
+        <CarModalDescriptionStyleBase className={ManropeR}>
           {car.description}
         </CarModalDescriptionStyleBase>
-        <CarModalSubHeaderStyleBase className="{ManropeM}">
+        <CarModalSubHeaderStyleBase className={ManropeM}>
           Accessories and functionalities:
         </CarModalSubHeaderStyleBase>
         <CarModalItemsStyleBase>
