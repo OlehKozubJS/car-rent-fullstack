0f5ac588063065e6b87183a965e9d08f1a73a647
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-05 02:47:08 +0200

    Update CarBrandSelectorMenuList.jsx

--- src/components/CarFilter/CarBrandSelector/CarBrandSelectorMenuList.jsx
+++ src/components/CarFilter/CarBrandSelector/CarBrandSelectorMenuList.jsx
@@ -1,24 +1,26 @@
 const CarBrandSelectorMenuList = ({ options, onChange, top }) => {
   return (
-    <ul className="menu-list" style={{ top: `${top}px` }}>
-      {options.map((option, optionIndex) => {
-        return (
-          <li key={optionIndex} className="menu-item">
-            <label htmlFor={optionIndex}>
-              <input
-                id={optionIndex}
-                type="radio"
-                name="menu-option-radio"
-                value={option}
-                onChange={onChange}
-                className="menu-option-radio"
-              />
-              <span>{option}</span>
-            </label>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="menu-list-container">
+      <ul className="menu-list" style={{ top: `${top}px` }}>
+        {options.map((option, optionIndex) => {
+          return (
+            <li key={optionIndex} className="menu-item">
+              <label htmlFor={optionIndex}>
+                <input
+                  id={optionIndex}
+                  type="radio"
+                  name="menu-option-radio"
+                  value={option}
+                  onChange={onChange}
+                  className="menu-option-radio"
+                />
+                <span>{option}</span>
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
