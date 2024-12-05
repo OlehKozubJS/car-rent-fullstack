eb2d32f0bcaddf1bc613102d6da114eef151c392
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-05 03:05:40 +0200

    Update CarRentStyles.jsx

--- src/CarRentStyles.jsx
+++ src/CarRentStyles.jsx
@@ -1,13 +1,7 @@
 import { Global, css } from "@emotion/react";
 
 const CarRentStyles = () => {
-  return (
-    <Global
-      styles={css`
-        ${""}
-      `}
-    />
-  );
+  return <Global styles={css``} />;
 };
 
 export { CarRentStyles };
