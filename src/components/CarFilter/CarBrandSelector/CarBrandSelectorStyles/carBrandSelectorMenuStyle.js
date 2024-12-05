26f2851f9797eb510a3ea3c8d755e9fb2a6f71d6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-05 02:57:46 +0200

    Update carBrandSelectorMenuStyle.js

--- src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/carBrandSelectorMenuStyle.js
+++ src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/carBrandSelectorMenuStyle.js
@@ -1,21 +1,24 @@
-const menuContainerStyle = `        
-        .menu-container {
-          box-sizing: border-box;
+import styled from "@emotion/styled";
 
-          width: 224px;
+const CarBrandSelectorMenuContainerStyleBase = styled.div`
+  .menu-container {
+    box-sizing: border-box;
 
-          padding-block: 14px;
-          padding-left: 18px;
-          padding-right: 8px;
+    width: 224px;
 
-          border-style: solid;
-          border-radius: 14px;
-          border-width: 1px;
-          border-color: rgba(18, 20, 23, 0.05);
+    padding-block: 14px;
+    padding-left: 18px;
+    padding-right: 8px;
 
-          display: flex;
-          justify-content: space-between;
-          align-items: flex-start;
-        }`;
+    border-style: solid;
+    border-radius: 14px;
+    border-width: 1px;
+    border-color: rgba(18, 20, 23, 0.05);
 
-export { menuContainerStyle };
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+`;
+
+export { CarBrandSelectorMenuContainerStyleBase };
