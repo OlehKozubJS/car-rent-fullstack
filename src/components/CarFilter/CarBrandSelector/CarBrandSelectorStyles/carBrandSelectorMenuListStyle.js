6c53dcc1afda6d4eb65d40ae358302b5bebf755b
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-05 02:10:41 +0200

    Update carBrandSelectorMenuListStyle.js

--- src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/carBrandSelectorMenuListStyle.js
+++ src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/carBrandSelectorMenuListStyle.js
@@ -1,35 +1,33 @@
 import styled from "@emotion/styled";
 
-const menuListStyle = `        
-        .menu-list-container {
+const      
+        menu-list-container `
           height: 244px;
           width: 190px;
 
           overflow: hidden;
           position: relative;
-        }
+        `
 
-        .menu-list {
+const        menu-list = styled.`
           list-style: none;
           margin: 0;
           padding: 0;
 
           position: absolute;
           left: 0px;          
-        }
+        `
 
-        .menu-item {
+const        menu-item = styled.`
           color: rgba(18, 20, 23, 0.2);
           
           transition: 500ms;
-        }
-
-        .menu-item:hover {
+                &:hover 
           color: rgb(18, 20, 23);
-        }
+        }`
 
-        .menu-option-radio {
+const        menu-option-radio styled.`
           display: none;
-        }`;
+        `;
 
-export { menuListStyle };
+export {  };
