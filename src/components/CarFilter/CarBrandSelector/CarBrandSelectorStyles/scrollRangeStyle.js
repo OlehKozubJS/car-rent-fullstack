56e84888a150b9f79b05505eda2caef4c58e936a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-05 03:18:31 +0200

    Update scrollRangeStyle.js

--- src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/scrollRangeStyle.js
+++ src/components/CarFilter/CarBrandSelector/CarBrandSelectorStyles/scrollRangeStyle.js
@@ -5,26 +5,26 @@ const scrollRangeStyle = `
           width: 8px;
 
           position: relative;
-        }
-
-        .scroll-range-rotor {
-          position: absolute;
-          width: 8px;
-          right: 0px;
-
-          background-color: rgba(18, 20, 23, 0.05);
-
-          border-radius: 10px;
-
-          transition: 500ms;
-        }
-          
-        .scroll-range-rotor:hover {
-          background-color: rgba(18, 20, 23, 0.5);
-        }
-
-        .scroll-range-rotor:active {
-          background-color: rgba(18, 20, 23, 0.25);
         }`;
+
+const ScrollRangeRotorStyleBase = styled.div`
+  position: absolute;
+  width: 8px;
+  right: 0px;
+
+  background-color: rgba(18, 20, 23, 0.05);
+
+  border-radius: 10px;
+
+  transition: 500ms;
+
+  &:hover {
+    background-color: rgba(18, 20, 23, 0.5);
+  }
+
+  &:active {
+    background-color: rgba(18, 20, 23, 0.25);
+  }
+`;
 
 export { scrollRangeStyle };
