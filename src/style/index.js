4c634002179be154e7dc7e90360360936c443a4a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-06 23:40:03 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -24,7 +24,7 @@ injectGlobal`
 
   body {
     font-family: "Ojuju", sans-serif;
-    background-image: radial-gradient(at center, red, orange, yellow, green, blue);
+    background-image: radial-gradient(at center, red, orange, yellow, green, cyan, blue);
   }
 `;
 
