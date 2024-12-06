77c56869c18ce29b357246276ac0c100632dfc7e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-06 23:32:09 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -15,6 +15,7 @@ injectGlobal`
     font-family: "Ojuju";
 
     src: local("Ojuju"), url("./Ojuju.eot") format(embedded-opentype"),
+      url("./Ojuju.eot") format(embedded-opentype"),
 
     unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f,
       U+A720-A7FF;
