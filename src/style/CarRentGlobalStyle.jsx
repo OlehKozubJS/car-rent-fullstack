8346045fc933b0f3fe232d70a5dcea11bd7e8546
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-06 18:45:54 +0200

    Update CarRentGlobalStyle.jsx

--- src/style/CarRentGlobalStyle.jsx
+++ src/style/CarRentGlobalStyle.jsx
@@ -7,7 +7,7 @@ injectGlobal`
 
     src: 
       local('Kings-Regular'),
-      url('./Kings-Regular.ttf') format('truetype');
+      url('/Kings-Regular.ttf') format('truetype');
         
     unicode-range: 
       U+0100-024f, U+1-1eff, U+20a0-20ab,
