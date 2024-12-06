926c9d0c0826918ede188d9918861117cb07a56c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-06 22:49:05 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -4,8 +4,8 @@ injectGlobal`
   @font-face {
     font-family: "Kings-Regular";
 
-    src: local("Kings-Regular"), url("./Kings-Regular.eot") format("embedded-opentype");
-    src: url("./Kings-Regular.eot?#iefix") format("embedded-opentype");
+    src: local("Kings-Regular"), url("./Kings-Regular.eot") format("embedded-opentype"),
+      url("./Kings-Regular.eot?#iefix") format("embedded-opentype");
 
     unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f,
       U+A720-A7FF;
