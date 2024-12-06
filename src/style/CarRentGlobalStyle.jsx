f10990ac3ba919eb05fedb4d0db09045ca61c132
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-06 21:50:04 +0200

    Update CarRentGlobalStyle.jsx

--- src/style/CarRentGlobalStyle.jsx
+++ src/style/CarRentGlobalStyle.jsx
@@ -25,19 +25,7 @@ injectGlobal`
 `;
 
 /*
-  src: url('../fonts/Kings-Regular.eot?#iefix') format('embedded-opentype');
 
-  @font-face {
-    font-family: "Kings-Regular";
-    src: 
-      local("Kings-Regular),
-      url('https://fonts.googleapis.com/css2?family=Kings&display=swap')
-        format('truetype');
-    font-weight: 400;
-    unicode-range: 
-      U+0100-024f, U+1-1eff, U+20a0-20ab,
-      U+20ad-20cf, U+2c60-2c7f, U+A720-A7FF;
-  }
 
   @font-face {
     font-family: Manrope-Regular;
