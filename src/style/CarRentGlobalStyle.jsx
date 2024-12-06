3ca341dc4d5217b7107dc01fae21d2fa8cb207ec
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-06 18:33:46 +0200

    Update CarRentGlobalStyle.jsx

--- src/style/CarRentGlobalStyle.jsx
+++ src/style/CarRentGlobalStyle.jsx
@@ -3,12 +3,11 @@ import { injectGlobal } from "@emotion/css";
 
 injectGlobal`
   @font-face {
-    font-family: "Kings-Regular";
-    font-weight: 400;
+    font-family: 'Kings-Regular';
 
     src: 
-      local("Kings-Regular"),
-      url("Kings-Regular.woff2") format("woff2");
+      local('Kings-Regular'),
+      url('Kings-Regular.woff2') format('woff2');
         
     unicode-range: 
       U+0100-024f, U+1-1eff, U+20a0-20ab,
