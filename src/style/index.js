81782328e235ef4b58bc2ed7a9741f3cb931e446
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-06 22:31:32 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -1,16 +1,19 @@
 import { injectGlobal } from "@emotion/css";
 
 injectGlobal`
-@font-face {
-  font-family: "Kings-Regular";
+  @font-face {
+    font-family: "Kings-Regular";
 
-  src: local("Kings-Regular"), url("../Kings-Regular.ttf") format("truetype"),
-    url("../Kings-Regular.eot") format("eot");
+    src: local("Kings-Regular"), url("../Kings-Regular.ttf") format("truetype"),
+      url("../Kings-Regular.eot") format("eot");
 
-  unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f,
-    U+A720-A7FF;
-}
+    unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f,
+      U+A720-A7FF;
+  }
 
+  body {
+    font-family: "Kings-Regular", cursive;
+  }
 `;
 
 import {
