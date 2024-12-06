38b2a3b6125bbc6d637d70a7b2913f8966e7757c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-06 23:10:09 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -14,7 +14,7 @@ injectGlobal`
   @font-face {
     font-family: "Ojuju-VariableFont_wght";
 
-    src: local("Ojuju-VariableFont_wght"), url("./Ojuju-VariableFont_wght.ttf") format("truetype"),
+    src: local("Ojuju-VariableFont_wght"), url("./Ojuju-VariableFont_wght.eot") format(embedded-opentype"),
 
     unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f,
       U+A720-A7FF;
