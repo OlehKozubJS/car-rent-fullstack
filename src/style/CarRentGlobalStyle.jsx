65fb0423fec029f83b47809f23dfb40c05a70467
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-06 21:50:43 +0200

    Update CarRentGlobalStyle.jsx

--- src/style/CarRentGlobalStyle.jsx
+++ src/style/CarRentGlobalStyle.jsx
@@ -3,6 +3,17 @@ import { injectGlobal } from "@emotion/css";
 const CarRentGlobalStyleBase = createGlobalStyle``;
 
 injectGlobal`
+  * {
+    font-family: 'MonsieurLaDoulaise-Regular', cursive;
+  } 
+
+  body {
+    background-color: red;
+    background-image: linear-gradient(to right, red, green, blue);
+  }
+`;
+
+/*
   @font-face {
     font-family: 'MonsieurLaDoulaise-Regular';
 
@@ -14,18 +25,6 @@ injectGlobal`
       U+0100-024f, U+1-1eff, U+20a0-20ab,
       U+20ad-20cf, U+2c60-2c7f, U+A720-A7FF;
   }
-
-  * {
-    font-family: 'MonsieurLaDoulaise-Regular', cursive;
-  } 
-
-  body {
-    background-image: linear-gradient(to right, red, green, blue);
-  }
-`;
-
-/*
-
 
   @font-face {
     font-family: Manrope-Regular;
