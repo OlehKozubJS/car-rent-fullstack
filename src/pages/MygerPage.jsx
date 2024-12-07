fa7cff0a3d94a029f3324f54dfd1989183e1e4bb
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-07 22:01:28 +0200

    Create MygerPage.jsx

---
+++ src/pages/MygerPage.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+import { carsLocalData } from "./imports";
+
+const MygerPage = () => {
+  const [isTestPage, setIsTestPage] = useState(false);
+
+  const openMygerPage = () => {
+    setIsMygerPage(true);
+  };
+
+  const closeMygerPage = () => {
+    setIsMygerPage(false);
+  };
+
+  //onSubmit({ make, minRentalPrice, maxRentalPrice, minMileage, maxMileage });
+
+  return isTestPage ? (
+    <div>
+      <button type="button" onClick={closeMygerPage}>
+        Close myger page
+      </button>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+  ) : (
+    <button type="button" onClick={openMygerPage}>
+      Open myger page
+    </button>
+  );
+};
+
+export { TestPage };
