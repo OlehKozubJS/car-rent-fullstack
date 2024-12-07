3d252054edaad84244363bf54192f7bcb6bd4d4e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-07 21:51:03 +0200

    Update Myger.jsx

--- src/pages/Myger.jsx
+++ src/pages/Myger.jsx
@@ -5,14 +5,6 @@ import { carsLocalData } from "./imports";
 const TestPage = () => {
   const [isTestPage, setIsTestPage] = useState(false);
 
-  const [time, setTime] = useState(0);
-
-  const [fibonacciArray, setFibonacciArray] = useState([0, 1]);
-
-  const [primeNumbersArray, setPrimeNumbersArray] = useState([2]);
-
-  const [date, setDate] = useState(new Date());
-
   const openTestPage = () => {
     setIsTestPage(true);
   };
@@ -125,67 +117,10 @@ const TestPage = () => {
 
   return isTestPage ? (
     <div>
-      <button type="button" onClick={closeTestPage}>
-        Close test page
-      </button>
-      {String(date)}
-      <p>{time}</p>
-      <p>{fibonacciArray.join(", ")}</p>
-      <p>{primeNumbersArray.join(", ")}</p>
-      <p>{squares.join(", ")}</p>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <h3>Car brand (make)</h3>
-          <label htmlFor="carMakeInput">
-            <input
-              id="carMakeInput"
-              type="text"
-              placeholder="Enter car make name or its part"
-              onChange={handleMake}
-              style={{ width: "200px" }}
-            />
-          </label>
-        </div>
-        <div>
-          <h3>Car rental price</h3>
-          <label htmlFor="minCarRentalPriceInput">
-            <input
-              id="minCarRentalPriceInput"
-              type="number"
-              placeholder="From"
-              onChange={handleMinimalRentalPrice}
-            />
-          </label>
-          <label htmlFor="maxCarRentalPriceInput">
-            <input
-              id="maxCarRentalPriceInput"
-              type="number"
-              placeholder="To"
-              onChange={handleMaximalRentalPrice}
-            />
-          </label>
-        </div>
-        <div>
-          <h3>Car mileage</h3>
-          <label htmlFor="minCarMileageInput">
-            <input
-              id="minCarMileageInput"
-              type="number"
-              placeholder="From"
-              onChange={handleMinimalMileage}
-            />
-          </label>
-          <label htmlFor="maxCarMileageInput">
-            <input
-              id="maxCarMileageInput"
-              type="number"
-              placeholder="To"
-              onChange={handleMaximalMileage}
-            />
-          </label>
-        </div>
-        <button type="submit">Find car</button>
-      </form>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
     </div>
   ) : (
     <button type="button" onClick={openTestPage}>
