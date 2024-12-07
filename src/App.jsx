b4f64c6b590e18a9d6d730dcbc92e7c151eaebc5
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-07 22:39:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,38 @@ const App = () => {
     "MygerPage",
   ]);
 
+  const handlePageName = (event) => {
+    setPageName(event.target.value);
+  };
+
   return (
     <section>
       <label htmlFor="MainPageRadio">
-        <input id="MainPageRadio" name="pageSwitcher" type="radio" onchange={} value="MainPage"/>
+        <input
+          id="MainPageRadio"
+          name="pageSwitcher"
+          type="radio"
+          onchange={handlePageName}
+          value="MainPage"
+        />
       </label>
       <label htmlFor="TestPageRadio">
-        <input id="TestPageRadio" name="pageSwitcher" type="radio" onChange={} value="TestPage"/>
+        <input
+          id="TestPageRadio"
+          name="pageSwitcher"
+          type="radio"
+          onChange={handlePageName}
+          value="TestPage"
+        />
       </label>
       <label htmlFor="MygerPageRadio">
-        <input id="MygerPageRadio" name="pageSwitcher" type="radio" onChange={} value="MygerPage" />
+        <input
+          id="MygerPageRadio"
+          name="pageSwitcher"
+          type="radio"
+          onChange={handlePageName}
+          value="MygerPage"
+        />
       </label>
 
       {pageName === "MainPage" && <MainPage />}
