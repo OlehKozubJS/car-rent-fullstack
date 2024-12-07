512a785825d91597c18770b9a655de6ef6348e37
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-07 22:15:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { eseEffect, useState } from "react";
 import { MainPage, TestPage, MygerPage } from "./pages";
 
 const App = () => {
+  const [] = useState();
+
   return (
     <div>
       {<MainPage />}
