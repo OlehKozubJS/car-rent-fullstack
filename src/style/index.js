2d45df8e447943e1ea1a36bfcfa58100cb803df0
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 19:37:23 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -5,7 +5,7 @@ injectGlobal`
     font-family: "Kings-Regular";
 
     src: local("Kings-Regular"),
-      url("./fonts/Kings-Regular.ttf") format("truetype"),
+      url(./fonts/Kings-Regular.ttf) format("truetype"),
       url(./fonts/Kings-Regular.eot?#iefix) format("embedded-opentype");
 
     unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f,
