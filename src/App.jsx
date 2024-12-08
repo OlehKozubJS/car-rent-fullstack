8a45b4aafe052b0bd32163c0f4171263bcb2b0fd
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 19:14:47 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
   };
 
   return (
-    <div style={{ fontFamily: "Kings-regular" }}>
+    <div style={{ fontFamily: "Manrope-Regular" }}>
       <header>
         <nav>
           <ul>
