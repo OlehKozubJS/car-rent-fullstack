f2c75aa3af50a29f142c01fff8afe43f3103da8e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 20:07:33 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -9,9 +9,9 @@ injectGlobal`
     unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f, U+A720-A7FF;
   }
 
-    @font-face {
-    font-family: "Kings-Regular";
-    src: local("Kings-Regular"), url(${KingsRegular}) format("woff2");
+  @font-face {
+    font-family: "Ojuju";
+    src: local("Ojuju"), url(${KingsRegular}) format("woff2");
     unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f, U+A720-A7FF;
   }
 `;
