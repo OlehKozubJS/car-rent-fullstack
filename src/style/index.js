b39d2db71236922fe2202369b6ae60d4fe67f0fa
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 20:17:34 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -18,5 +18,3 @@ injectGlobal`
 import commonFonts from "./CarRentGlobalStyle.module.css";
 
 export default commonFonts;
-
-//     unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f, U+A720-A7FF;
