d5380a8a9de137a744233480fc9a74ba8bd83966
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 20:14:59 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -7,7 +7,6 @@ injectGlobal`
   @font-face {
     font-family: "Kings-Regular";
     src: url(${KingsRegular}) format("woff");
-    unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f, U+A720-A7FF;
   }
 
   @font-face {
