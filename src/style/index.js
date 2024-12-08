7fc98d09ee6644f91ba534678d308b663ee9953c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 19:52:16 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -1,19 +1,19 @@
 import { injectGlobal } from "@emotion/css";
 
-import kr from "./fonts/Kings-Regular.woff";
+import KingsRegular from "./fonts/Kings-Regular.woff";
 
 injectGlobal`
   @font-face {
     font-family: "Kings-Regular";
 
-    src: local("Kings-Regular"),
-      url(${kr}) format("woff");
+    src: url(${KingsRegular});
 
-    unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f,
-      U+A720-A7FF;
+
   }
 `;
 
 import commonFonts from "./CarRentGlobalStyle.module.css";
 
 export default commonFonts;
+
+//     unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f, U+A720-A7FF;
