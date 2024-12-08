ad2a806e865bac9133617b9430e900e4c3f22409
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 20:13:45 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -1,12 +1,12 @@
 import { injectGlobal } from "@emotion/css";
 
-import KingsRegular from "./fonts/Kings-Regular.woff2";
+import KingsRegular from "./fonts/Kings-Regular.woff";
 import Ojuju from "./fonts/Ojuju.ttf";
 
 injectGlobal`
   @font-face {
     font-family: "Kings-Regular";
-    src: local("Kings-Regular"), url(${KingsRegular}) format(woff2);
+    src: local("Kings-Regular"), url(${KingsRegular}) format(woff);
     unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f, U+A720-A7FF;
   }
 
