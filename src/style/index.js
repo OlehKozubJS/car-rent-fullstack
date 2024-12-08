c571b3fb867d52b715b1ba36e807e06eb104161b
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 20:07:02 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -6,6 +6,13 @@ injectGlobal`
   @font-face {
     font-family: "Kings-Regular";
     src: local("Kings-Regular"), url(${KingsRegular}) format("woff2");
+    unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f, U+A720-A7FF;
+  }
+
+    @font-face {
+    font-family: "Kings-Regular";
+    src: local("Kings-Regular"), url(${KingsRegular}) format("woff2");
+    unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f, U+A720-A7FF;
   }
 `;
 
