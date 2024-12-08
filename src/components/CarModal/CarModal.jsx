5999a996e94986aa3c4b169d3264bd62b3642364
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 18:55:02 +0200

    Update CarModal.jsx

--- src/components/CarModal/CarModal.jsx
+++ src/components/CarModal/CarModal.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { carsLocalData, ManropeR } from "./imports";
+import { carsLocalData } from "./imports";
 
 import { SVGimage } from "../../images";
 
