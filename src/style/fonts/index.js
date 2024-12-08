add9ea564ada6dd8aec29576ec6874f3800ac544
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 19:05:55 +0200

    Delete index.js

--- src/style/fonts/index.js
+++
@@ -1,9 +0,0 @@
-import commonFonts from "./Fonts.module.css";
-
-const ManropeR = commonFonts.ManropeR;
-const ManropeM = commonFonts.ManropeM;
-const ManropeB = commonFonts.ManropeB;
-const MontserratR = commonFonts.MontserratR;
-const MontserratB = commonFonts.MontserratB;
-
-export { ManropeR, ManropeM, ManropeB, MontserratR, MontserratB, commonFonts };
