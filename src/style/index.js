a7380253f1fedae60474bb9fef035f0b39672a60
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 19:50:36 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -1,11 +1,13 @@
 import { injectGlobal } from "@emotion/css";
 
+import kr from "./fonts/Kings-Regular.woff";
+
 injectGlobal`
   @font-face {
     font-family: "Kings-Regular";
 
     src: local("Kings-Regular"),
-      url("./fonts/Kings-Regular.woff2") format("woff2");
+      url(${kr}) format("woff");
 
     unicode-range: U+0100-024f, U+1-1eff, U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f,
       U+A720-A7FF;
