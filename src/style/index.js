5683697f0f76c73d2e91517f9313a250c9e9922a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-08 21:13:38 +0200

    Update index.js

--- src/style/index.js
+++ src/style/index.js
@@ -3,6 +3,7 @@ import { injectGlobal } from "@emotion/css";
 import KingsRegular from "./fonts/Kings-Regular.ttf";
 import Ojuju from "./fonts/Ojuju.ttf";
 import ManropeRegular from "./fonts/Manrope-Regular.ttf";
+import MysteryQuest from "./fonts/MysteryQuest.ttf";
 
 injectGlobal`
   @font-face {
