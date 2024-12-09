03077da870564df693030efd2f0e4bd0b9788587
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-09 19:47:41 +0200

    Update index.js

--- src/style/fonts/index.js
+++ src/style/fonts/index.js
@@ -6,26 +6,4 @@ import ManropeRegular from "./fonts/Manrope-Regular.ttf";
 import ManropeMedium from "./fonts/Manrope-Medium.ttf";
 import MysteryQuest from "./fonts/MysteryQuest-Regular.ttf";
 
-injectGlobal`
-  @font-face {
-    font-family: "Kings-Regular";
-    src: url(${KingsRegular});
-  }
-
-  @font-face {
-    font-family: "Ojuju";
-    src: url(${Ojuju});
-  }
-
-  @font-face {
-    font-family: "Manrope-Regular";
-    src: url(${ManropeRegular});
-  }
-
-  @font-face {
-    font-family: "MysteryQuest";
-    src: url(${MysteryQuest});
-  }
-`;
-
 export {};
