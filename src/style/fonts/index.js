c310606b2dfaf0b60e6bf9c3f646c43935fb77da
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-09 19:59:03 +0200

    Update index.js

--- src/style/fonts/index.js
+++ src/style/fonts/index.js
@@ -1,8 +1,11 @@
-import KingsRegular from "./Kings-Regular.ttf";
-import Ojuju from "./Ojuju.ttf";
 import ManropeRegular from "./Manrope-Regular.ttf";
 import ManropeMedium from "./Manrope-Medium.ttf";
 import ManropeSemiBold from "./Manrope-SemiBold.ttf";
+import MontserratRegular from "./Montserrat-Regular.ttf";
+import MontserratSemiBold from "./Manrope-SemiBold.ttf";
+
+import KingsRegular from "./Kings-Regular.ttf";
+import Ojuju from "./Ojuju.ttf";
 import MysteryQuest from "./MysteryQuest-Regular.ttf";
 
 export {};
