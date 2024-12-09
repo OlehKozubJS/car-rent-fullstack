3f8d99e2e7bf043aed39a9eda95c322dda0a7636
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-09 19:57:42 +0200

    Update index.js

--- src/style/fonts/index.js
+++ src/style/fonts/index.js
@@ -2,7 +2,7 @@ import KingsRegular from "./Kings-Regular.ttf";
 import Ojuju from "./Ojuju.ttf";
 import ManropeRegular from "./Manrope-Regular.ttf";
 import ManropeMedium from "./Manrope-Medium.ttf";
-import from "";
+import ManropeSemiBold from "./Manrope-SemiBold.ttf";
 import MysteryQuest from "./MysteryQuest-Regular.ttf";
 
 export {};
