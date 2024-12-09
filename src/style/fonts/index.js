d3aa1ad93629df6613fb8d848fd6c094513cbf9a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-09 19:47:14 +0200

    Create index.js

---
+++ src/style/fonts/index.js
@@ -0,0 +1,29 @@
+import { injectGlobal } from "@emotion/css";
+
+import KingsRegular from "./fonts/Kings-Regular.ttf";
+import Ojuju from "./fonts/Ojuju.ttf";
+import ManropeRegular from "./fonts/Manrope-Regular.ttf";
+import ManropeMedium from "./fonts/Manrope-Medium.ttf";
+import MysteryQuest from "./fonts/MysteryQuest-Regular.ttf";
+
+injectGlobal`
+  @font-face {
+    font-family: "Kings-Regular";
+    src: url(${KingsRegular});
+  }
+
+  @font-face {
+    font-family: "Ojuju";
+    src: url(${Ojuju});
+  }
+
+  @font-face {
+    font-family: "Manrope-Regular";
+    src: url(${ManropeRegular});
+  }
+
+  @font-face {
+    font-family: "MysteryQuest";
+    src: url(${MysteryQuest});
+  }
+`;
