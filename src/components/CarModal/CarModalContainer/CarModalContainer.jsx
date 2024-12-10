9a67e42d90c53fd5d60ad16c7742671455925288
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-10 21:26:40 +0200

    Update CarModalContainer.jsx

--- src/components/CarModal/CarModalContainer/CarModalContainer.jsx
+++ src/components/CarModal/CarModalContainer/CarModalContainer.jsx
@@ -1,46 +1,14 @@
-import { CarModalCloseButton } from "./CarModalCloseButton";
-import { CarModalHeader } from "./CarModalHeader";
-import { CarModalCommonInformation } from "./CarModalCommonInformation";
-import { CarModalDescription } from "./CarModalDescription";
-import { CarModalAccessoriesInformation } from "./CarModalAccessoriesInformation";
-import { CarModalRentalInformation } from "./CarModalRentalInformation";
-import { CarModalLink } from "./CarModalLink";
-
 import {
   CarModalBackdropStyleBase,
   CarModalContainerStyleBase,
-  CarModalImageStyleBase,
-} from "./CarModalStyles";
+} from "./CarModalContainerStyles";
 
-const CarModal = ({ car, onClose }) => {
+const CarModalContainer = ({ children }) => {
   return (
     <CarModalBackdropStyleBase>
-      <CarModalContainerStyleBase>
-        <CarModalCloseButton onClose={onClose} />
-        <CarModalImageStyleBase src={car.img} alt="" />
-        <CarModalHeader make={car.make} model={car.model} year={car.year} />
-        <CarModalCommonInformation
-          address={car.address}
-          id={car.id}
-          year={car.year}
-          type={car.type}
-          fuelConsumption={car.fuelConsumption}
-          engineSize={car.engineSize}
-        />
-        <CarModalDescription description={car.description} />
-        <CarModalAccessoriesInformation
-          accessories={car.accessories}
-          functionalities={car.functionalities}
-        />
-        <CarModalRentalInformation
-          rentalConditions={car.rentalConditions}
-          mileage={car.mileage}
-          rentalPrice={car.rentalPrice}
-        />
-        <CarModalLink />
-      </CarModalContainerStyleBase>
+      <CarModalContainerStyleBase>{children}</CarModalContainerStyleBase>
     </CarModalBackdropStyleBase>
   );
 };
 
-export { CarModal };
+export { CarModalContainer };
