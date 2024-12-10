5d3e3e365d3e281ea3e74da8b9222976b44e6d2b
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-10 23:48:39 +0200

    Update CarModalCloseButtonStyles.js

--- src/components/CarModal/CarModalCloseButton/CarModalCloseButtonStyles.js
+++ src/components/CarModal/CarModalCloseButton/CarModalCloseButtonStyles.js
@@ -6,6 +6,7 @@ const CarModalCloseButtonContainerStyleBase = styled.div`
 `;
 
 const CarModalCloseButtonStyleBase = styled.button`
+  position: absolute;
   border: none;
   outline: none;
   padding: 0;
