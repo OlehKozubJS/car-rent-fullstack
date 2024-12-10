fe4120b4fbfd2ce661838588823d1faf2c95cac8
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-11 01:00:33 +0200

    Update index.js

--- src/style/fonts/index.js
+++ src/style/fonts/index.js
@@ -2,7 +2,7 @@ import ManropeRegular from "./Manrope-Regular.ttf";
 import ManropeMedium from "./Manrope-Medium.ttf";
 import ManropeSemiBold from "./Manrope-SemiBold.ttf";
 import MontserratRegular from "./Montserrat-Regular.ttf";
-import MontserratSemiBold from "./Manrope-SemiBold.ttf";
+import MontserratSemiBold from "./Montserrat-SemiBold.ttf";
 
 import KingsRegular from "./Kings-Regular.ttf";
 import Ojuju from "./Ojuju.ttf";
