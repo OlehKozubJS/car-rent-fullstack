7ca0433ceb677c5e1f87ecbaa134d7e25e4f6769
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-11 00:37:58 +0200

    Update CarModalHeader.jsx

--- src/components/CarModal/CarModalHeader/CarModalHeader.jsx
+++ src/components/CarModal/CarModalHeader/CarModalHeader.jsx
@@ -4,11 +4,23 @@ import { CarModalHeaderStyleBase } from "./CarModalHeaderStyles";
 
 const CarModalHeader = ({ make, year, model }) => {
   return (
-    <CarModalHeaderStyleBase>
-      <span>{make}</span>{" "}
-      <CarModalItemValueStyleBase>{model}</CarModalItemValueStyleBase>,{" "}
-      <span>{year}</span>
-    </CarModalHeaderStyleBase>
+    <>
+      <CarModalHeaderStyleBase>
+        <span>{make}</span>{" "}
+        <CarModalItemValueStyleBase>{model}</CarModalItemValueStyleBase>,{" "}
+        <span>{year}</span>
+      </CarModalHeaderStyleBase>
+      <CarModalHeaderStyleBase>
+        <span>{make}</span>{" "}
+        <CarModalItemValueStyleBase>{model}</CarModalItemValueStyleBase>,{" "}
+        <span>{year}</span>
+      </CarModalHeaderStyleBase>
+      <CarModalHeaderStyleBase>
+        <span>{make}</span>{" "}
+        <CarModalItemValueStyleBase>{model}</CarModalItemValueStyleBase>,{" "}
+        <span>{year}</span>
+      </CarModalHeaderStyleBase>
+    </>
   );
 };
 
