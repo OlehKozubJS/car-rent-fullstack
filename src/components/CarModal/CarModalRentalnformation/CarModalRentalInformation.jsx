fa8e9894219ac91ebf72c7d4214e905527095711
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-10 20:44:49 +0200

    Update CarModalRentalInformation.jsx

--- src/components/CarModal/CarModalRentalnformation/CarModalRentalInformation.jsx
+++ src/components/CarModal/CarModalRentalnformation/CarModalRentalInformation.jsx
@@ -7,32 +7,37 @@ import {
 
 const CarModal = ({ rentalConditions, mileage, rentalPrice }) => {
   return (
-    <CarModalRentalItemsStyleBase>
-      <CarModalRentalItemStyleBase>
-        Minimum age:{" "}
-        <CarModalRentalItemValueStyleBase>
-          {rentalConditions.split("\n")[0].split(" ")[2]}
-        </CarModalRentalItemValueStyleBase>
-      </CarModalRentalItemStyleBase>
-      <CarModalRentalItemStyleBase>
-        {rentalConditions.split("\n")[1]}
-      </CarModalRentalItemStyleBase>
-      <CarModalRentalItemStyleBase>
-        {rentalConditions.split("\n")[2]}
-      </CarModalRentalItemStyleBase>
-      <CarModalRentalItemStyleBase>
-        Mileage:{" "}
-        <CarModalRentalItemValueStyleBase>
-          {mileage}
-        </CarModalRentalItemValueStyleBase>
-      </CarModalRentalItemStyleBase>
-      <CarModalRentalItemStyleBase>
-        Price:{" "}
-        <CarModalRentalItemValueStyleBase>
-          {rentalPrice}
-        </CarModalRentalItemValueStyleBase>
-      </CarModalRentalItemStyleBase>
-    </CarModalRentalItemsStyleBase>
+    <>
+      <CarModalSubHeaderStyleBase>
+        Rental Conditions:{" "}
+      </CarModalSubHeaderStyleBase>
+      <CarModalRentalItemsStyleBase>
+        <CarModalRentalItemStyleBase>
+          Minimum age:{" "}
+          <CarModalRentalItemValueStyleBase>
+            {rentalConditions.split("\n")[0].split(" ")[2]}
+          </CarModalRentalItemValueStyleBase>
+        </CarModalRentalItemStyleBase>
+        <CarModalRentalItemStyleBase>
+          {rentalConditions.split("\n")[1]}
+        </CarModalRentalItemStyleBase>
+        <CarModalRentalItemStyleBase>
+          {rentalConditions.split("\n")[2]}
+        </CarModalRentalItemStyleBase>
+        <CarModalRentalItemStyleBase>
+          Mileage:{" "}
+          <CarModalRentalItemValueStyleBase>
+            {mileage}
+          </CarModalRentalItemValueStyleBase>
+        </CarModalRentalItemStyleBase>
+        <CarModalRentalItemStyleBase>
+          Price:{" "}
+          <CarModalRentalItemValueStyleBase>
+            {rentalPrice}
+          </CarModalRentalItemValueStyleBase>
+        </CarModalRentalItemStyleBase>
+      </CarModalRentalItemsStyleBase>
+    </>
   );
 };
 
