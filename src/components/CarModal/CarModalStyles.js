e19ff413dfb184267454234b2003e769050246f1
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-10 21:29:36 +0200

    Update CarModalStyles.js

--- src/components/CarModal/CarModalStyles.js
+++ src/components/CarModal/CarModalStyles.js
@@ -1,26 +1,5 @@
 import styled from "@emotion/styled";
 
-const CarModalBackdropStyleBase = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(18, 20, 23, 0.5);
-`;
-
-const CarModalContainerStyleBase = styled.article`
-  box-sizing: border-box;
-  max-width: 50%;
-  height: 100%;
-  padding: 15px 37px 40px 45px;
-  border-radius: 24px;
-  background: #fff;
-`;
-
 const CarModalImageStyleBase = styled.img`
   display: block;
   height: 314px;
