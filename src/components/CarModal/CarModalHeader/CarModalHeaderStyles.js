17c6d7b14d33707039575ab27c6929b8a883e1d7
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-11 00:40:19 +0200

    Update CarModalHeaderStyles.js

--- src/components/CarModal/CarModalHeader/CarModalHeaderStyles.js
+++ src/components/CarModal/CarModalHeader/CarModalHeaderStyles.js
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 
-const CarModalHeaderStyleBase = styled.h3`
+const CarModalHeaderStyleBase1 = styled.h3`
   margin-top: 14px;
 
   font-family: "Manrope-Medium";
@@ -9,4 +9,26 @@ const CarModalHeaderStyleBase = styled.h3`
   color: #121417;
 `;
 
-export { CarModalHeaderStyleBase };
+const CarModalHeaderStyleBase2 = styled.h3`
+  margin-top: 14px;
+
+  font-family: "Manrope-Medium";
+  font-size: 18px;
+  line-height: 24px;
+  color: #121417;
+`;
+
+const CarModalHeaderStyleBase3 = styled.h3`
+  margin-top: 14px;
+
+  font-family: "Manrope-Medium";
+  font-size: 18px;
+  line-height: 24px;
+  color: #121417;
+`;
+
+export {
+  CarModalHeaderStyleBase1,
+  CarModalHeaderStyleBase2,
+  CarModalHeaderStyleBase3,
+};
