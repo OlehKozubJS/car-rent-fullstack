f9f0697c09d3c78fd2938d5f875446d58f8e3012
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-10 20:04:20 +0200

    Update CarModalStyles.js

--- src/components/CarModal/CarModalStyles.js
+++ src/components/CarModal/CarModalStyles.js
@@ -90,29 +90,6 @@ const CarModalRentalItemValueStyleBase = styled.span`
   font-family: "Montserrat-SemiBold", sans-serif;
 `;
 
-const CarModalLinkStyleBase = styled.a`
-  margin-top: 24px;
-
-  display: inline-flex;
-  padding: 12px 50px;
-  justify-content: center;
-  align-items: center;
-
-  border: none;
-  border-radius: 12px;
-  background: #3470ff;
-
-  color: #fff;
-  font-size: 14px;
-  line-height: 20px;
-
-  font-family: "Manrope-SemiBold", sans-serif;
-
-  &:hover {
-    background: #0b44cd;
-  }
-`;
-
 export {
   CarModalBackdropStyleBase,
   CarModalContainerStyleBase,
@@ -124,5 +101,4 @@ export {
   CarModalRentalItemsStyleBase,
   CarModalRentalItemStyleBase,
   CarModalRentalItemValueStyleBase,
-  CarModalLinkStyleBase,
 };
