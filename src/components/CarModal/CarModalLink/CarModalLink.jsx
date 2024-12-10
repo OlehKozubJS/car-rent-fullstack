21ffed4d86d54240d658c10b3255226145f3eaa3
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-10 20:01:22 +0200

    Update CarModalLink.jsx

--- src/components/CarModal/CarModalLink/CarModalLink.jsx
+++ src/components/CarModal/CarModalLink/CarModalLink.jsx
@@ -1,112 +1,11 @@
-import { CarModalCloseButton } from "./CarModalCloseButton";
-import { CarModalHeader } from "./CarModalHeader";
+import { CarModalLinkStyleBase } from "./CarModalLinkStyles";
 
-import { CarModalDescription } from "./CarModalDescription";
-
-import {
-  CarModalBackdropStyleBase,
-  CarModalContainerStyleBase,
-  CarModalImageStyleBase,
-  CarModalItemsStyleBase,
-  CarModalItemStyleBase,
-  CarModalItemValueStyleBase,
-  CarModalSubHeaderStyleBase,
-  CarModalRentalItemsStyleBase,
-  CarModalRentalItemStyleBase,
-  CarModalRentalItemValueStyleBase,
-  CarModalLinkStyleBase,
-} from "./CarModalStyles";
-
-const CarModal = ({ car, onClose }) => {
+const CarModalLink = () => {
   return (
-    <CarModalBackdropStyleBase>
-      <CarModalContainerStyleBase>
-        <CarModalCloseButton onClose={onClose} />
-        <CarModalImageStyleBase src={car.img} alt="" />
-        <CarModalHeader make={car.make} model={car.model} year={car.year} />
-        <CarModalItemsStyleBase>
-          <CarModalItemStyleBase>
-            {car.address.split(", ")[1]}
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            {car.address.split(", ")[2]}
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            Id:{" "}
-            <CarModalItemValueStyleBase>{car.id}</CarModalItemValueStyleBase>
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            Year:{" "}
-            <CarModalItemValueStyleBase>{car.year}</CarModalItemValueStyleBase>
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            Type:{" "}
-            <CarModalItemValueStyleBase>{car.type}</CarModalItemValueStyleBase>
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            Fuel Consumption:{" "}
-            <CarModalItemValueStyleBase>
-              {car.fuelConsumption}
-            </CarModalItemValueStyleBase>
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            Engine Size:{" "}
-            <CarModalItemValueStyleBase>
-              {car.engineSize}
-            </CarModalItemValueStyleBase>
-          </CarModalItemStyleBase>
-        </CarModalItemsStyleBase>
-        <CarModalDescription description={car.description} />
-        <CarModalSubHeaderStyleBase>
-          Accessories and functionalities:
-        </CarModalSubHeaderStyleBase>
-        <CarModalItemsStyleBase>
-          {car.accessories.map((item, index) => {
-            return (
-              <CarModalItemStyleBase key={index}>{item}</CarModalItemStyleBase>
-            );
-          })}
-          {car.functionalities.map((item, index) => {
-            return (
-              <CarModalItemStyleBase key={index}>{item}</CarModalItemStyleBase>
-            );
-          })}
-        </CarModalItemsStyleBase>
-        <CarModalSubHeaderStyleBase>
-          Rental Conditions:{" "}
-        </CarModalSubHeaderStyleBase>
-        <CarModalRentalItemsStyleBase>
-          <CarModalRentalItemStyleBase>
-            Minimum age:{" "}
-            <CarModalRentalItemValueStyleBase>
-              {car.rentalConditions.split("\n")[0].split(" ")[2]}
-            </CarModalRentalItemValueStyleBase>
-          </CarModalRentalItemStyleBase>
-          <CarModalRentalItemStyleBase>
-            {car.rentalConditions.split("\n")[1]}
-          </CarModalRentalItemStyleBase>
-          <CarModalRentalItemStyleBase>
-            {car.rentalConditions.split("\n")[2]}
-          </CarModalRentalItemStyleBase>
-          <CarModalRentalItemStyleBase>
-            Mileage:{" "}
-            <CarModalRentalItemValueStyleBase>
-              {car.mileage}
-            </CarModalRentalItemValueStyleBase>
-          </CarModalRentalItemStyleBase>
-          <CarModalRentalItemStyleBase>
-            Price:{" "}
-            <CarModalRentalItemValueStyleBase>
-              {car.rentalPrice}
-            </CarModalRentalItemValueStyleBase>
-          </CarModalRentalItemStyleBase>
-        </CarModalRentalItemsStyleBase>
-        <CarModalLinkStyleBase href="tel:+380730000000">
-          Rental car
-        </CarModalLinkStyleBase>
-      </CarModalContainerStyleBase>
-    </CarModalBackdropStyleBase>
+    <CarModalLinkStyleBase href="tel:+380730000000">
+      Rental car
+    </CarModalLinkStyleBase>
   );
 };
 
-export { CarModal };
+export { CarModalLink };
