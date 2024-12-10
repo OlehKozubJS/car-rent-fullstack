1c1898d11ead99557799f8581b4d2642732be113
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-10 21:06:15 +0200

    Update CarModalCommonInformation.jsx

--- src/components/CarModal/CarModalCommonInformation/CarModalCommonInformation.jsx
+++ src/components/CarModal/CarModalCommonInformation/CarModalCommonInformation.jsx
@@ -1,72 +1,40 @@
-import { CarModalCloseButton } from "./CarModalCloseButton";
-import { CarModalHeader } from "./CarModalHeader";
-
-import { CarModalDescription } from "./CarModalDescription";
-import { CarModalAccessoriesInformation } from "./CarModalAccessoriesInformation";
-import { CarModalRentalInformation } from "./CarModalRentalInformation";
-import { CarModalLink } from "./CarModalLink";
-
 import {
-  CarModalBackdropStyleBase,
-  CarModalContainerStyleBase,
-  CarModalImageStyleBase,
   CarModalItemsStyleBase,
   CarModalItemStyleBase,
   CarModalItemValueStyleBase,
-} from "./CarModalStyles";
+} from "./imports";
 
-const CarModal = ({ car, onClose }) => {
+const CarModal = ({ address, id, type, fuelConsumption }) => {
   return (
-    <CarModalBackdropStyleBase>
-      <CarModalContainerStyleBase>
-        <CarModalCloseButton onClose={onClose} />
-        <CarModalImageStyleBase src={car.img} alt="" />
-        <CarModalHeader make={car.make} model={car.model} year={car.year} />
-        <CarModalItemsStyleBase>
-          <CarModalItemStyleBase>
-            {car.address.split(", ")[1]}
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            {car.address.split(", ")[2]}
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            Id:{" "}
-            <CarModalItemValueStyleBase>{car.id}</CarModalItemValueStyleBase>
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            Year:{" "}
-            <CarModalItemValueStyleBase>{car.year}</CarModalItemValueStyleBase>
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            Type:{" "}
-            <CarModalItemValueStyleBase>{car.type}</CarModalItemValueStyleBase>
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            Fuel Consumption:{" "}
-            <CarModalItemValueStyleBase>
-              {car.fuelConsumption}
-            </CarModalItemValueStyleBase>
-          </CarModalItemStyleBase>
-          <CarModalItemStyleBase>
-            Engine Size:{" "}
-            <CarModalItemValueStyleBase>
-              {car.engineSize}
-            </CarModalItemValueStyleBase>
-          </CarModalItemStyleBase>
-        </CarModalItemsStyleBase>
-        <CarModalDescription description={car.description} />
-        <CarModalAccessoriesInformation
-          accessories={car.accessories}
-          functionalities={car.functionalities}
-        />
-        <CarModalRentalInformation
-          rentalConditions={car.rentalConditions}
-          mileage={car.mileage}
-          rentalPrice={car.rentalPrice}
-        />
-        <CarModalLink />
-      </CarModalContainerStyleBase>
-    </CarModalBackdropStyleBase>
+    <CarModalItemsStyleBase>
+      <CarModalItemStyleBase>
+        {car.address.split(", ")[1]}
+      </CarModalItemStyleBase>
+      <CarModalItemStyleBase>
+        {car.address.split(", ")[2]}
+      </CarModalItemStyleBase>
+      <CarModalItemStyleBase>
+        Id: <CarModalItemValueStyleBase>{id}</CarModalItemValueStyleBase>
+      </CarModalItemStyleBase>
+      <CarModalItemStyleBase>
+        Year: <CarModalItemValueStyleBase>{year}</CarModalItemValueStyleBase>
+      </CarModalItemStyleBase>
+      <CarModalItemStyleBase>
+        Type: <CarModalItemValueStyleBase>{type}</CarModalItemValueStyleBase>
+      </CarModalItemStyleBase>
+      <CarModalItemStyleBase>
+        Fuel Consumption:{" "}
+        <CarModalItemValueStyleBase>
+          {fuelConsumption}
+        </CarModalItemValueStyleBase>
+      </CarModalItemStyleBase>
+      <CarModalItemStyleBase>
+        Engine Size:{" "}
+        <CarModalItemValueStyleBase>
+          {car.engineSize}
+        </CarModalItemValueStyleBase>
+      </CarModalItemStyleBase>
+    </CarModalItemsStyleBase>
   );
 };
 
