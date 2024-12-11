87a05831357a5d1381efb173424efeafad70e620
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 00:15:47 +0200

    Update CarModalHeader.jsx

--- src/components/CarModal/CarModalHeader/CarModalHeader.jsx
+++ src/components/CarModal/CarModalHeader/CarModalHeader.jsx
@@ -14,21 +14,6 @@ const CarModalHeader = ({ make, year, model }) => {
         <CarModalItemValueStyleBase>{model}</CarModalItemValueStyleBase>,{" "}
         <span>{year}</span>
       </CarModalHeaderStyleBaseOne>
-      <CarModalHeaderStyleBaseTwo font="manrope-medium">
-        <span>{make}</span>{" "}
-        <CarModalItemValueStyleBase>{model}</CarModalItemValueStyleBase>,{" "}
-        <span>{year}</span>
-      </CarModalHeaderStyleBaseTwo>
-      <CarModalHeaderStyleBaseThree>
-        <span>{make}</span>{" "}
-        <CarModalItemValueStyleBase>{model}</CarModalItemValueStyleBase>,{" "}
-        <span>{year}</span>
-      </CarModalHeaderStyleBaseThree>
-      <span style={{ fontFamily: "manrope-regular" }}>Regular</span>
-      <span style={{ fontFamily: "manrope-medium" }}>Medium</span>
-      <span style={{ fontFamily: "manrope-semibold" }}>Semibold</span>
-      <span style={{ fontFamily: "montserrat-regular" }}>Regular</span>
-      <span style={{ fontFamily: "montserrat-semibold" }}>Semibold</span>
     </>
   );
 };
