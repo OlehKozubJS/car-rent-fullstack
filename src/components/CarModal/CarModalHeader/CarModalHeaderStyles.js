8c9b4f96c2e1ad283c54700510ef51a9163b19cc
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 00:16:10 +0200

    Update CarModalHeaderStyles.js

--- src/components/CarModal/CarModalHeader/CarModalHeaderStyles.js
+++ src/components/CarModal/CarModalHeader/CarModalHeaderStyles.js
@@ -9,28 +9,4 @@ const CarModalHeaderStyleBaseOne = styled.h3`
   color: #121417;
 `;
 
-const CarModalHeaderStyleBaseTwo = styled.h3`
-  margin-top: 14px;
-
-  font-family: ${({ font }) => {
-    return font;
-  }};
-  font-size: 18px;
-  line-height: 24px;
-  color: #121417;
-`;
-
-const CarModalHeaderStyleBaseThree = styled.h3`
-  margin-top: 14px;
-
-  font-family: "Manrope-SemiBold";
-  font-size: 18px;
-  line-height: 24px;
-  color: #121417;
-`;
-
-export {
-  CarModalHeaderStyleBaseOne,
-  CarModalHeaderStyleBaseTwo,
-  CarModalHeaderStyleBaseThree,
-};
+export { CarModalHeaderStyleBaseOne };
