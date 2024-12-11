d8b016e4c82f79ce2489af2e7aff4f4d9b534636
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-11 23:41:28 +0200

    Update MygerPage.jsx

--- src/pages/MygerPage.jsx
+++ src/pages/MygerPage.jsx
@@ -86,6 +86,8 @@ const MygerPage = () => {
       <span style={{ fontFamily: "manrope-regular" }}>Regular</span>
       <span style={{ fontFamily: "manrope-medium" }}>Medium</span>
       <span style={{ fontFamily: "manrope-semibold" }}>Semibold</span>
+      <span style={{ fontFamily: "montserrat-regular" }}>Regular</span>
+      <span style={{ fontFamily: "montserrat-semibold" }}>Semibold</span>
     </div>
   );
 };
