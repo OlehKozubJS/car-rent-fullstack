02e5a97abbad5b2dbc5c4acad5081bd30af2d218
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 20:18:02 +0200

    Update CarGallery.jsx

--- src/components/CarGallery/CarGallery.jsx
+++ src/components/CarGallery/CarGallery.jsx
@@ -1,12 +1,12 @@
 import {} from "./CarGalleryStyles";
 
-import { CarItem } from "./CarGalleryItem";
+import { CarGalleryItem } from "./CarGalleryItem";
 
 const CarGallery = ({ cars, onClick }) => {
   return (
     <ul>
       {cars.map((car, carIndex) => {
-        return <CarItem key={carIndex} car={car} onClick={onClick} />;
+        return <CaGalleryItem key={carIndex} car={car} onClick={onClick} />;
       })}
     </ul>
   );
