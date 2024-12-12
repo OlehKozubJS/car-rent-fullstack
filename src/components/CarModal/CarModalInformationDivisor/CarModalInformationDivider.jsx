999c6c7701ef601ab27cad7df00084a8f037378a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 11:47:00 +0200

    Update CarModalInformationDivider.jsx

--- src/components/CarModal/CarModalInformationDivisor/CarModalInformationDivider.jsx
+++ src/components/CarModal/CarModalInformationDivisor/CarModalInformationDivider.jsx
@@ -8,7 +8,9 @@ const CarModalInformationDividerStyleBase = styled.div`
 `;
 
 const CarModalInformationDivider = () => {
-  return <CarModalInformationDivider></CarModalInformationDivider>;
+  return (
+    <CarModalInformationDividerStyleBase></CarModalInformationDividerStyleBase>
+  );
 };
 
 export { CarModalInformationDivider };
