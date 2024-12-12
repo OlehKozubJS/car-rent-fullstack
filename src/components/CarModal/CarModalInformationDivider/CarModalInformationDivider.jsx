e7c6a6e0af3b5b97ce8e4ddcd301e447c75e3f09
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 11:49:24 +0200

    Create CarModalInformationDivider.jsx

---
+++ src/components/CarModal/CarModalInformationDivider/CarModalInformationDivider.jsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+const CarModalInformationDividerStyleBase = styled.div`
+  height: 16px;
+  width: 10px;
+
+  background-color: rgba(18, 20, 23, 0.1);
+`;
+
+const CarModalInformationDivider = () => {
+  return (
+    <CarModalInformationDividerStyleBase></CarModalInformationDividerStyleBase>
+  );
+};
+
+export { CarModalInformationDivider };
