0229d4a5db65dfc5bd7320bc21740a3e2fbb96b6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 19:34:29 +0200

    Update CarGalleryInformationDivider.jsx

--- src/components/CarGallery/CarGalleryInformationDivider/CarGalleryInformationDivider.jsx
+++ src/components/CarGallery/CarGalleryInformationDivider/CarGalleryInformationDivider.jsx
@@ -1,16 +1,16 @@
 import styled from "@emotion/styled";
 
-const CarModalInformationDividerStyleBase = styled.div`
+const CarGalleryInformationDividerStyleBase = styled.div`
   height: 16px;
   width: 0.5px;
 
   background-color: rgba(18, 20, 23, 0.1);
 `;
 
-const CarModalInformationDivider = () => {
+const CarGalleryInformationDivider = () => {
   return (
-    <CarModalInformationDividerStyleBase></CarModalInformationDividerStyleBase>
+    <CarGalleryInformationDividerStyleBase></CarGalleryInformationDividerStyleBase>
   );
 };
 
-export { CarModalInformationDivider };
+export { CarGalleryInformationDivider };
