60f71fbb01ef44ad2dec033017c307aa7cd207a6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 21:03:13 +0200

    Update TestPage.jsx

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -181,6 +181,14 @@ const TestPage = () => {
         </div>
         <button type="submit">Find car</button>
       </form>
+      <span style={{ fontFamily: "manrope-regular" }}>Regular</span>
+      <span style={{ fontFamily: "manrope-medium" }}>Medium</span>
+      <span style={{ fontFamily: "manrope-semibold" }}>Semibold</span>
+      <span style={{ fontFamily: "montserrat-regular" }}>Regular</span>
+      <span style={{ fontFamily: "montserrat-semibold" }}>Semibold</span>
+      <div style={{ backgroundColor: "black" }}>
+        <SVGimage name="not-fav" />
+      </div>
     </div>
   );
 };
