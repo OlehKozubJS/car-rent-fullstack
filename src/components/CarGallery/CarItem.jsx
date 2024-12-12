32f0b6aabaa8ecb9c758b19efb4754ff1258386f
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 17:47:09 +0200

    Update CarItem.jsx

--- src/components/CarGallery/CarItem.jsx
+++ src/components/CarGallery/CarItem.jsx
@@ -3,7 +3,9 @@ import { useState, useEffect } from "react";
 import { SVGimage } from "../../images";
 
 const CarItem = ({ car }) => {
-  return <li>{JSON.stringify(car)}</li>;
+  return <li>{JSON.stringify(car)}
+  <button onClick={}>Learn more</button>
+  </li>;
 };
 
 export { CarItem };
