506d2b98f2693c58628b681640416900c61dd17c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 18:39:46 +0200

    Create getCarGalleryItemsWithDivisors.js

---
+++ src/components/CarGallery/CarGalleryAccessoriesInformation/getCarGalleryItemsWithDivisors.js
@@ -0,0 +1,23 @@
+import { CarModalInformationDivider } from "../CarModalInformationDivider";
+
+import { CarModalItemStyleBase } from "./imports";
+
+const getCarModalItemsWithDivisors = (items) => {
+  let CarModalItemsArray = [];
+
+  for (let itemIndex = 0; itemIndex < items.length; itemIndex += 1) {
+    CarModalItemsArray = [
+      ...CarModalItemsArray,
+      <CarModalItemStyleBase key={`item${itemIndex}`}>
+        {items[itemIndex]}
+      </CarModalItemStyleBase>,
+      itemIndex < items.length - 1 && (
+        <CarModalInformationDivider key={`divisor${itemIndex}`} />
+      ),
+    ];
+  }
+
+  return CarModalItemsArray;
+};
+
+export { getCarModalItemsWithDivisors };
