45c4b791848951ffff87c1b844a3724054b481d7
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 23:31:39 +0200

    Create FavouritesPage.jsx

---
+++ src/pages/FavouritesPage.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { CarFilter, CarGallery, CarModal } from "./imports";
+
+const MainPage = ({ cars }) => {
+  const [isModal, setIsModal] = useState(false);
+  const [car, setCar] = useState(null);
+
+  const openModal = (newCar) => {
+    setCar(newCar);
+    setIsModal(true);
+  };
+
+  const closeModal = () => {
+    setIsModal(false);
+  };
+
+  return (
+    <section>
+      {isModal && <CarModal car={car} onClose={closeModal} />}
+      <CarFilter />
+      <CarGallery cars={cars} onClick={openModal} />
+    </section>
+  );
+};
+
+export { MainPage };
