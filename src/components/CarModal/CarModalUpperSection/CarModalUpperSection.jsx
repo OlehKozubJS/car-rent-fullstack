cc1387e30520c438247625134f00a43529c652a9
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 23:11:02 +0200

    Update CarModalUpperSection.jsx

--- src/components/CarModal/CarModalUpperSection/CarModalUpperSection.jsx
+++ src/components/CarModal/CarModalUpperSection/CarModalUpperSection.jsx
@@ -7,7 +7,7 @@ import { CarModalImageStyleBase } from "../CarModalStyles";
 const CarModalUpperSection = ({ car }) => {
   return (
     <section>
-      <CarModalImageStyleBase src={car.img} alt="" />
+      <CarModalImageStyleBase src={car.photoLink} alt="" />
       <CarModalHeader make={car.make} model={car.model} year={car.year} />
       <CarModalCommonInformation
         address={car.address}
