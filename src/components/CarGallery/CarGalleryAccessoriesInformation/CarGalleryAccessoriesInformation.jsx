08a44c8468e745238199e2bb35b5af8984a8e36e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 21:15:27 +0200

    Update CarGalleryAccessoriesInformation.jsx

--- src/components/CarGallery/CarGalleryAccessoriesInformation/CarGalleryAccessoriesInformation.jsx
+++ src/components/CarGallery/CarGalleryAccessoriesInformation/CarGalleryAccessoriesInformation.jsx
@@ -1,16 +1,10 @@
 import { getCarGalleryItemsWithDivisors } from "./getCarGalleryItemsWithDivisors";
 
-import {
-  CarGallerySubHeaderStyleBase,
-  CarGalleryItemsStyleBase,
-} from "./imports";
+import { CarGalleryItemsStyleBase } from "./imports";
 
 const CarGalleryAccessoriesInformation = ({ accessories, functionalities }) => {
   return (
     <section>
-      <CarGallerySubHeaderStyleBase>
-        Accessories and functionalities:
-      </CarGallerySubHeaderStyleBase>
       <CarGalleryItemsStyleBase>
         {getCarGalleryItemsWithDivisors(accessories)}
         {getCarGalleryItemsWithDivisors(functionalities)}
