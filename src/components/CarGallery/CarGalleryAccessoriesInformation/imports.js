e408bc703c16ecf54dfcdff2091e60921637dbcc
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 18:40:12 +0200

    Create imports.js

---
+++ src/components/CarGallery/CarGalleryAccessoriesInformation/imports.js
@@ -0,0 +1,11 @@
+import {
+  CarModalSubHeaderStyleBase,
+  CarModalItemsStyleBase,
+  CarModalItemStyleBase,
+} from "../CarModalStyles";
+
+export {
+  CarModalSubHeaderStyleBase,
+  CarModalItemsStyleBase,
+  CarModalItemStyleBase,
+};
