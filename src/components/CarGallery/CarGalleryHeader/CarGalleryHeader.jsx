964bcb155988e4e7cdf32ae1723e00f5c939ae15
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 18:43:34 +0200

    Create CarGalleryHeader.jsx

---
+++ src/components/CarGallery/CarGalleryHeader/CarGalleryHeader.jsx
@@ -0,0 +1,15 @@
+import { CarModalItemValueStyleBase } from "./imports";
+
+import { CarModalHeaderStyleBase } from "./CarModalHeaderStyles";
+
+const CarModalHeader = ({ make, year, model }) => {
+  return (
+    <CarModalHeaderStyleBase>
+      <span>{make}</span>{" "}
+      <CarModalItemValueStyleBase>{model}</CarModalItemValueStyleBase>,{" "}
+      <span>{year}</span>
+    </CarModalHeaderStyleBase>
+  );
+};
+
+export { CarModalHeader };
