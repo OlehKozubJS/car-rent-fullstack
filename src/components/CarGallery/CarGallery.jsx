e52fbb92bd7966a23550f61954886539597b6546
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 17:31:02 +0200

    Update CarGallery.jsx

--- src/components/CarGallery/CarGallery.jsx
+++ src/components/CarGallery/CarGallery.jsx
@@ -1,13 +1,8 @@
 import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-
-import { getSelectedCars } from "../../hooks";
 
 import { CarItem } from "./CarItem";
 
 const CarGallery = () => {
-  const selectedCarsInformation = useSelector(getSelectedCars);
-
   return (
     <>
       <ul>
