f9bcbbd0c8dfc0502384fafb104cfd85707f73a7
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 18:27:38 +0200

    Create CarGalleryCommonInformation.jsx

---
+++ src/components/CarGallery/CarGalleryCommonInformation/CarGalleryCommonInformation.jsx
@@ -0,0 +1,52 @@
+import { CarModalInformationDivider } from "../CarModalInformationDivider";
+
+import {
+  CarModalItemsStyleBase,
+  CarModalItemStyleBase,
+  CarModalItemValueStyleBase,
+} from "./imports";
+
+const CarModalCommonInformation = ({
+  address,
+  id,
+  year,
+  type,
+  fuelConsumption,
+  engineSize,
+}) => {
+  return (
+    <>
+      <CarModalItemsStyleBase>
+        <CarModalItemStyleBase>{address.split(", ")[1]}</CarModalItemStyleBase>
+        <CarModalInformationDivider />
+        <CarModalItemStyleBase>{address.split(", ")[2]}</CarModalItemStyleBase>
+        <CarModalInformationDivider />
+        <CarModalItemStyleBase>
+          Id: <CarModalItemValueStyleBase>{id}</CarModalItemValueStyleBase>
+        </CarModalItemStyleBase>
+        <CarModalInformationDivider />
+        <CarModalItemStyleBase>
+          Year: <CarModalItemValueStyleBase>{year}</CarModalItemValueStyleBase>
+        </CarModalItemStyleBase>
+        <CarModalInformationDivider />
+        <CarModalItemStyleBase>
+          Type: <CarModalItemValueStyleBase>{type}</CarModalItemValueStyleBase>
+        </CarModalItemStyleBase>
+        <CarModalInformationDivider />
+        <CarModalItemStyleBase>
+          Fuel Consumption:{" "}
+          <CarModalItemValueStyleBase>
+            {fuelConsumption}
+          </CarModalItemValueStyleBase>
+        </CarModalItemStyleBase>
+        <CarModalInformationDivider />
+        <CarModalItemStyleBase>
+          Engine Size:{" "}
+          <CarModalItemValueStyleBase>{engineSize}</CarModalItemValueStyleBase>
+        </CarModalItemStyleBase>
+      </CarModalItemsStyleBase>
+    </>
+  );
+};
+
+export { CarModalCommonInformation };
