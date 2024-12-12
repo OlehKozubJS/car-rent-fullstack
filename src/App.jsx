201671696746f9e78b5ca1e2f646d2fbdec78e8b
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 23:50:27 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
             "Test page": <TestPage />,
             "Myger page": <MygerPage />,
           }}
-          onChange={handleCurrentPageName}
+          onChange={handleCurrentPage}
         />
       </header>
       <main>{currentPage}</main>
