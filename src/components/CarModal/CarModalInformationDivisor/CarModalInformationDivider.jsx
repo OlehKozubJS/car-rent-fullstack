c5d917ff7e6fe28eb3f62b2ac2727b2f649fe1a7
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 11:46:21 +0200

    Update CarModalInformationDivider.jsx

--- src/components/CarModal/CarModalInformationDivisor/CarModalInformationDivider.jsx
+++ src/components/CarModal/CarModalInformationDivisor/CarModalInformationDivider.jsx
@@ -3,6 +3,8 @@ import styled from "@emotion/styled";
 const CarModalInformationDividerStyleBase = styled.div`
   height: 16px;
   width: 10px;
+
+  background-color: rgba(18, 20, 23, 0.1);
 `;
 
 const CarModalInformationDivider = () => {
