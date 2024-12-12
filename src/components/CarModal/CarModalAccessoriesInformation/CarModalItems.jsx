028967b40996b7707e20a7679216ba3e9f6ac756
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 14:41:29 +0200

    Update CarModalItems.jsx

--- src/components/CarModal/CarModalAccessoriesInformation/CarModalItems.jsx
+++ src/components/CarModal/CarModalAccessoriesInformation/CarModalItems.jsx
@@ -7,23 +7,9 @@ import {
 } from "./imports";
 
 const CarModalAccessoriesInformation = ({ accessories, functionalities }) => {
-  return (
-      <CarModalItemsStyleBase>
-        {accessories.map((item, index, array) => {
-          return (
-            <CarModalItemStyleBase key={index}>
-              {item}
-            </CarModalItemStyleBase>
-          );
-        })}
-        {functionalities.map((item, index) => {
-          return (
-            <CarModalItemStyleBase key={index}>{item}</CarModalItemStyleBase>
-          );
-        })}
-      </CarModalItemsStyleBase>
-    </>
-  );
+  return accessories.map((item, index, array) => {
+    return <CarModalItemStyleBase key={index}>{item}</CarModalItemStyleBase>;
+  });
 };
 
 export { CarModalAccessoriesInformation };
