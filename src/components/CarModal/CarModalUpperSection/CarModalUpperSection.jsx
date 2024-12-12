3be7ccafc487fa6227ef0c37f7d33c25719c2d78
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 17:15:22 +0200

    Update CarModalUpperSection.jsx

--- src/components/CarModal/CarModalUpperSection/CarModalUpperSection.jsx
+++ src/components/CarModal/CarModalUpperSection/CarModalUpperSection.jsx
@@ -1,43 +1,27 @@
-import { CarModalContainer } from "./CarModalContainer";
-import { CarModalCloseButton } from "./CarModalCloseButton";
-import { CarModalHeader } from "./CarModalHeader";
-import { CarModalCommonInformation } from "./CarModalCommonInformation";
-import { CarModalDescription } from "./CarModalDescription";
-import { CarModalAccessoriesInformation } from "./CarModalAccessoriesInformation";
-import { CarModalRentalInformation } from "./CarModalRentalInformation";
-import { CarModalLink } from "./CarModalLink";
+import { CarModalCloseButton } from "../CarModalCloseButton";
+import { CarModalHeader } from "../CarModalHeader";
+import { CarModalCommonInformation } from "../CarModalCommonInformation";
+import { CarModalDescription } from "../CarModalDescription";
 
 import { CarModalImageStyleBase } from "./CarModalStyles";
 
-const CarModal = ({ car, onClose }) => {
+const CarModalUpperSection = ({ car, onClose }) => {
   return (
-    <CarModalContainer>
-      <section>
-        <CarModalCloseButton onClose={onClose} />
-        <CarModalImageStyleBase src={car.img} alt="" />
-        <CarModalHeader make={car.make} model={car.model} year={car.year} />
-        <CarModalCommonInformation
-          address={car.address}
-          id={car.id}
-          year={car.year}
-          type={car.type}
-          fuelConsumption={car.fuelConsumption}
-          engineSize={car.engineSize}
-        />
-        <CarModalDescription description={car.description} />
-      </section>
-      <CarModalAccessoriesInformation
-        accessories={car.accessories}
-        functionalities={car.functionalities}
+    <section>
+      <CarModalCloseButton onClose={onClose} />
+      <CarModalImageStyleBase src={car.img} alt="" />
+      <CarModalHeader make={car.make} model={car.model} year={car.year} />
+      <CarModalCommonInformation
+        address={car.address}
+        id={car.id}
+        year={car.year}
+        type={car.type}
+        fuelConsumption={car.fuelConsumption}
+        engineSize={car.engineSize}
       />
-      <CarModalRentalInformation
-        rentalConditions={car.rentalConditions}
-        mileage={car.mileage}
-        rentalPrice={car.rentalPrice}
-      />
-      <CarModalLink />
-    </CarModalContainer>
+      <CarModalDescription description={car.description} />
+    </section>
   );
 };
 
-export { CarModal };
+export { CarModalUpperSection };
