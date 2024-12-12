9290a30b9077a9c16e61e0d1bb08cac67ef966b2
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 23:39:29 +0200

    Update PageNavigation.jsx

--- src/components/PageNavigation/PageNavigation.jsx
+++ src/components/PageNavigation/PageNavigation.jsx
@@ -12,32 +12,27 @@ const PageNavigation = ({ pages, onClick }) => {
   };
 
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            {Object.keys(pages).map((pageName) => {
-              return (
-                <li key={pageName}>
-                  <label htmlFor={pageName}>
-                    <input
-                      id={pageName}
-                      name="pageSwitcher"
-                      type="radio"
-                      onChange={handleCurrentPageName}
-                      value={pageName}
-                      checked={currentPageName === pageName}
-                    />
-                    <span>{pageName}</span>
-                  </label>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </header>
-      <main>{pages[currentPageName]}</main>
-    </div>
+    <nav>
+      <ul>
+        {Object.keys(pages).map((pageName) => {
+          return (
+            <li key={pageName}>
+              <label htmlFor={pageName}>
+                <input
+                  id={pageName}
+                  name="pageSwitcher"
+                  type="radio"
+                  onChange={handleCurrentPageName}
+                  value={pageName}
+                  checked={currentPageName === pageName}
+                />
+                <span>{pageName}</span>
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 };
 
