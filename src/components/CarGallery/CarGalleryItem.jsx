cae2920018177f555fae4c89eb84c2aa8dce7e83
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 20:16:42 +0200

    Create CarGalleryItem.jsx

---
+++ src/components/CarGallery/CarGalleryItem.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+import { CarGalleryHeader } from "./CarGalleryHeader";
+import { CarGalleryCommonInformation } from "./CarGalleryCommonInformation";
+import { CarGalleryAccessoriesInformation } from "./CarGalleryAccessoriesInformation";
+
+import {
+  CarGalleryContainerStyleBase,
+  CarGalleryImageStyleBase,
+  CarGalleryButtonStyleBase,
+} from "./CarGalleryStyles";
+
+import { SVGimage } from "../../images";
+
+const CarItem = ({ car, onClick }) => {
+  const [ifFavourite, setIsFavourite] = useState(false);
+
+  const markAsFavourite = () => {
+    setIsFavourite(true);
+  };
+
+  const unMarkAsFavourite = () => {
+    setIsFavourite(false);
+  };
+
+  const handleClick = () => {
+    onClick(car);
+  };
+
+  return (
+    <CarGalleryContainerStyleBase>
+      <CarGalleryImageStyleBase src={car.img} alt="" />
+      <CarGalleryHeader make={car.make} model={car.model} year={car.year} />
+      <CarGalleryCommonInformation
+        address={car.address}
+        id={car.id}
+        year={car.year}
+        type={car.type}
+        fuelConsumption={car.fuelConsumption}
+        engineSize={car.engineSize}
+      />
+      <CarGalleryAccessoriesInformation
+        accessories={car.accessories}
+        functionalities={car.functionalities}
+      />
+      <CarGalleryButtonStyleBase onClick={handleClick}>
+        Learn more
+      </CarGalleryButtonStyleBase>
+    </CarGalleryContainerStyleBase>
+  );
+};
+
+export { CarItem };
