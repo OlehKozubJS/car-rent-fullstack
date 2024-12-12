576fc472d2f5ea40d17f8de9cd6a81c746604d8f
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 17:14:25 +0200

    Create CarModalUpperSection.jsx

---
+++ src/components/CarModal/CarModalUpperSection/CarModalUpperSection.jsx
@@ -0,0 +1,43 @@
+import { CarModalContainer } from "./CarModalContainer";
+import { CarModalCloseButton } from "./CarModalCloseButton";
+import { CarModalHeader } from "./CarModalHeader";
+import { CarModalCommonInformation } from "./CarModalCommonInformation";
+import { CarModalDescription } from "./CarModalDescription";
+import { CarModalAccessoriesInformation } from "./CarModalAccessoriesInformation";
+import { CarModalRentalInformation } from "./CarModalRentalInformation";
+import { CarModalLink } from "./CarModalLink";
+
+import { CarModalImageStyleBase } from "./CarModalStyles";
+
+const CarModal = ({ car, onClose }) => {
+  return (
+    <CarModalContainer>
+      <section>
+        <CarModalCloseButton onClose={onClose} />
+        <CarModalImageStyleBase src={car.img} alt="" />
+        <CarModalHeader make={car.make} model={car.model} year={car.year} />
+        <CarModalCommonInformation
+          address={car.address}
+          id={car.id}
+          year={car.year}
+          type={car.type}
+          fuelConsumption={car.fuelConsumption}
+          engineSize={car.engineSize}
+        />
+        <CarModalDescription description={car.description} />
+      </section>
+      <CarModalAccessoriesInformation
+        accessories={car.accessories}
+        functionalities={car.functionalities}
+      />
+      <CarModalRentalInformation
+        rentalConditions={car.rentalConditions}
+        mileage={car.mileage}
+        rentalPrice={car.rentalPrice}
+      />
+      <CarModalLink />
+    </CarModalContainer>
+  );
+};
+
+export { CarModal };
