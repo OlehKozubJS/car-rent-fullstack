e737f12eaf2f2c23e9f24cd2647c2770891595cf
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 18:30:18 +0200

    Create imports.js

---
+++ src/components/CarGallery/CarGalleryCommonInformation/imports.js
@@ -0,0 +1,11 @@
+import {
+  CarModalItemsStyleBase,
+  CarModalItemStyleBase,
+  CarModalItemValueStyleBase,
+} from "../CarModalStyles";
+
+export {
+  CarModalItemsStyleBase,
+  CarModalItemStyleBase,
+  CarModalItemValueStyleBase,
+};
