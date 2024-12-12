e7c8b1509c27830311aa90e05e5e2aa64229e4bb
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-12 12:07:56 +0200

    Update CarModalCommonInformation.jsx

--- src/components/CarModal/CarModalCommonInformation/CarModalCommonInformation.jsx
+++ src/components/CarModal/CarModalCommonInformation/CarModalCommonInformation.jsx
@@ -17,23 +17,27 @@ const CarModalCommonInformation = ({
   return (
     <CarModalItemsStyleBase>
       <CarModalItemStyleBase>{address.split(", ")[1]}</CarModalItemStyleBase>
+      <CarModalInformationDivider />
       <CarModalItemStyleBase>{address.split(", ")[2]}</CarModalItemStyleBase>
+      <CarModalInformationDivider />
       <CarModalItemStyleBase>
         Id: <CarModalItemValueStyleBase>{id}</CarModalItemValueStyleBase>
       </CarModalItemStyleBase>
+      <CarModalInformationDivider />
       <CarModalItemStyleBase>
         Year: <CarModalItemValueStyleBase>{year}</CarModalItemValueStyleBase>
       </CarModalItemStyleBase>
+      <CarModalInformationDivider />
       <CarModalItemStyleBase>
         Type: <CarModalItemValueStyleBase>{type}</CarModalItemValueStyleBase>
       </CarModalItemStyleBase>
-      <br />
       <CarModalItemStyleBase>
         Fuel Consumption:{" "}
         <CarModalItemValueStyleBase>
           {fuelConsumption}
         </CarModalItemValueStyleBase>
       </CarModalItemStyleBase>
+      <CarModalInformationDivider />
       <CarModalItemStyleBase>
         Engine Size:{" "}
         <CarModalItemValueStyleBase>{engineSize}</CarModalItemValueStyleBase>
