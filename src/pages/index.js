d94cb185cc0f5497a1ee48764550ed4d74694be4
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-13 22:08:15 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { MainPage } from "./MainPage";
 import { TestPage } from "./TestPage";
 import { MygerPage } from "./MygerPage";
+import { FavouritesPage } from "./FavouritesPage";
 
-export { MainPage, TestPage, MygerPage };
+export { MainPage, TestPage, MygerPage, FavouritesPage };
