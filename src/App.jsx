90effaa0f552592c3ee6c224ec14449cc7d46589
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-14 00:48:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
               name: "Favourites page",
               page: (
                 <FavouritesPage
-                  cars={favouriteCars.cars}
+                  cars={favouriteCars}
                   onFavouriteClick={handleFavouriteCars}
                 />
               ),
