0c77015a0812a1b79d9d7cb5784f9a08a35f3f7a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-13 23:03:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
     setCurrentPage(page);
   };
 
-  const [] = useState();
+  const [favouriteCars, setFavouriteCars] = useState();
 
   const handleFavouriteCars = () => {};
 
