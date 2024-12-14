fd94ebc8589797d5ae2109fd6b769c29ae8a9804
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-14 11:45:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, lazy, Suspense } from "react";
 
 import { carsLocalData } from "./database";
 
-import { MainPage, TestPage, MygerPage, FavouritesPage } from "./pages";
+const MainPage = lazy();
+const TestPage = lazy();
+const MygerPage = lazy();
+const FavouritesPage = lazy();
 
 import { PageNavigation } from "./components";
 
