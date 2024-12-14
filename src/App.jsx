5c090f05e27bd6669bee1976df112413f13d04c2
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-14 15:28:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,6 @@ import { PageNavigation } from "./components";
 import "./style";
 
 const App = () => {
-  const [favouriteCars, setFavouriteCars] = useState([]);
-
   const handleFavouriteCars = (carData) => {
     console.log(carData);
     setFavouriteCars([...favouriteCars, carData]);
@@ -36,57 +34,10 @@ const App = () => {
     console.log(favouriteCars);
   }, [favouriteCars]);
 
-  const [currentPage, setCurrentPage] = useState({
-    name: "Favourites page",
-    page: (
-      <FavouritesPage
-        cars={favouriteCars}
-        onFavouriteClick={handleFavouriteCars}
-      />
-    ),
-  });
-
-  const handleCurrentPage = (page) => {
-    setCurrentPage(page);
-  };
-
   return (
     <div style={{ fontFamily: "Manrope-Regular" }}>
-      <header>
-        <PageNavigation
-          pages={[
-            {
-              name: "Main page",
-              page: (
-                <MainPage
-                  cars={carsLocalData}
-                  onFavouriteClick={handleFavouriteCars}
-                />
-              ),
-            },
-            {
-              name: "Favourites page",
-              page: (
-                <FavouritesPage
-                  cars={favouriteCars}
-                  onFavouriteClick={handleFavouriteCars}
-                />
-              ),
-            },
-            {
-              name: "Test page",
-              page: <TestPage />,
-            },
-            {
-              name: "Myger page",
-              page: <MygerPage />,
-            },
-          ]}
-          onChange={handleCurrentPage}
-          currentPageName={currentPage.name}
-        />
-      </header>
-      <main>{currentPage.page}</main>
+      <header></header>
+      <main></main>
     </div>
   );
 };
