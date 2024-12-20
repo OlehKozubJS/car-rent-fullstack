e662e425c33b77a9dea611c74944dc60e4e21e3c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-21 01:32:51 +0200

    Update RouterLayout.jsx

--- src/components/RouterLayout/RouterLayout.jsx
+++ src/components/RouterLayout/RouterLayout.jsx
@@ -9,22 +9,7 @@ const RouterLayout = () => {
   return (
     <div>
       <header>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Car Rent</NavLink>
-            </li>
-            <li>
-              <NavLink to="/FavouriteCars">Favourite Cars</NavLink>
-            </li>
-            <li>
-              <NavLink to="/TestOne">Test One</NavLink>
-            </li>
-            <li>
-              <NavLink to="TestTwo">Test Two</NavLink>
-            </li>
-          </ul>
-        </nav>
+        <RouterNavigation />
       </header>
       <main>
         <Suspense downfall={<section>loading...</section>}>
