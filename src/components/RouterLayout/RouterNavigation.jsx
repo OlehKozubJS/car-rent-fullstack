f6720690d39f3c40460e18a53a052de97708332d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-21 00:59:52 +0200

    Update RouterNavigation.jsx

---
+++ src/components/RouterLayout/RouterNavigation.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState, lazy, Suspense } from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+
+import { carsLocalData } from "./database";
+
+const [MainPage, FavouriteCarsPage, TestPageOne, TestPageTwo] = [
+  "MainPage",
+  "FavouriteCarsPage",
+  "TestPageOne",
+  "TestPageTwo",
+].map((pageName) => {
+  return lazy(() => import(`./pages/${pageName}`));
+});
+
+import "./style";
+
+const RouterLayout = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Car Rent</NavLink>
+            </li>
+            <li>
+              <NavLink to="/FavouriteCars">Favourite Cars</NavLink>
+            </li>
+            <li>
+              <NavLink to="/TestOne">Test One</NavLink>
+            </li>
+            <li>
+              <NavLink to="TestTwo">Test Two</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Suspense downfall={<p>loading...</p>}>
+          <Outlet></Outlet>
+        </Suspense>
+      </main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export { RouterLayout };
