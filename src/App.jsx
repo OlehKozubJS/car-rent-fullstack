ec9cab87568e9e169c22ef33643c59d2ba15bcf9
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-21 02:03:41 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,53 +30,25 @@ const App = () => {
   };
 
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Car Rent</NavLink>
-            </li>
-            <li>
-              <NavLink to="/FavouriteCars">Favourite Cars</NavLink>
-            </li>
-            <li>
-              <NavLink to="/TestOne">Test One</NavLink>
-            </li>
-            <li>
-              <NavLink to="TestTwo">Test Two</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Suspense downfall={<p>loading...</p>}>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <MainPage
-                  cars={carsLocalData}
-                  onFavouriteClick={addFavouriteCar}
-                />
-              }
-            />
-            <Route
-              path="/FavouriteCars"
-              element={
-                <FavouriteCarsPage
-                  cars={favouriteCars}
-                  onFavouriteClick={removeFavouriteCar}
-                />
-              }
-            />
-            <Route path="/TestOne" element={<TestPageOne />} />
-            <Route path="/TestTwo" element={<TestPageTwo />} />
-          </Routes>
-        </Suspense>
-      </main>
-      <footer></footer>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <MainPage cars={carsLocalData} onFavouriteClick={addFavouriteCar} />
+        }
+      />
+      <Route
+        path="/FavouriteCars"
+        element={
+          <FavouriteCarsPage
+            cars={favouriteCars}
+            onFavouriteClick={removeFavouriteCar}
+          />
+        }
+      />
+      <Route path="/TestOne" element={<TestPageOne />} />
+      <Route path="/TestTwo" element={<TestPageTwo />} />
+    </Routes>
   );
 };
 
