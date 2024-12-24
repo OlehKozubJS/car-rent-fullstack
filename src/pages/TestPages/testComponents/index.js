1ec725ec0dfaedb9dd45b09d396b7ab1c1f4fedd
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 01:40:12 +0200

    Update index.js

--- src/pages/TestPages/testComponents/index.js
+++ src/pages/TestPages/testComponents/index.js
@@ -1,5 +1,6 @@
 import { CarFilterTest } from "./CarFilterTest";
 import { FibonacciTest } from "./FibonacciTest";
 import { PrimeNumbersTest } from "./PrimeNumbersTest";
+import { DateAndTimeTest } from "./DateAndTimeTest";
 
-export { CarFilterTest, FibonacciTest, PrimeNumbersTest };
+export { CarFilterTest, FibonacciTest, PrimeNumbersTest, DateAndTimeTest };
