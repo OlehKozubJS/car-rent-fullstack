7596db3a41977eb9b95450e00e3a34bb587d7a74
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 01:55:55 +0200

    Update SquaresTest.jsx

--- src/pages/TestPages/testComponents/SquaresTest/SquaresTest.jsx
+++ src/pages/TestPages/testComponents/SquaresTest/SquaresTest.jsx
@@ -1,12 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { SVGimage } from "./imports";
-
-const TestPageOne = () => {
-  const varNameToString = (objectName) => {
-    return Object.keys(objectName)[0];
-  };
-
+const SquaresTest = () => {
   const [squares, setSquares] = useState([]);
 
   const getSquare = () => {
@@ -17,20 +11,7 @@ const TestPageOne = () => {
 
   useEffect(getSquare, [squares]);
 
-  return (
-    <div>
-      <p>{squares.join(", ")}</p>
-
-      <span style={{ fontFamily: "manrope-regular" }}>Regular</span>
-      <span style={{ fontFamily: "manrope-medium" }}>Medium</span>
-      <span style={{ fontFamily: "manrope-semibold" }}>Semibold</span>
-      <span style={{ fontFamily: "montserrat-regular" }}>Regular</span>
-      <span style={{ fontFamily: "montserrat-semibold" }}>Semibold</span>
-      <div style={{ backgroundColor: "black" }}>
-        <SVGimage name="fav" />
-      </div>
-    </div>
-  );
+  return <p>{squares.join(", ")}</p>;
 };
 
-export default TestPageOne;
+export { SquaresTest };
