82e15a0bbcc277f3e64b6a1c790d4e66c57975d7
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-24 23:58:05 +0200

    Update CarFilterTest.jsx

--- src/pages/TestPages/testComponents/CarFilterTest.jsx
+++ src/pages/TestPages/testComponents/CarFilterTest.jsx
@@ -125,74 +125,59 @@ const TestPageOne = () => {
   };
 
   return (
-    <div>
-      {String(date)}
-      <p>{time}</p>
-      <p>{fibonacciArray.join(", ")}</p>
-      <p>{primeNumbersArray.join(", ")}</p>
-      <p>{squares.join(", ")}</p>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <h3>Car brand (make)</h3>
-          <label htmlFor="carMakeInput">
-            <input
-              id="carMakeInput"
-              type="text"
-              placeholder="Enter car make name or its part"
-              onChange={handleMake}
-              style={{ width: "200px" }}
-            />
-          </label>
-        </div>
-        <div>
-          <h3>Car rental price</h3>
-          <label htmlFor="minCarRentalPriceInput">
-            <input
-              id="minCarRentalPriceInput"
-              type="number"
-              placeholder="From"
-              onChange={handleMinimalRentalPrice}
-            />
-          </label>
-          <label htmlFor="maxCarRentalPriceInput">
-            <input
-              id="maxCarRentalPriceInput"
-              type="number"
-              placeholder="To"
-              onChange={handleMaximalRentalPrice}
-            />
-          </label>
-        </div>
-        <div>
-          <h3>Car mileage</h3>
-          <label htmlFor="minCarMileageInput">
-            <input
-              id="minCarMileageInput"
-              type="number"
-              placeholder="From"
-              onChange={handleMinimalMileage}
-            />
-          </label>
-          <label htmlFor="maxCarMileageInput">
-            <input
-              id="maxCarMileageInput"
-              type="number"
-              placeholder="To"
-              onChange={handleMaximalMileage}
-            />
-          </label>
-        </div>
-        <button type="submit">Find car</button>
-      </form>
-      <span style={{ fontFamily: "manrope-regular" }}>Regular</span>
-      <span style={{ fontFamily: "manrope-medium" }}>Medium</span>
-      <span style={{ fontFamily: "manrope-semibold" }}>Semibold</span>
-      <span style={{ fontFamily: "montserrat-regular" }}>Regular</span>
-      <span style={{ fontFamily: "montserrat-semibold" }}>Semibold</span>
-      <div style={{ backgroundColor: "black" }}>
-        <SVGimage name="fav" />
+    <form onSubmit={handleSubmit}>
+      <div>
+        <h3>Car brand (make)</h3>
+        <label htmlFor="carMakeInput">
+          <input
+            id="carMakeInput"
+            type="text"
+            placeholder="Enter car make name or its part"
+            onChange={handleMake}
+            style={{ width: "200px" }}
+          />
+        </label>
       </div>
-    </div>
+      <div>
+        <h3>Car rental price</h3>
+        <label htmlFor="minCarRentalPriceInput">
+          <input
+            id="minCarRentalPriceInput"
+            type="number"
+            placeholder="From"
+            onChange={handleMinimalRentalPrice}
+          />
+        </label>
+        <label htmlFor="maxCarRentalPriceInput">
+          <input
+            id="maxCarRentalPriceInput"
+            type="number"
+            placeholder="To"
+            onChange={handleMaximalRentalPrice}
+          />
+        </label>
+      </div>
+      <div>
+        <h3>Car mileage</h3>
+        <label htmlFor="minCarMileageInput">
+          <input
+            id="minCarMileageInput"
+            type="number"
+            placeholder="From"
+            onChange={handleMinimalMileage}
+          />
+        </label>
+        <label htmlFor="maxCarMileageInput">
+          <input
+            id="maxCarMileageInput"
+            type="number"
+            placeholder="To"
+            onChange={handleMaximalMileage}
+          />
+        </label>
+      </div>
+      <button type="submit">Find car</button>
+    </form>
   );
 };
 
