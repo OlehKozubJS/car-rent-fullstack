89130b0cc1a64555c9e9c32e006ccbb05b6e9b33
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 01:32:56 +0200

    Create index.js

---
+++ src/pages/TestPages/testComponents/DateAndTimeTest/index.js
@@ -0,0 +1,5 @@
+import { CarFilterTest } from "./CarFilterTest";
+import { FibonacciTest } from "./FibonacciTest";
+import { PrimeNumbersTest } from "./PrimeNumbersTest";
+
+export { CarFilterTest, FibonacciTest, PrimeNumbersTest };
