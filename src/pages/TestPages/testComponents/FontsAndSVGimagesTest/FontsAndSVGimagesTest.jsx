f58a25928691a291a8f93480223fc1c56bf253bb
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 01:53:22 +0200

    Update FontsAndSVGimagesTest.jsx

--- src/pages/TestPages/testComponents/FontsAndSVGimagesTest/FontsAndSVGimagesTest.jsx
+++ src/pages/TestPages/testComponents/FontsAndSVGimagesTest/FontsAndSVGimagesTest.jsx
@@ -1,26 +1,8 @@
-import { useState, useEffect } from "react";
-
 import { SVGimage } from "./imports";
 
-const TestPageOne = () => {
-  const varNameToString = (objectName) => {
-    return Object.keys(objectName)[0];
-  };
-
-  const [squares, setSquares] = useState([]);
-
-  const getSquare = () => {
-    if (squares.length < 10) {
-      setSquares([...squares, Math.pow(squares.length, 2)]);
-    }
-  };
-
-  useEffect(getSquare, [squares]);
-
+const FontsAndSVGimagesTest = () => {
   return (
     <div>
-      <p>{squares.join(", ")}</p>
-
       <span style={{ fontFamily: "manrope-regular" }}>Regular</span>
       <span style={{ fontFamily: "manrope-medium" }}>Medium</span>
       <span style={{ fontFamily: "manrope-semibold" }}>Semibold</span>
@@ -33,4 +15,4 @@ const TestPageOne = () => {
   );
 };
 
-export default TestPageOne;
+export default FontsAndSVGimagesTest;
