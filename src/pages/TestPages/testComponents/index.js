43ae598284c9e7c50795fcf5bfedd3b613d09e7b
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 03:35:26 +0200

    Update index.js

--- src/pages/TestPages/testComponents/index.js
+++ src/pages/TestPages/testComponents/index.js
@@ -6,7 +6,7 @@ import { FontsAndSVGimagesTest } from "./FontsAndSVGimagesTest";
 import { SquaresTest } from "./SquaresTest";
 import { StarsEffectTest } from "./StarsEffectTest";
 import { PageNavigationTest } from "./PageNavigationTest";
-//import {} from "./TestPageLayout";
+import { TestPageLayout } from "./TestPageLayout";
 
 export {
   CarFilterTest,
@@ -17,4 +17,5 @@ export {
   SquaresTest,
   StarsEffectTest,
   PageNavigationTest,
+  TestPageLayout,
 };
