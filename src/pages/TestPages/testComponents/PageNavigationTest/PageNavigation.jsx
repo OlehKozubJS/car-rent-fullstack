4c0bd635257455529f01c4e701882703f390ae4c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 02:03:38 +0200

    Create PageNavigation.jsx

---
+++ src/pages/TestPages/testComponents/PageNavigationTest/PageNavigation.jsx
@@ -0,0 +1,31 @@
+const PageNavigation = ({ pages, onChange, currentPageName }) => {
+  const handleCurrentPage = (event) => {
+    onChange(pages.find(({ name }) => event.target.value === name));
+  };
+
+  return (
+    <nav>
+      <ul>
+        {pages.map(({ name }) => {
+          return (
+            <li key={name}>
+              <label htmlFor={name}>
+                <input
+                  id={name}
+                  name="pageSwitcher"
+                  type="radio"
+                  onChange={handleCurrentPage}
+                  value={name}
+                  checked={currentPageName === name}
+                />
+                <span>{name}</span>
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export { PageNavigation };
