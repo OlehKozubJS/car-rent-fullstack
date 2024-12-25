b9aac336fad84190c4af9bad541fed058756fac3
OlehKozubJS <122114245+OlehKozubJS@users.noreply.github.com>
2024-12-25 02:20:58 +0000

    Deploying to gh-pages from @ OlehKozubJS/car-rent-fullstack@068ba4bed43b0ff860942ebb9807ef741eadfa60 🚀

---
+++ static/js/788.ca64db1d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rent_fullstack=self.webpackChunkcar_rent_fullstack||[]).push([[788,60],{7788:(r,e,s)=>{s.r(e),s.d(e,{MoirTest:()=>c});var t=s(6060),a=s(3329);const c=()=>(0,a.jsx)("div",{children:(0,a.jsx)(t.StripedSquare,{})})},6060:(r,e,s)=>{s.r(e),s.d(e,{StripedSquare:()=>a});var t=s(3329);const a=()=>(0,t.jsx)("div",{children:"Striped square"})}}]);
+//# sourceMappingURL=788.ca64db1d.chunk.js.map
