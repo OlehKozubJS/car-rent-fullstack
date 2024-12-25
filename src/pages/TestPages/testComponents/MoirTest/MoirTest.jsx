33c53da7d7b7fc91d314ede8807a9cc0db086ad6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 05:08:19 +0200

    Update MoirTest.jsx

--- src/pages/TestPages/testComponents/MoirTest/MoirTest.jsx
+++ src/pages/TestPages/testComponents/MoirTest/MoirTest.jsx
@@ -2,6 +2,10 @@ import styled from "@emotion/styled";
 
 import { StripedSquare } from "./StripedSquare";
 
+const MoirTestStyleBase = styled.div`
+  postion: relative;
+`;
+
 const MoirTest = () => {
   return (
     <div>
