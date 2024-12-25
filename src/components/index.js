71cc5067a53ffecfba085561733bf526ca134f53
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 02:07:24 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import { CarFilter } from "./CarFilter";
 import { CarGallery } from "./CarGallery";
 import { CarModal } from "./CarModal";
-import { PageNavigation } from "./PageNavigation";
 import { RouterLayout } from "./RouterLayout";
 
-export { CarFilter, CarGallery, CarModal, PageNavigation, RouterLayout };
+export { CarFilter, CarGallery, CarModal, RouterLayout };
