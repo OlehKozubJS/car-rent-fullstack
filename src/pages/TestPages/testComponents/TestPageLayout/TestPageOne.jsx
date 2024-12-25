a9e28e3047f788ad1b1b7feb16d94cd25032ef3d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 03:16:42 +0200

    Update TestPageOne.jsx

--- src/pages/TestPages/testComponents/TestPageLayout/TestPageOne.jsx
+++ src/pages/TestPages/testComponents/TestPageLayout/TestPageOne.jsx
@@ -1,15 +1,5 @@
 import { Routes, Route, Outlet, NavLink } from "react-router-dom";
 
-import {
-  CarFilterTest,
-  DateAndTimeTest,
-  FibonacciTest,
-  FontsAndSVGimagesTest,
-  PrimeNumbersTest,
-  StarsEffectTest,
-  SquaresTest,
-} from "./testComponents";
-
 const TestPageOne = () => {
   const varNameToString = (objectName) => {
     return Object.keys(objectName)[0];
@@ -28,25 +18,9 @@ const TestPageOne = () => {
           <NavLink to="moir">Moir test</NavLink>
         </li>
       </ul>
-      <Routes>
-        <Route path="/" element={<Outlet />}>
-          <Route
-            path="tests"
-            element={
-              <div>
-                <CarFilterTest />
-                <DateAndTimeTest />
-                <FibonacciTest />
-                <FontsAndSVGimagesTest />
-                <PrimeNumbersTest />
-                <SquaresTest />
-              </div>
-            }
-          />
-          <Route path="stars" element={<StarsEffectTest />} />
-          <Route path="moir" element={<div>Moir</div>} />
-        </Route>
-      </Routes>
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 };
