6169cbb584edd40f40b681f8da771959a4d18923
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 05:01:01 +0200

    Update StripedSquare.jsx

--- src/pages/TestPages/testComponents/MoirTest/StripedSquare.jsx
+++ src/pages/TestPages/testComponents/MoirTest/StripedSquare.jsx
@@ -6,16 +6,14 @@ const SquareStyleBase = styled.div`
   height: 250px;
   width: 250px;
 
-  background-color: green;
-
   display: flex;
   flex-direction: column;
 `;
 
 const StripeStyleBase = styled.div`
-  height: 250px;
-  width: 10px;
-  background-color: ${({ color }) => {
+  height: 10px;
+  width: 250px;
+  background: ${({ color }) => {
     return color;
   }};
 `;
@@ -24,13 +22,16 @@ const StripedSquare = () => {
   const [stripes, setStripes] = useState([]);
 
   useEffect(() => {
-    if (stripes.length >= 10) {
+    if (stripes.length >= 25) {
       return;
     }
 
     setStripes([
       ...stripes,
-      <StripeStyleBase key={stripes.length + 1}>
+      <StripeStyleBase
+        key={stripes.length + 1}
+        color={stripes.length % 2 === 0 ? "blue" : "none"}
+      >
         {stripes.length}
       </StripeStyleBase>,
     ]);
