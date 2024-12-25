f46504cc2909dcbb42ac5968438eea9a03861af7
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-25 04:27:12 +0200

    Update StripedSquare.jsx

--- src/pages/TestPages/testComponents/MoirTest/StripedSquare.jsx
+++ src/pages/TestPages/testComponents/MoirTest/StripedSquare.jsx
@@ -3,6 +3,10 @@ import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 
 const StripedSquare = () => {
+  const [stripes, setStripes] = useState([]);
+
+  useEffect(() => {}, []);
+
   return <div>Striped square</div>;
 };
 
