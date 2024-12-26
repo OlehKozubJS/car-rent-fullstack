b677b7a492ec8385c1b25fe0075c7510d2f3b3d6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-26 23:33:29 +0200

    Update MoirTest.jsx

--- src/pages/TestPages/testComponents/MoirTest/MoirTest.jsx
+++ src/pages/TestPages/testComponents/MoirTest/MoirTest.jsx
@@ -17,58 +17,10 @@ const MoirTest = () => {
 
   useEffect(useKeyboard(setTop(top - 5), "w"), [top]);
   useEffect(useKeyboard(setTop(top + 5), "s"), [top]);
-  useEffect(useKeyboard(setLeft()), [left]);
-
-  useEffect(() => {
-    const changeTop = (event) => {
-      if (event.type === "keydown" && event.key === "w") {
-        setTop(top - 5);
-      }
-      if (event.type === "keydown" && event.key === "s") {
-        setTop(top + 5);
-      }
-    };
-
-    window.addEventListener("keydown", changeTop);
-
-    return () => {
-      window.removeEventListener("keydown", changeTop);
-    };
-  }, [top]);
-
-  useEffect(() => {
-    const changeLeft = (event) => {
-      if (event.type === "keydown" && event.key === "a") {
-        setLeft(left - 5);
-      }
-      if (event.type === "keydown" && event.key === "d") {
-        setLeft(left + 5);
-      }
-    };
-
-    window.addEventListener("keydown", changeLeft);
-
-    return () => {
-      window.removeEventListener("keydown", changeLeft);
-    };
-  }, [left]);
-
-  useEffect(() => {
-    const changeAngle = (event) => {
-      if (event.type === "keydown" && event.key === "q") {
-        setAngle(angle - 5);
-      }
-      if (event.type === "keydown" && event.key === "e") {
-        setAngle(angle + 5);
-      }
-    };
-
-    window.addEventListener("keydown", changeAngle);
-
-    return () => {
-      window.removeEventListener("keydown", changeAngle);
-    };
-  }, [angle]);
+  useEffect(useKeyboard(setLeft(left - 5), "a"), [left]);
+  useEffect(useKeyboard(setLeft(left - 5), "a"), [left]);
+  useEffect(useKeyboard(setAngle(angle - 5), "q"), [angle]);
+  useEffect(usekeyboard(setAngle(angle + 5), "e"), [angle]);
 
   return (
     <MoirTestStyleBase>
