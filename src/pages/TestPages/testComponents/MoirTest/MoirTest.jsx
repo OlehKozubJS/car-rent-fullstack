2f173c4b5634a2c20f180edb881d8d4c136d74dc
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-26 22:05:49 +0200

    Update MoirTest.jsx

--- src/pages/TestPages/testComponents/MoirTest/MoirTest.jsx
+++ src/pages/TestPages/testComponents/MoirTest/MoirTest.jsx
@@ -67,7 +67,7 @@ const MoirTest = () => {
   return (
     <MoirTestStyleBase>
       <StripedSquare top={top} left={left} angle={angle} />
-      <StripedSquare top={300} left={300} />
+      <StripedSquare top={300} left={300} angle={90} height={500} width={500} />
     </MoirTestStyleBase>
   );
 };
