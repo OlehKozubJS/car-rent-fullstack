aa56f6287159264b64640315fb72a0f4520b21d0
OlehKozubJS <122114245+OlehKozubJS@users.noreply.github.com>
2024-12-26 21:12:18 +0000

    Deploying to gh-pages from @ OlehKozubJS/car-rent-fullstack@91bae83454b8ba4434b6b59e52f567c285047d5a 🚀

---
+++ static/js/543.40e4853f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rent_fullstack=self.webpackChunkcar_rent_fullstack||[]).push([[543],{543:(e,n,t)=>{t.r(n),t.d(n,{useKeyboard:()=>r});const r=(e,n)=>()=>{const t=t=>{"keydown"===t.type&&t.key===e&&n(parameters)};return window.addEventListener("keydown",t),()=>{window.removeEventListener("keydown",t)}}}}]);
+//# sourceMappingURL=543.40e4853f.chunk.js.map
