c0b1656aa3d4494f2e623ff49d656e20d5d6f52d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-26 22:58:38 +0200

    Update useKeyboard.js

--- src/pages/TestPages/testHooks/useKeyboard.js
+++ src/pages/TestPages/testHooks/useKeyboard.js
@@ -1,6 +1,10 @@
-const useKeyboard = (callback, keyName) => {
+const useKeyboard = (callback, key) => {
   return () => {
-    const keydownCallback = () => {};
+    const keydownCallback = (event) => {
+      if (event.type === "keydown" && event.key === key) {
+        callback();
+      }
+    };
 
     window.addEventListener("keydown", keydownCallback);
 
