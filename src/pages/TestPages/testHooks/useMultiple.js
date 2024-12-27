4f79abdc7e1862749a4504692aed952fe7df7b4d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-27 14:15:24 +0200

    Update useMultiple.js

--- src/pages/TestPages/testHooks/useMultiple.js
+++ src/pages/TestPages/testHooks/useMultiple.js
@@ -1,3 +1,7 @@
-const useMultiple = () => {};
+const useMultiple = (iterations, step, callback) => {
+  for (let iteration = 1; iteration < iterations; iteration += step) {
+    callback();
+  }
+};
 
 export { useMultiple };
