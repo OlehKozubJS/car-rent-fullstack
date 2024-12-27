7529c66d8a63d2ddd003c129c1de30c84d3d339d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-27 14:43:09 +0200

    Update StripedSquare.jsx

--- src/pages/TestPages/testComponents/MoirTest/StripedSquare.jsx
+++ src/pages/TestPages/testComponents/MoirTest/StripedSquare.jsx
@@ -47,6 +47,7 @@ const StripeStyleBase = styled.div`
 
 const StripedSquare = ({ height, width, top, left, angle }) => {
   const [stripes, setStripes] = useState([]);
+  const [number, setNumber] = useState(0);
 
   useMultiple(height / 5, 1, () => {
     setStripes([
@@ -56,6 +57,10 @@ const StripedSquare = ({ height, width, top, left, angle }) => {
         color={stripes.length % 2 === 0 ? "rgba(0, 0, 250, 0.5)" : "none"}
       ></StripeStyleBase>,
     ]);
+  });
+
+  useMultiple(10, 10, () => {
+    console.log(number);
   });
 
   return (
