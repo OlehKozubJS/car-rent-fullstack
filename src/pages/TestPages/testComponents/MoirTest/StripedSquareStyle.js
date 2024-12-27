5a80c04db8ef863ab24a06a4aa97e71d6cd099d2
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-27 15:55:18 +0200

    Create StripedSquareStyle.js

---
+++ src/pages/TestPages/testComponents/MoirTest/StripedSquareStyle.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+
+import { useMultiple } from "../imports";
+
+import styled from "@emotion/styled";
+
+const SquareStyleBase = styled.div`
+  position: absolute;
+
+  top: ${({ top }) => {
+    return top;
+  }}px;
+
+  left: ${({ left }) => {
+    return left;
+  }}px;
+
+  transform: rotate(
+    ${({ angle }) => {
+      return angle;
+    }}deg
+  );
+
+  height: ${({ height }) => {
+    return height;
+  }}px;
+
+  width: ${({ width }) => {
+    return width;
+  }}px;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const StripeStyleBase = styled.div`
+  height: 5px;
+
+  width: ${({ width }) => {
+    return width;
+  }}px;
+
+  background: ${({ color }) => {
+    return color;
+  }};
+`;
+
+const StripedSquare = ({ height, width, top, left, angle }) => {
+  let newStripes = [];
+
+  useMultiple(height / 5, 1, (number) => {
+    setNewStripes([
+      ...newStripes,
+      <StripeStyleBase
+        key={number + 1}
+        color={number % 2 === 0 ? "rgba(0, 0, 250, 0.5)" : "none"}
+        width={width}
+      ></StripeStyleBase>,
+    ]);
+  });
+
+  return (
+    <SquareStyleBase
+      top={top}
+      left={left}
+      angle={angle}
+      height={height}
+      width={width}
+    >
+      {newStripes}
+    </SquareStyleBase>
+  );
+};
+
+export { StripedSquare };
