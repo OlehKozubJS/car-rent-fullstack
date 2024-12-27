b1d47f3352a3e0f584c14a9edf1adebb5e40cfbc
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-27 15:56:16 +0200

    Update StripedSquareStyle.js

--- src/pages/TestPages/testComponents/MoirTest/StripedSquareStyle.js
+++ src/pages/TestPages/testComponents/MoirTest/StripedSquareStyle.js
@@ -1,7 +1,3 @@
-import { useState, useEffect } from "react";
-
-import { useMultiple } from "../imports";
-
 import styled from "@emotion/styled";
 
 const SquareStyleBase = styled.div`
@@ -45,31 +41,4 @@ const StripeStyleBase = styled.div`
   }};
 `;
 
-const StripedSquare = ({ height, width, top, left, angle }) => {
-  let newStripes = [];
-
-  useMultiple(height / 5, 1, (number) => {
-    setNewStripes([
-      ...newStripes,
-      <StripeStyleBase
-        key={number + 1}
-        color={number % 2 === 0 ? "rgba(0, 0, 250, 0.5)" : "none"}
-        width={width}
-      ></StripeStyleBase>,
-    ]);
-  });
-
-  return (
-    <SquareStyleBase
-      top={top}
-      left={left}
-      angle={angle}
-      height={height}
-      width={width}
-    >
-      {newStripes}
-    </SquareStyleBase>
-  );
-};
-
-export { StripedSquare };
+export { SquareStyleBase, StripeStyleBase };
