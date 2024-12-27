3e7260861f38352b272c63108f1ef97b2afa2aab
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-27 23:23:26 +0200

    Create TestPage.jsx

---
+++ src/pages/TestPage/TestPage.jsx
@@ -0,0 +1,65 @@
+import { Routes, Route, Outlet, NavLink } from "react-router-dom";
+
+import {
+  CarFilterTest,
+  DateAndTimeTest,
+  FibonacciTest,
+  FontsAndSVGimagesTest,
+  PrimeNumbersTest,
+  StarsEffectTest,
+  SquaresTest,
+  TestPageLayout,
+  MoirTest,
+} from "./testComponents";
+
+const TestPage = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<TestPageLayout />}>
+        <Route
+          path="tests"
+          element={
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <NavLink to="dateAndTime">Date and time</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="fibonacci">Fibonacci</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="primeNumbers">Prime numbers</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="squares">Squares</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="fontsAndSVGimages">
+                      Fonts and SVG images
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="carFilter">Car filter</NavLink>
+                  </li>
+                </ul>
+              </nav>
+              <Outlet />
+            </div>
+          }
+        >
+          <Route path="dateAndTime" element={<DateAndTimeTest />} />
+          <Route path="fibonacci" element={<FibonacciTest />} />
+          <Route path="primeNumbers" element={<PrimeNumbersTest />} />
+          <Route path="squares" element={<SquaresTest />} />
+          <Route path="fontsAndSVGimages" element={<FontsAndSVGimagesTest />} />
+          <Route path="carFilter" element={<CarFilterTest />} />
+        </Route>
+        <Route path="stars" element={<StarsEffectTest />} />
+        <Route path="moir" element={<MoirTest />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default TestPage;
