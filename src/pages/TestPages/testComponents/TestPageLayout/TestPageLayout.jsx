739181ee228bc5b1f87c8c0ca11d7aa47afaf1cf
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-27 22:59:35 +0200

    Update TestPageLayout.jsx

--- src/pages/TestPages/testComponents/TestPageLayout/TestPageLayout.jsx
+++ src/pages/TestPages/testComponents/TestPageLayout/TestPageLayout.jsx
@@ -3,20 +3,20 @@ import { Outlet, NavLink } from "react-router-dom";
 const TestPageLayout = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <NavLink to="tests">Various tests</NavLink>
-        </li>
-        <li>
-          <NavLink to="stars">Stars effect test</NavLink>
-        </li>
-        <li>
-          <NavLink to="moir">Moir test</NavLink>
-        </li>
-      </ul>
-      <div>
-        <Outlet />
-      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="tests">Various tests</NavLink>
+          </li>
+          <li>
+            <NavLink to="stars">Stars effect test</NavLink>
+          </li>
+          <li>
+            <NavLink to="moir">Moir test</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
     </div>
   );
 };
