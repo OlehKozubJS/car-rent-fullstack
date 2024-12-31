d3331c3d8f7b275fda86db1ca2c88843cacfd479
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-31 20:44:01 +0200

    Update SimplestCustomRange.jsx

--- src/pages/SimplestCustomRange.jsx
+++ src/pages/SimplestCustomRange.jsx
@@ -3,7 +3,28 @@ import { useEffect, useState } from "react";
 import { handleEventListener } from "./hooks";
 
 import { RangeField, RangeDial } from "./style";
+/*
+.RangeField {
+  width: 500px;
+  height: 50px;
+  background-color: rgb(200, 200, 250);
 
+  position: relative;
+}
+
+.RangeDial {
+  position: absolute;
+  top: 0;
+
+  width: 50px;
+  height: 50px;
+  background-color: rgb(100, 100, 250);
+}
+
+.RangeIndicator {
+  color: rgb(0, 0, 250);
+}
+*/
 const SimplestCustomRange = ({ value, onChange }) => {
   const [isDraggable, setIsDraggable] = useState(false);
   const [rangeValue, setRangeValue] = useState(value);
