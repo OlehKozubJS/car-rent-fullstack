dcbf42672b4eeac35c1255be862fbd2d0e0e1437
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-01 02:12:44 +0200

    Update TestPage.jsx

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -3,7 +3,11 @@ import { useEffect, useState } from "react";
 import { LogicGates, SimplestCustomRange } from "./test";
 
 const TestPage = () => {
-  const [] = useState();
+  const [rangeValue, setRangeValue] = useState(0);
+
+  const handleRangeValue = (newRangeValue) => {
+    setRangeValue(newRangeValue);
+  };
 
   return (
     <section>
