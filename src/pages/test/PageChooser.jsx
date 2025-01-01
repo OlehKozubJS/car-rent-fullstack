31f519b7f4cea14a0fd7a39370ab6cc4feb36504
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-01 03:34:32 +0200

    Create PageChooser.jsx

---
+++ src/pages/test/PageChooser.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+import { LogicGates, SimplestCustomRange } from "./test";
+
+const TestPage = () => {
+  const [componentName, setComponentName] = useState("Simplest custom range");
+  const [rangeValue, setRangeValue] = useState(0);
+
+  const handleRangeValue = (newRangeValue) => {
+    setRangeValue(newRangeValue);
+  };
+
+  const chooseComponent = ({ target }) => {
+    setComponentName(target.value);
+  };
+
+  return (
+    <section>
+      <ul>
+        {["Simplest custom range", "Logic gates"].map((value) => {
+          return (
+            <li key={value}>
+              <label htmlFor={value}>
+                <input
+                  id={value}
+                  name="chooseComponentRadio"
+                  type="radio"
+                  value={value}
+                  onChange={chooseComponent}
+                  checked={componentName === value}
+                />
+                <span>{value}</span>
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+      {componentName === "Simplest custom range" && (
+        <SimplestCustomRange onChange={handleRangeValue} value={rangeValue} />
+      )}
+      {componentName === "Logic gates" && <LogicGates />}
+      <p>{rangeValue}</p>
+    </section>
+  );
+};
+
+export default TestPage;
