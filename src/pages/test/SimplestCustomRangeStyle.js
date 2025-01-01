c48e0ea322e73051151fc2797ffef41553e8a847
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-01 02:45:02 +0200

    Update SimplestCustomRangeStyle.js

--- src/pages/test/SimplestCustomRangeStyle.js
+++ src/pages/test/SimplestCustomRangeStyle.js
@@ -1,7 +1,3 @@
-import { useEffect, useState } from "react";
-
-import { handleEventListener } from "../../hooks";
-
 import styled from "@emotion/styled";
 
 const RangeField = styled.div`
@@ -23,43 +19,4 @@ const RangeDial = styled.div`
   background-color: rgb(100, 100, 250);
 `;
 
-const SimplestCustomRange = ({ value, onChange }) => {
-  const [isDraggable, setIsDraggable] = useState(false);
-  const [rangeValue, setRangeValue] = useState(value);
-  const [mouseDownX, setMouseDownX] = useState(0);
-
-  const handleMouseDown = ({ clientX }) => {
-    setIsDraggable(true);
-    setMouseDownX(clientX);
-  };
-
-  const handleMouseMove = ({ clientX }) => {
-    if (isDraggable) {
-      const newRangeValue = rangeValue + clientX - mouseDownX;
-
-      if (newRangeValue >= 0 && newRangeValue <= 450) {
-        setRangeValue(newRangeValue);
-      }
-    }
-  };
-
-  const handleMouseUp = () => {
-    setIsDraggable(false);
-  };
-
-  useEffect(handleEventListener("mousemove", handleMouseMove), [isDraggable]);
-
-  useEffect(handleEventListener("mouseup", handleMouseUp), [isDraggable]);
-
-  useEffect(() => {
-    onChange(rangeValue);
-  }, [rangeValue]);
-
-  return (
-    <RangeField>
-      <RangeDial onMouseDown={handleMouseDown} left={rangeValue}></RangeDial>
-    </RangeField>
-  );
-};
-
-export { SimplestCustomRange };
+export { RangeField, RangeDial };
