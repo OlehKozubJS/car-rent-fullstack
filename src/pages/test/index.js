7ccf5b68552ff76cd9c0c43c8b2f917dc506da4e
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-01 17:43:05 +0200

    Update index.js

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,5 +1,6 @@
 import { LogicGates } from "./LogicGates";
 import { SimplestCustomRange } from "./SimplestCustomRange";
 import { PageChooser } from "./PageChooser";
+import { DraggableComponent } from "./DraggableComponent";
 
-export { LogicGates, SimplestCustomRange, PageChooser };
+export { LogicGates, SimplestCustomRange, PageChooser, DraggableComponent };
