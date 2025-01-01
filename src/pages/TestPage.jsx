e81246b4258aded607404f5070cbd1cf199ab9be
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-01 03:43:23 +0200

    Update TestPage.jsx

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -16,25 +16,11 @@ const TestPage = () => {
 
   return (
     <section>
-      <ul>
-        {["Simplest custom range", "Logic gates"].map((value) => {
-          return (
-            <li key={value}>
-              <label htmlFor={value}>
-                <input
-                  id={value}
-                  name="chooseComponentRadio"
-                  type="radio"
-                  value={value}
-                  onChange={chooseComponent}
-                  checked={componentName === value}
-                />
-                <span>{value}</span>
-              </label>
-            </li>
-          );
-        })}
-      </ul>
+      <PageChooser
+        componentNames={["Simplest custom range", "Logic gates"]}
+        currentComponentName={componentName}
+        onChange={chooseComponent}
+      />
       {componentName === "Simplest custom range" && (
         <SimplestCustomRange onChange={handleRangeValue} value={rangeValue} />
       )}
