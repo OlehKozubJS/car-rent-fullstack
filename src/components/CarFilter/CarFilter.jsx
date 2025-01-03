772dd1c13f1ead26ab4772263f6a57a95d5ff044
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-03 23:27:19 +0200

    Update CarFilter.jsx

--- src/components/CarFilter/CarFilter.jsx
+++ src/components/CarFilter/CarFilter.jsx
@@ -1,11 +1,7 @@
-import { useState, useEffect } from "react";
-
-import { makesLocalData } from "./imports";
-
 import { CarBrandSelector } from "./CarBrandSelector";
 import { CarMileageSelector } from "./CarMileageSelector";
 
-const CarFilter = () => {
+const CarFilter = ({ makesLocalData }) => {
   return (
     <form>
       <CarBrandSelector carBrands={makesLocalData} />
