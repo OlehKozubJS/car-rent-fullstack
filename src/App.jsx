92750d11df3c62ee3a3ff22d39b2dd83faae1eed
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-03 23:39:13 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ const App = () => {
           element={
             <MainPage
               cars={carsLocalData}
-              brands={makesLocalData}
+              carBrands={makesLocalData}
               onFavouriteClick={addFavouriteCar}
             />
           }
