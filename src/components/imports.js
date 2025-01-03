23bb42212f22648f37f1b215d2d70167ff369c93
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-03 23:24:29 +0200

    Update imports.js

--- src/components/imports.js
+++ src/components/imports.js
@@ -1,4 +1,5 @@
 import { makesLocalData } from "../database";
+import { handleEventListener } from "../hooks";
 import { SVGimage } from "../images";
 
-export { makesLocalData, SVGimage };
+export { makesLocalData, handleEventListener, SVGimage };
