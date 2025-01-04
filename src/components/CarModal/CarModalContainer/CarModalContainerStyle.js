2aad82a031eef1d0991bd24ddb91a7fd500506b1
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-04 21:32:32 +0200

    Create CarModalContainerStyle.js

---
+++ src/components/CarModal/CarModalContainer/CarModalContainerStyle.js
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+
+const CarModalBackdropStyleBase = styled.section`
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(18, 20, 23, 0.5);
+`;
+
+const CarModalContainerStyleBase = styled.article`
+  position: relative;
+  width: 541px;
+  padding: 40px;
+
+  border-radius: 24px;
+  background: #fff;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 24px;
+`;
+
+export { CarModalBackdropStyleBase, CarModalContainerStyleBase };
