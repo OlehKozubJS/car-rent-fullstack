3c1c54669da86280bfb37b8e57150c94d9460c6c
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-05 00:03:14 +0200

    Update CarFilter.jsx

--- src/components/CarFilter/CarFilter.jsx
+++ src/components/CarFilter/CarFilter.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { CarBrandSelector } from "./CarBrandSelector";
 import { CarMileageSelector } from "./CarMileageSelector";
 import { CarPriceSelector } from "./CarPriceSelector";
