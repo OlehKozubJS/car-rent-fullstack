712017db5260771e341109c2e286a471a8ddc3d3
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-05 00:40:29 +0200

    Update CarFilter.jsx

--- src/components/CarFilter/CarFilter.jsx
+++ src/components/CarFilter/CarFilter.jsx
@@ -37,7 +37,7 @@ const CarFilter = ({ carBrands, onSubmit }) => {
   };
 
   const handleSubmit = () => {
-    onSubmit();
+    onSubmit({ carBrand, minMileage, maxMileage, minPrice, maxPrice });
   };
 
   return (
