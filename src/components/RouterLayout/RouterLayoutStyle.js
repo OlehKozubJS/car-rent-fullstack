9e0709a5d71db936aab61c3f16ec08e5b8758fe7
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-04 23:10:29 +0200

    Update RouterLayoutStyle.js

--- src/components/RouterLayout/RouterLayoutStyle.js
+++ src/components/RouterLayout/RouterLayoutStyle.js
@@ -23,9 +23,7 @@ const RouterNavigationLinkStyleBase = styled(NavLink)`
   border: none;
   border-radius: 12px;
   background-color: ${({ isactive }) => {
-    return isactive === "false"
-      ? "rgb(52, 112, 255)"
-      : "rgba(138, 138, 137, 1)";
+    return isactive === "true" ? "rgb(52, 112, 255)" : "rgba(138, 138, 137, 1)";
   }};
 
   color: #fff;
