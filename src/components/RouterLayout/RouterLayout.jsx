c95481cd1b5b07bc58bc7194235e97be06d6cb57
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-04 22:43:54 +0200

    Update RouterLayout.jsx

--- src/components/RouterLayout/RouterLayout.jsx
+++ src/components/RouterLayout/RouterLayout.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 
 import { RouterNavigation } from "./RouterNavigation";
 
-const RouterLayout = ({ pages }) => {
+const RouterLayout = ({ pages, currentLocation }) => {
   return (
     <div>
       <header>
-        <RouterNavigation pages={pages} />
+        <RouterNavigation pages={pages} currentLocation={currentLocation} />
       </header>
       <main>
         <Suspense downfall={<section>loading...</section>}>
