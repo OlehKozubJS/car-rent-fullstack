2f58d3b73f7672e149e5dc5cf1fdc5e33a8bd799
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-04 20:12:00 +0200

    Create CarBrandSelectorInput.jsx

---
+++ src/components/CarFilter/CarBrandSelector/CarBrandSelectorInput/CarBrandSelectorInput.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+import {
+  CarBrandSelectorInputLabelStyleBase,
+  CarBrandSelectorInputStyleBase,
+  CarBrandSelectorInputButtonStyleBase,
+} from "./CarBrandSelectorStyles";
+
+import { SVGimage } from "../imports";
+
+const CarBrandSelectorInput = ({
+  inputValue,
+  onChange,
+  buttonValue,
+  onClick,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleChange = (event) => {
+    onChange(event.target.value);
+  };
+
+  const open = () => {
+    setIsOpen(true);
+  };
+
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    onClick(isOpen);
+  }, [isOpen]);
+
+  useEffect(() => {
+    setIsOpen(buttonValue);
+  }, [buttonValue]);
+
+  return (
+    <CarBrandSelectorInputLabelStyleBase htmlFor="car-filter-input">
+      <CarBrandSelectorInputStyleBase
+        id="car-filter-input"
+        type="text"
+        placeholder="Enter the text"
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <CarBrandSelectorInputButtonStyleBase
+        type="button"
+        onClick={isOpen ? close : open}
+      >
+        {<SVGimage name={isOpen ? "up" : "down"} />}
+      </CarBrandSelectorInputButtonStyleBase>
+    </CarBrandSelectorInputLabelStyleBase>
+  );
+};
+
+export { CarBrandSelectorInput };
