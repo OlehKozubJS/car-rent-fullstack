500a4eff18c918119edad2e91fef0dc45ab7e19e
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-04 20:08:22 +0200

    Update imports.js

--- src/components/CarFilter/CarBrandSelector/imports.js
+++ src/components/CarFilter/CarBrandSelector/imports.js
@@ -1,4 +1,4 @@
 import { CarFilterHeader } from "../CarFilterStyle";
-import { SVGimage } from "../imports";
+import { handleEventListener, SVGimage } from "../imports";
 
 export { SVGimage, CarFilterHeader };
