50aff0552f99df1e5ea2d795ca7412e0591ee959
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-04 19:40:48 +0200

    Create CarFilterStyle.js

---
+++ src/components/CarFilter/CarFilterStyle.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const CarFilterHeader = styled.h3`
+  font-family: "manrope-medium";
+  font-size: 14px;
+  line-height: 18px;
+  color: rgba(138, 138, 137, 1);
+`;
+
+export { CarFilterHeader };
