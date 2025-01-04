878795a7e1c6ea7e7790c65594eec3cbf9b9ce24
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-04 17:27:03 +0200

    Create CarMileageSelector.jsx

---
+++ src/components/CarFilter/CarMileageSelector/CarMileageSelector.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+const CarMileageSelector = ({ cars }) => {
+  const [carBrands] = useState(cars);
+
+  return (
+    <div>
+      <h3>Car mileage / km</h3>
+
+      <label htmlFor="mileageFrom">
+        <input id="mileageFrom" type="number" placeholder="From" />
+      </label>
+
+      <label htmlFor="mileageTo">
+        <input id="mileageTo" type="number" placeholder="To" />
+      </label>
+    </div>
+  );
+};
+
+export { CarMileageSelector };
