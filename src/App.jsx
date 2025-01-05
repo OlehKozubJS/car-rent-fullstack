bff99df14039cdf4f2420b440ccce10bf61d1599
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-05 14:59:20 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ const App = () => {
               onFavouriteClick={addFavouriteCar}
             />
           }
-        ></Route>
+        />
         <Route
           path="FavouriteCars"
           element={
