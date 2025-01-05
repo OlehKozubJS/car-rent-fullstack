03fef0ad786d348a155a724c37a8d099df3508d7
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-05 14:38:13 +0200

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -47,6 +47,7 @@ const MainPage = ({ cars, carBrands, onFavouriteClick }) => {
   return (
     <section>
       <NavLink to="toProgressWins">hello</NavLink>
+      <NavLink to="/">goodbye</NavLink>
       <Outlet />
       {isModal && <CarModal car={car} onClose={closeModal} />}
       <CarFilter carBrands={carBrands} onSubmit={handleFilteredCars} />
