ab3d658419523abbfbd16ebca9928761cf29b95e
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-05 15:03:44 +0200

    Update TestPage.jsx

--- src/pages/test/TestPage.jsx
+++ src/pages/test/TestPage.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import {} from "react-router-dom";
+
 import { LogicGates } from "./LogicGates";
 import { SimplestCustomRange } from "./SimplestCustomRange";
 import { PageChooser } from "./PageChooser";
