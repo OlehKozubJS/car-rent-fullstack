3642bc7721debf3112f6f8d644d812541176449b
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-11 22:04:04 +0200

    Update RecursionFunctionTest.jsx

--- src/pages/test/RecursionFunctionTest.jsx
+++ src/pages/test/RecursionFunctionTest.jsx
@@ -1,4 +1,13 @@
 const RecursionFunctionTest = () => {
+  const FibonacciFunction = (number) => {
+    let fibonacciArray = [];
+
+    if (number <= 1) {
+      fibonacciArray = [...fibonacciArray, number];
+    } else {
+    }
+  };
+
   return (
     <div>
       <p></p>
