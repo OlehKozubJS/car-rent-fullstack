7b0e1d702dd1f73cc0da13782e2b0131d17e6765
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 20:36:44 +0200

    Update CustomRangeField.jsx

--- src/pages/test/CustomRange/CustomRangeField.jsx
+++ src/pages/test/CustomRange/CustomRangeField.jsx
@@ -3,57 +3,18 @@ import { useState } from "react";
 import { CustomRange } from "./CustomRange";
 
 const CustomRangeField = () => {
-  const [componentName, setComponentName] = useState("Logic gates");
   const [rangeValue, setRangeValue] = useState(0);
-  const [pagePath, setPagePath] = useState("test");
-
-  const handlePagePath = (event) => {
-    event.preventDefault();
-    setPagePath(event.currentTarget.elements.pageNameInput.value);
-    event.currentTarget.reset();
-  };
 
   const handleRangeValue = (newRangeValue) => {
     setRangeValue(newRangeValue);
   };
 
-  const chooseComponent = (newComponentName) => {
-    setComponentName(newComponentName);
-  };
-
   return (
     <section>
-      <ComponentChooser
-        componentNames={[
-          "Logic gates",
-          "Simplest custom range",
-          "Draggable component",
-        ]}
-        currentComponentName={componentName}
-        onChange={chooseComponent}
-      />
-      {componentName === "Logic gates" && <LogicGates />}
-      {componentName === "Simplest custom range" && (
-        <SimplestCustomRange onChange={handleRangeValue} value={rangeValue} />
-      )}
-      {componentName === "Draggable component" && <DraggableComponentField />}
+      <CustomRange onChange={handleRangeValue} value={rangeValue} />
       <p>{rangeValue}</p>
-      <p>valueY={valueY}</p>
-      <p>valueX={valueX}</p>
-      <div>
-        <form onSubmit={handlePagePath}>
-          <input
-            name="pageNameInput"
-            type="text"
-            placeholder="Enter page name"
-          />
-          <button type="submit">Enter</button>
-        </form>
-        <Navigate to={`/${pagePath}`} />
-      </div>
-      <RecursionFunctionTest />
     </section>
   );
 };
 
-export { TestPage };
+export {};
