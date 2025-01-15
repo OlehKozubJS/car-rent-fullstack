c9379742dedbfd333e2ba35f92bc1bca372586f4
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 18:45:17 +0200

    Update RecursionFunctionTest.jsx

--- src/pages/test/RecursionFunctionTest.jsx
+++ src/pages/test/RecursionFunctionTest.jsx
@@ -22,13 +22,13 @@ const RecursionFunctionTest = () => {
 
   return (
     <div>
-      <p>Square of 0: {recursionFunction(0)}</p>
-      <p>Square of 1: {recursionFunction(1)}</p>
-      <p>Square of 2: {recursionFunction(2)}</p>
+      <p>Square of 0: {recursionFunction2(0)}</p>
+      <p>Square of 1: {recursionFunction2(1)}</p>
+      <p>Square of 2: {recursionFunction2(2)}</p>
       <p>Square of 3: {recursionFunction2(3)}</p>
-      <p>Square of 4: {recursionFunction(4)}</p>
-      <p>Square of 5: {recursionFunction(5)}</p>
-      <p>Square of 6: {recursionFunction(6)}</p>
+      <p>Square of 4: {recursionFunction2(4)}</p>
+      <p>Square of 5: {recursionFunction2(5)}</p>
+      <p>Square of 6: {recursionFunction2(6)}</p>
       <p>Square of 7: {recursionFunction(7)}</p>
       <p>Square of 8: {recursionFunction(8)}</p>
       <p>Square of 9: {recursionFunction(9)}</p>
