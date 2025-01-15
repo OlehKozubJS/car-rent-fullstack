a2903875451d2a1946d23e0bf1c53f2e44a5bfb2
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 19:48:47 +0200

    Update SVGimage.jsx

--- src/images/SVGimage.jsx
+++ src/images/SVGimage.jsx
@@ -1,5 +1,11 @@
 import icons from "./icons.svg";
-import styled from "@emotion/css";
+import styled from "@emotion/styled";
+
+const SVGimageStyleBase = styled.svg`
+  display: block;
+  width: 24px;
+  height: 24px;
+`;
 
 const SVGimage = ({ name }) => {
   return (
