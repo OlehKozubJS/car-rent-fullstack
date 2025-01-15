2f13b0142f02ac562c05ffc9ad30d6d78f407c87
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 23:37:59 +0200

    Update GradientHover.jsx

--- src/pages/test/GradientHover/GradientHover.jsx
+++ src/pages/test/GradientHover/GradientHover.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import styled from "styled-components";
+import {} from "./GradientHoverStyle";
 
 const GradientHoverTestComponentStyleBase = styled.div`
   height: 250px;
