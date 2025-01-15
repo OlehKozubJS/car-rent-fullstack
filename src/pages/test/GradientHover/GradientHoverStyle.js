a9c3e9c73a53c38fc9d3c1d74b0592ba1ef6f8a0
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 23:37:17 +0200

    Create GradientHoverStyle.js

---
+++ src/pages/test/GradientHover/GradientHoverStyle.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+
+import styled from "styled-components";
+
+const GradientHoverTestComponentStyleBase = styled.div`
+  height: 250px;
+  width: 400px;
+
+  background-color: black;
+  color: white;
+
+  &:hover {
+    background-image: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 0)
+        ${({ gradientleft }) => {
+          return gradientleft;
+        }}px,
+      rgba(255, 255, 255, 1)
+        ${({ gradientleft }) => {
+          return gradientleft + 20;
+        }}px,
+      rgba(255, 255, 255, 1)
+        ${({ gradientleft }) => {
+          return gradientleft + 30;
+        }}px,
+      rgba(255, 255, 255, 0)
+        ${({ gradientleft }) => {
+          return gradientleft + 50;
+        }}px
+    );
+  }
+`;
+
+const GradientHover = () => {
+  const handleMouseOver = () => {};
+
+  return (
+    <div>
+      <GradientHoverTestComponentStyleBase
+        gradientleft={30}
+        onMouseOver={handleMouseOver}
+      >
+        Gradient hover
+      </GradientHoverTestComponentStyleBase>
+    </div>
+  );
+};
+
+export { GradientHover };
