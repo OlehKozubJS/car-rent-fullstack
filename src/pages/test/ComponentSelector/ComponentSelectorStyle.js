ec855227dc35f5531e4a2d4ff396cb32a1c80449
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 21:47:46 +0200

    Create ComponentSelectorStyle.js

---
+++ src/pages/test/ComponentSelector/ComponentSelectorStyle.js
@@ -0,0 +1,40 @@
+import styled from "@emotion/styled";
+import { NavLink } from "react-router-dom";
+
+const RouterNavigationContainerStyleBase = styled.nav`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  column-gap: 18px;
+
+  margin-bottom: 50px;
+`;
+
+const RouterNavigationLinkStyleBase = styled(NavLink)`
+  margin-top: 28px;
+
+  height: 36px;
+
+  display: flex;
+  padding: 12px 50px;
+  justify-content: center;
+  align-items: center;
+
+  border: none;
+  border-radius: 12px;
+  background-color: ${({ isactive }) => {
+    return isactive === "true" ? "rgb(52, 112, 255)" : "rgba(138, 138, 137, 1)";
+  }};
+
+  color: #fff;
+  font-size: 14px;
+  line-height: 20px;
+
+  font-family: "Manrope-SemiBold", sans-serif;
+
+  &:hover {
+    background-color: rgb(11, 68, 205);
+  }
+`;
+
+export { RouterNavigationContainerStyleBase, RouterNavigationLinkStyleBase };
