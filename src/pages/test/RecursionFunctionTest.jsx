f24b421aefe8c1f42c35d95c854cc8df8d58eb00
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 18:29:51 +0200

    Update RecursionFunctionTest.jsx

--- src/pages/test/RecursionFunctionTest.jsx
+++ src/pages/test/RecursionFunctionTest.jsx
@@ -9,7 +9,17 @@ const RecursionFunctionTest = () => {
 
   return (
     <div>
+      <p>{recursionFunction(0)}</p>
+      <p>{recursionFunction(1)}</p>
+      <p>{recursionFunction(2)}</p>
+      <p>{recursionFunction(3)}</p>
+      <p>{recursionFunction(4)}</p>
       <p>{recursionFunction(5)}</p>
+      <p>{recursionFunction(6)}</p>
+      <p>{recursionFunction(7)}</p>
+      <p>{recursionFunction(8)}</p>
+      <p>{recursionFunction(9)}</p>
+      <p>{recursionFunction(10)}</p>
     </div>
   );
 };
