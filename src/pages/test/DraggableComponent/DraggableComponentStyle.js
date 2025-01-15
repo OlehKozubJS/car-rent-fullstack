37631d7ba050844ddf8fdd873b66a443641f0618
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 20:07:54 +0200

    Create DraggableComponentStyle.js

---
+++ src/pages/test/DraggableComponent/DraggableComponentStyle.js
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+
+const RangeField = styled.div`
+  width: 500px;
+  height: 50px;
+  background-color: rgb(200, 200, 250);
+
+  position: relative;
+`;
+
+const RangeDial = styled.div`
+  position: absolute;
+  top: 0;
+  left: ${({ left }) => {
+    return left;
+  }}px;
+  width: 50px;
+  height: 50px;
+  background-color: rgb(100, 100, 250);
+`;
+
+const DraggableComponentStyle = styled.div`
+  position: absolute;
+  top: ${({ top }) => {
+    return top;
+  }}px;
+  left: ${({ left }) => {
+    return left;
+  }}px;
+  width: 50px;
+  height: 50px;
+  background-color: rgb(100, 100, 250);
+`;
+
+export { RangeField, RangeDial, DraggableComponentStyle };
