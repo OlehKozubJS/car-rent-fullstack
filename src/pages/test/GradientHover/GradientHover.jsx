6282c3c36d0b0dfa09bd226f12060be8cfc2aa76
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 22:34:55 +0200

    Update GradientHover.jsx

--- src/pages/test/GradientHover/GradientHover.jsx
+++ src/pages/test/GradientHover/GradientHover.jsx
@@ -8,6 +8,13 @@ const GradientHoverTestComponentStyleBase = styled.div`
   color: white;
 
   &:hover {
+    background-image: linear-gradient(
+      to right,
+      black 0%,
+      white 5%,
+      white 10%,
+      black 15%
+    );
   }
 `;
 
