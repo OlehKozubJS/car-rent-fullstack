cebb36f3da111e2a533e3bc92c68103b0cab4ea1
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-16 00:14:10 +0200

    Update GradientHoverStyle.js

--- src/pages/test/GradientHover/GradientHoverStyle.js
+++ src/pages/test/GradientHover/GradientHoverStyle.js
@@ -21,20 +21,6 @@ const GradientHoverGradientComponentStyleBase = styled.div`
   background: none;
   color: white;
 `;
-const setLinearGradient = (gradientLeft) => {
-  `background-image: linear-gradient(
-    to right,
-  rgba(255, 255, 255, 0),
-  rgba(255, 255, 255, 0)
-    ${rgradientLeft}px,
-  rgba(255, 255, 255, 1)
-    ${gradientLeft}px,
-  rgba(255, 255, 255, 1)
-    ${gradientLeft}px,
-  rgba(255, 255, 255, 0)
-    ${gradientLeft}px
-  );`;
-};
 
 export {
   GradientHoverTestComponentStyleBase,
