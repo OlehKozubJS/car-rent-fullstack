d9ce86e2c096e11daaae65dc604e6b5f9dceca13
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 23:42:07 +0200

    Update GradientHoverStyle.js

--- src/pages/test/GradientHover/GradientHoverStyle.js
+++ src/pages/test/GradientHover/GradientHoverStyle.js
@@ -6,29 +6,6 @@ const GradientHoverTestComponentStyleBase = styled.div`
 
   background-color: black;
   color: white;
-
-  &:hover {
-    background-image: linear-gradient(
-      to right,
-      rgba(255, 255, 255, 0),
-      rgba(255, 255, 255, 0)
-        ${({ gradientleft }) => {
-          return gradientleft;
-        }}px,
-      rgba(255, 255, 255, 1)
-        ${({ gradientleft }) => {
-          return gradientleft + 20;
-        }}px,
-      rgba(255, 255, 255, 1)
-        ${({ gradientleft }) => {
-          return gradientleft + 30;
-        }}px,
-      rgba(255, 255, 255, 0)
-        ${({ gradientleft }) => {
-          return gradientleft + 50;
-        }}px
-    );
-  }
 `;
 
 const GradientHoverGradientComponentStyleBase = styled.div`
