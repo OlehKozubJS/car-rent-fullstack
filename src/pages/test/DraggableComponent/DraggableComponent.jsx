a3ab2549f465d906b0c0cb0c4dea7cd3ec347016
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 20:07:08 +0200

    Create DraggableComponent.jsx

---
+++ src/pages/test/DraggableComponent/DraggableComponent.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+import { handleEventListener } from "../../../hooks";
+
+import { DraggableComponentStyle } from "./SimplestCustomRangeStyle";
+
+const DraggableComponent = ({ valueY, valueX, onChange }) => {
+  const [isDraggable, setIsDraggable] = useState(false);
+  const [mouseDownY, setMouseDownY] = useState(valueY);
+  const [mouseDownX, setMouseDownX] = useState(valueX);
+
+  const handleMouseDown = ({ clientY, clientX }) => {
+    setIsDraggable(true);
+    setMouseDownY(clientY);
+    setMouseDownX(clientX);
+  };
+
+  const handleMouseMove = ({ clientY, clientX }) => {
+    if (isDraggable) {
+      onChange({
+        valueY: valueY + clientY - mouseDownY,
+        valueX: valueX + clientX - mouseDownX,
+      });
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDraggable(false);
+  };
+
+  useEffect(handleEventListener("mousedown", handleMouseDown), [isDraggable]);
+  useEffect(handleEventListener("mousemove", handleMouseMove), [isDraggable]);
+  useEffect(handleEventListener("mouseup", handleMouseUp), [isDraggable]);
+
+  return (
+    <DraggableComponentStyle
+      onMouseDown={handleMouseDown}
+      top={valueY}
+      left={valueX}
+    ></DraggableComponentStyle>
+  );
+};
+
+export { DraggableComponent };
