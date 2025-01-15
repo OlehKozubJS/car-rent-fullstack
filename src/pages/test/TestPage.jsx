184e9794f4d3a60ef69d915ae969b4fe597cc12c
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-15 21:08:11 +0200

    Update TestPage.jsx

--- src/pages/test/TestPage.jsx
+++ src/pages/test/TestPage.jsx
@@ -11,14 +11,6 @@ import { NavigationTest } from "./NavigationTest";
 const TestPage = () => {
   const [componentName, setComponentName] = useState("Logic gates");
 
-  const [pagePath, setPagePath] = useState("test");
-
-  const handlePagePath = (event) => {
-    event.preventDefault();
-    setPagePath(event.currentTarget.elements.pageNameInput.value);
-    event.currentTarget.reset();
-  };
-
   const chooseComponent = (newComponentName) => {
     setComponentName(newComponentName);
   };
