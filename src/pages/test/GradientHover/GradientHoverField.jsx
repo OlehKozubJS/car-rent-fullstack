fe8a272d2cff3e922576041937d26ab9958e00ea
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-16 15:44:12 +0200

    Create GradientHoverField.jsx

---
+++ src/pages/test/GradientHover/GradientHoverField.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+
+import { handleInterval } from "../../../hooks";
+
+import { setLinearGradient } from "./setLinearGradient";
+
+import {
+  GradientHoverTestComponentStyleBase,
+  GradientHoverGradientComponentStyleBase,
+} from "./GradientHoverStyle";
+
+const GradientHover = () => {
+  const [isGradientAnimation, setIsGradientAnimation] = useState(false);
+  const [parentNodeInformation, setParentnodeInformation] = useState({});
+  const [gradientLeft, setGradientLeft] = useState(-40);
+
+  const gradientAnimation = () => {
+    if (isGradientAnimation) {
+      if (gradientLeft >= parentNodeInformation.offsetWidth + 40) {
+        setGradientLeft(-40);
+        setIsGradientAnimation(false);
+      } else {
+        setGradientLeft(gradientLeft + 5);
+      }
+    }
+  };
+
+  const handleMouseOver = (event) => {
+    setParentnodeInformation(event.target.parentNode);
+    setIsGradientAnimation(true);
+  };
+
+  useEffect(() => {
+    const animation = setInterval(gradientAnimation, 5);
+    if (!isGradientAnimation) {
+      clearInterval(animation);
+    }
+    return () => {
+      clearInterval(animation);
+    };
+  }, [isGradientAnimation, gradientLeft]);
+
+  return (
+    <div>
+      <GradientHoverTestComponentStyleBase>
+        Gradient hover
+        <GradientHoverGradientComponentStyleBase
+          onMouseOver={handleMouseOver}
+          backgroundimage={setLinearGradient(gradientLeft)}
+        ></GradientHoverGradientComponentStyleBase>
+      </GradientHoverTestComponentStyleBase>
+    </div>
+  );
+};
+
+export { GradientHover };
