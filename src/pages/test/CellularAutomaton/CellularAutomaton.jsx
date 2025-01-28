82ff2e253dd1d832fad79a8cf662fe82f4bad2c6
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-28 23:11:33 +0200

    Update CellularAutomaton.jsx

--- src/pages/test/CellularAutomaton/CellularAutomaton.jsx
+++ src/pages/test/CellularAutomaton/CellularAutomaton.jsx
@@ -6,13 +6,13 @@ import {
 import { Cells } from "./Cells";
 
 const createCllsDataArray = () => {
-  let cells = [];
+  let cellsData = [];
 
   for (let cellNumber = 1; cellNumber <= 2500; cellNumber += 1) {
-    cells = [...cells, false];
+    cellsData = [...cells, false];
   }
 
-  return cells;
+  return cellsData;
 };
 
 const CellularAutomaton = () => {
