fe5efd80a15f6d3f9abc5cdca0ca764a50e00787
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-28 19:13:34 +0200

    Update Cells.jsx

--- src/pages/test/CellularAutomaton/Cells.jsx
+++ src/pages/test/CellularAutomaton/Cells.jsx
@@ -1,7 +1,13 @@
 import { CellStyleBase } from "./CellularAutomatonStyle";
 
 const CellularAutomaton = () => {
-  return <CellStyleBase></CellStyleBase>;
+  const cells = [];
+
+  for (let cellNumber = 1; cellNumber <= 2500; cellNumber += 1) {
+    cells = [...cells, <CellStyleBase></CellStyleBase>];
+  }
+
+  return cells;
 };
 
 export { CellularAutomaton };
