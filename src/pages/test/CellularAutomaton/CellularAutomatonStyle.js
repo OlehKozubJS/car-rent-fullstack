512d476b90813c1145f4b7f12367216361fd28a0
OlehKozubJS <oleg.kozub54@gmail.com>
2025-01-28 17:45:48 +0200

    Update CellularAutomatonStyle.js

--- src/pages/test/CellularAutomaton/CellularAutomatonStyle.js
+++ src/pages/test/CellularAutomaton/CellularAutomatonStyle.js
@@ -1,6 +1,13 @@
 import styled from "@emotion/styled";
 
-const CellularFieldStyleBase = styled.div``;
+const CellularFieldStyleBase = styled.div`
+  height: 500px;
+  width: 500px;
+
+  border-style: solid;
+  border-width: 1px;
+  border-color: rgb(0, 0, 0);
+`;
 
 const CellStyleBase = styled.div`
   height: 10px;
