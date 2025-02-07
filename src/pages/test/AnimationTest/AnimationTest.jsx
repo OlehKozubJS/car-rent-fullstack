d0975e8db514e15c2f1a2daeb9006c44dd9d9984
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-07 21:41:03 +0200

    Update AnimationTest.jsx

--- src/pages/test/AnimationTest/AnimationTest.jsx
+++ src/pages/test/AnimationTest/AnimationTest.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+
 const AnimationTest = () => {
-  return <div>This is animation test</div>;
+  return (
+    <div>
+      This is animation test<></>
+    </div>
+  );
 };
 
 export { AnimationTest };
