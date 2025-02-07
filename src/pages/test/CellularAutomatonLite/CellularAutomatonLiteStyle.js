050a76d4dd0f1cf533881d9c99e05451a7da3117
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-07 18:05:15 +0200

    Create CellularAutomatonLiteStyle.js

---
+++ src/pages/test/CellularAutomatonLite/CellularAutomatonLiteStyle.js
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+
+const CellularFieldStyleBase = styled.div`
+  box-sizing: content-box;
+  height: 500px;
+  width: 500px;
+
+  border-style: solid;
+  border-width: 1px;
+  border-color: rgb(0, 0, 0);
+
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const CellStyleBase = styled.div`
+  height: 10px;
+  width: 10px;
+
+  border-style: solid;
+  border-width: 1px;
+  border-color: rgb(0, 0, 0);
+
+  background-color: ${({ isActive }) => {
+    return (isActive && "rgb(0, 0, 0)") || "rgb(255, 255, 255)";
+  }};
+
+  font-size: 9px;
+  color: ${({ isActive }) => {
+    return (isActive && "rgb(255, 255, 255)") || "rgb(0, 0, 0)";
+  }};
+`;
+
+export { CellularFieldStyleBase, CellStyleBase };
