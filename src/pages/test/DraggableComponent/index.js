1f9845a4d1787f953ad4845ce865be39ba922727
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-07 21:54:45 +0200

    Update index.js

--- src/pages/test/DraggableComponent/index.js
+++ src/pages/test/DraggableComponent/index.js
@@ -1,4 +1,4 @@
 import { DraggableComponent } from "./DraggableComponent";
 import { DraggableComponentField } from "./DraggableComponentField";
 
-export { DraggableComponentField, DraggableComponent };
+export { DraggableComponent, DraggableComponentField };
