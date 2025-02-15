fcdb5794fd89789758d286dec1e42d13175ce149
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-15 15:25:36 +0200

    Update TotalDigitalization.jsx

--- src/pages/test/TotalDigitalization/TotalDigitalization.jsx
+++ src/pages/test/TotalDigitalization/TotalDigitalization.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 const TotalDigitalization = () => {
-  const [] = useState();
-  const [] = useState();
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState("");
 
   const transformTextToDigits = () => {};
 
