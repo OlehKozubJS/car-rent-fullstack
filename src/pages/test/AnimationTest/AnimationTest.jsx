d4ad298f2f8e711f69516687c2596b512b9c458b
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-15 11:15:19 +0200

    Update AnimationTest.jsx

--- src/pages/test/AnimationTest/AnimationTest.jsx
+++ src/pages/test/AnimationTest/AnimationTest.jsx
@@ -6,7 +6,7 @@ const AnimationTest = () => {
   const [transformRotateValue, setTransformRotateValue] = useState(0);
   const [transformRotateYvalue, setTransformRotateYvalue] = useState(0);
   const [transformRotateXvalue, setTransformRotateXvalue] = useState(0);
-  const [perspective, setPerspective] = useState(0);
+  const [perspectiveValue, setPerspectiveValue] = useState(0);
 
   const handleTransformRotateValue = ({ valueX }) => {
     if (valueX >= 0 && valueX <= 180) {
@@ -26,9 +26,9 @@ const AnimationTest = () => {
     }
   };
 
-  const handlePerspective = ({ valueX }) => {
+  const handlePerspectiveValue = ({ valueX }) => {
     if (valueX >= 0 && valueX <= 180) {
-      setPerspective(valueX);
+      setPerspectiveValue(valueX);
     }
   };
 
@@ -58,6 +58,16 @@ const AnimationTest = () => {
         <DraggableComponent
           onChange={handleTransformRotateXvalue}
           valueX={transformRotateXvalue}
+          valueY={0}
+          height={50}
+          width={150}
+          backgroundColor={"blue"}
+        />
+      </div>
+      <div style={{ position: "relative", height: "50px" }}>
+        <DraggableComponent
+          onChange={handlePerspectiveValue}
+          valueX={perspective}
           valueY={0}
           height={50}
           width={150}
