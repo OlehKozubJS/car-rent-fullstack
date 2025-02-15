9ea2a7c04ba5217e30f66a86fadeefe995f941f3
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-15 23:54:34 +0200

    Update TotalDigitalization.jsx

--- src/pages/test/TotalDigitalization/TotalDigitalization.jsx
+++ src/pages/test/TotalDigitalization/TotalDigitalization.jsx
@@ -60,7 +60,7 @@ const TotalDigitalization = () => {
   const transformRussianCyrillicToLatin = () => {
     setResult(
       query.map((character) => {
-        return Object.keys(rcl).includes(character)?:character;
+        return Object.keys(rcl).includes(character)?rcl:character;
       })
     );
   };
