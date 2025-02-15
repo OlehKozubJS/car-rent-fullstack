6a6cb3581af0d13f5468804809dcf1f54af45d66
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-15 17:55:07 +0200

    Update BinarySystem.jsx

--- src/pages/test/BinarySystem/BinarySystem.jsx
+++ src/pages/test/BinarySystem/BinarySystem.jsx
@@ -4,9 +4,11 @@ const BinarySystem = () => {
   const [query, setQuery] = useState("");
   const [result, setResult] = useState("");
 
+  const handleQuery = () => {};
+
   return (
     <div>
-      <input type="text" placeholder="Enter a number" />
+      <input type="text" placeholder="Enter a number" onChange={handleQuery} />
       <button>Decimal to binary</button>
       <button>Binary to decimal</button>
       <p>{result}</p>
