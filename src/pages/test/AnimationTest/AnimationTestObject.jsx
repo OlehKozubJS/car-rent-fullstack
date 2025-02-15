091e0c057ffe223c857278fb545f589892b7a13b
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-15 13:58:41 +0200

    Create AnimationTestObject.jsx

---
+++ src/pages/test/AnimationTest/AnimationTestObject.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+import { AnimationTestControllers } from "./AnimationTestControllers";
+
+const AnimationTest = () => {
+  const [transformValues, setTransformValues] = useState({
+    r: 0,
+    ry: 0,
+    rx: 0,
+    p: 500,
+  });
+
+  const handleTransformValues = (newValues) => setTransformValues(newValues);
+
+  return (
+    <div>
+      <AnimationTestControllers
+        onChange={handleTransformValues}
+        value={transformValues}
+      />
+      <div>
+        <div
+          style={{
+            height: "250px",
+            width: "250px",
+            border: "5px solid rgb(150, 250, 250)",
+            backgroundColor: "red",
+            transformOrigin: "center",
+            transform: `perspective(${transformValues.p}px) rotate(${transformValues.r}deg) rotateY(${transformValues.ry}deg) rotateX(${transformValues.rx}deg)`,
+          }}
+        >
+          Hello
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { AnimationTest };
