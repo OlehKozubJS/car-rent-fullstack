459532a7a93d5011b7095759f8bea62ce82da5c3
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-15 14:14:12 +0200

    Update TotalDigitalization.jsx

---
+++ src/pages/test/TotalDigitalization/TotalDigitalization.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const TotalDigitalization = () => {
+  return (
+    <div>
+      <div></div>
+      <p></p>
+    </div>
+  );
+};
+
+export { TotalDigitalization };
