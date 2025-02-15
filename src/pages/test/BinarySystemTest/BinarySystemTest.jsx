183cd020937ea301e725d799dddde2163f82c2f7
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-15 21:13:02 +0200

    Update BinarySystemTest.jsx

--- src/pages/test/BinarySystemTest/BinarySystemTest.jsx
+++ src/pages/test/BinarySystemTest/BinarySystemTest.jsx
@@ -1,5 +1,15 @@
 import { useState } from "react";
 
+const dtb = (query) => {
+  if (query === 0) {
+    return "";
+  } else {
+    const binaryDigit = query % 2;
+    const newQuotient = Math.floor(query / 2);
+    return dtb(newQuotient) + binaryDigit;
+  }
+};
+
 const BinarySystemTest = () => {
   const [query, setQuery] = useState(0);
   const [result, setResult] = useState();
@@ -9,27 +19,19 @@ const BinarySystemTest = () => {
   };
 
   const transformDecimalToBinary = () => {
-    if (query === 0) {
-      setResult("");
-    } else {
-      const binaryDigit = query % 2;
-      setQuery(Math.floor(query / 2));
-      setResult(transformDecimalToBinary() + binaryDigit);
-    }
+    setResult(dtb(query));
   };
 
-  const transformBinaryToDecimal = () => {
-    setResult(
-      [...query.split("")].sort((a, b) => Number(b) - Number(a)).join("")
-    );
-  };
+  const transformBinaryToDecimal = () => {};
 
   return (
     <div>
       <input type="text" placeholder="Enter a number" onChange={handleQuery} />
       <button onClick={transformDecimalToBinary}>Decimal to binary</button>
       <button onClick={transformBinaryToDecimal}>Binary to decimal</button>
-      <p>{result}</p>
+      <p>
+        {[...query.split("")].sort((a, b) => Number(b) - Number(a)).join("")}
+      </p>
     </div>
   );
 };
