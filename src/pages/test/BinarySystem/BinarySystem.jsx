b3db842353302fcd02a0f553684bf22dcc5652c0
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-15 17:50:14 +0200

    Update BinarySystem.jsx

---
+++ src/pages/test/BinarySystem/BinarySystem.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const BinarySystem = () => {
+  return (
+    <div>
+      <input />
+      <button></button>
+      <button></button>
+      <p></p>
+    </div>
+  );
+};
+
+export { BinarySystem };
