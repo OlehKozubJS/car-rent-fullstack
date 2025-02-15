7beb09d934f892e468f31ea970a4b6fda9f8f715
OlehKozubJS <oleg.kozub54@gmail.com>
2025-02-15 17:53:58 +0200

    Update BinarySystem.jsx

--- src/pages/test/BinarySystem/BinarySystem.jsx
+++ src/pages/test/BinarySystem/BinarySystem.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 
 const BinarySystem = () => {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState("");
+
   return (
     <div>
       <input type="text" placeholder="Enter a number" />
       <button>Decimal to binary</button>
       <button>Binary to decimal</button>
-      <p></p>
+      <p>{}</p>
     </div>
   );
 };
